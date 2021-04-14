5844c690d9b8cb810b1eebcb392f77682468d540
braginini <bangvalo@gmail.com>
2021-04-14 14:33:11 +0200

    refactor: simplify iface package

--- iface/cfg.go
+++ iface/cfg.go
@@ -3,11 +3,63 @@ package iface
 import (
 	log "github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
+	"golang.zx2c4.com/wireguard/device"
+	"golang.zx2c4.com/wireguard/ipc"
+	"golang.zx2c4.com/wireguard/tun"
 	"golang.zx2c4.com/wireguard/wgctrl"
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 	"net"
 	"time"
 )
+
+const (
+	defaultMTU = 1280
+)
+
+// Saves tun device object - is it required?
+var tunIface tun.Device
+
+// Create Creates a new Wireguard interface, sets a given IP and brings it up.
+// Will reuse an existing one.
+func Create(iface string, address string) error {
+	var err error
+
+	tunIface, err = tun.CreateTUN(iface, defaultMTU)
+	if err != nil {
+		return err
+	}
+
+	// We need to create a wireguard-go device and listen to configuration requests
+	tunDevice := device.NewDevice(tunIface, device.NewLogger(device.LogLevelSilent, "[wiretrustee] "))
+	tunDevice.Up()
+	tunSock, err := ipc.UAPIOpen(iface)
+	if err != nil {
+		return err
+	}
+	uapi, err := ipc.UAPIListen(iface, tunSock)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			conn, err := uapi.Accept()
+			if err != nil {
+				log.Debugln(err)
+				return
+			}
+			go tunDevice.IpcHandle(conn)
+		}
+	}()
+
+	log.Debugln("UAPI listener started")
+
+	err = assignAddr(iface, address)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 // Extends the functionality of Configure(iface string, privateKey string) by generating a new Wireguard private key
 func ConfigureWithKeyGen(iface string) (*wgtypes.Key, error) {
