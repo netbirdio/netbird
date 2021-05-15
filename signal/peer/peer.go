5d3166b4d2a8e582e74df7efd2e11320fd1a698a
mlsmaycon <mlsmaycon@gmail.com>
2021-05-15 15:20:49 +0500

    fix doc and lint warns for signal package

--- signal/peer/peer.go
+++ signal/peer/peer.go
@@ -5,7 +5,7 @@ import (
 	"github.com/wiretrustee/wiretrustee/signal/proto"
 )
 
-// Representation of a connected Peer
+// Peer representation of a connected Peer
 type Peer struct {
 	// a unique id of the Peer (e.g. sha256 fingerprint of the Wireguard public key)
 	Id string
@@ -14,6 +14,7 @@ type Peer struct {
 	Stream proto.SignalExchange_ConnectStreamServer
 }
 
+// NewPeer creates a new instance of a connected Peer
 func NewPeer(id string, stream proto.SignalExchange_ConnectStreamServer) *Peer {
 	return &Peer{
 		Id:     id,
@@ -21,19 +22,20 @@ func NewPeer(id string, stream proto.SignalExchange_ConnectStreamServer) *Peer {
 	}
 }
 
-// registry that holds all currently connected Peers
+// Registry registry that holds all currently connected Peers
 type Registry struct {
 	// Peer.key -> Peer
 	Peers map[string]*Peer
 }
 
+// NewRegistry creates a new connected Peer registry
 func NewRegistry() *Registry {
 	return &Registry{
 		Peers: make(map[string]*Peer),
 	}
 }
 
-// Registers peer in the registry
+// Register registers peer in the registry
 func (reg *Registry) Register(peer *Peer) {
 	if _, exists := reg.Peers[peer.Id]; exists {
 		log.Warnf("peer [%s] has been already registered", peer.Id)
@@ -45,7 +47,7 @@ func (reg *Registry) Register(peer *Peer) {
 	reg.Peers[peer.Id] = peer
 }
 
-// Deregister Peer from the Registry (usually once it disconnects)
+// DeregisterHub deregister Peer from the Registry (usually once it disconnects)
 func (reg *Registry) DeregisterHub(peer *Peer) {
 	if _, ok := reg.Peers[peer.Id]; ok {
 		delete(reg.Peers, peer.Id)
