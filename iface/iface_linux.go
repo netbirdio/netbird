31c8749bb5be1b253ef7eabca889509eaadce663
mlsmaycon <mlsmaycon@gmail.com>
2021-06-06 00:40:44 +0200

    Add windows support and update wireguard-go deps

--- iface/iface_linux.go
+++ iface/iface_linux.go
@@ -3,6 +3,8 @@ package iface
 import (
 	log "github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
+	"golang.zx2c4.com/wireguard/tun"
+
 	"os"
 )
 
@@ -11,23 +13,38 @@ import (
 //)
 
 // assignAddr Adds IP address to the tunnel interface
-func assignAddr(iface string, address string) error {
+func assignAddr(address string, tunDevice tun.Device) error {
+	var err error
 	attrs := netlink.NewLinkAttrs()
-	attrs.Name = iface
+	attrs.Name, err = tunDevice.Name()
 
 	link := wgLink{
 		attrs: &attrs,
 	}
 
-	log.Debugf("adding address %s to interface: %s", address, iface)
+	log.Debugf("adding address %s to interface: %s", address, attrs.Name)
 	addr, _ := netlink.ParseAddr(address)
-	err := netlink.AddrAdd(&link, addr)
+	err = netlink.AddrAdd(&link, addr)
 	if os.IsExist(err) {
-		log.Infof("interface %s already has the address: %s", iface, address)
+		log.Infof("interface %s already has the address: %s", attrs.Name, address)
 	} else if err != nil {
 		return err
 	}
 	// On linux, the link must be brought up
 	err = netlink.LinkSetUp(&link)
 	return err
+}
+
+type wgLink struct {
+	attrs *netlink.LinkAttrs
+}
+
+// Attrs returns the Wireguard's default attributes
+func (w *wgLink) Attrs() *netlink.LinkAttrs {
+	return w.attrs
+}
+
+// Type returns the interface type
+func (w *wgLink) Type() string {
+	return "wireguard"
 }
