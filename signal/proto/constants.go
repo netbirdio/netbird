9538693f2da82d654bef9b8d66be610e4f5e35eb
braginini <bangvalo@gmail.com>
2021-06-17 11:12:35 +0200

    fix: #35 peer Registration Race when client connects to the signal server

--- signal/proto/constants.go
+++ signal/proto/constants.go
@@ -2,3 +2,4 @@ package proto
 
 // protocol constants, field names that can be used by both client and server
 const HeaderId = "x-wiretrustee-peer-id"
+const HeaderRegistered = "x-wiretrustee-peer-registered"
