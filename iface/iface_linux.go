e6d446eb9e70e48934ba1b6781da61e6e0ade79d
mlsmaycon <mlsmaycon@gmail.com>
2021-06-18 13:01:43 +0200

    add linux native wg interface

--- iface/iface_linux.go
+++ iface/iface_linux.go
@@ -3,23 +3,63 @@ package iface
 import (
 	log "github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
-	"golang.zx2c4.com/wireguard/tun"
-
 	"os"
 )
 
-//const (
-//	interfacePrefix = "wg"
-//)
-
-// assignAddr Adds IP address to the tunnel interface
-func assignAddr(address string, tunDevice tun.Device) error {
-	var err error
+// Create Creates a new Wireguard interface, sets a given IP and brings it up.
+// Will reuse an existing one.
+func Create(iface string, address string) error {
 	attrs := netlink.NewLinkAttrs()
-	attrs.Name, err = tunDevice.Name()
+	attrs.Name = iface
+
+	link := wgLink{
+		attrs: &attrs,
+	}
+
+	log.Debugf("adding device: %s", iface)
+	err := netlink.LinkAdd(&link)
+	if os.IsExist(err) {
+		log.Infof("interface %s already exists. Will reuse.", iface)
+	} else if err != nil {
+		return err
+	}
+
+	log.Debugf("adding address %s to interface: %s", address, iface)
+	addr, _ := netlink.ParseAddr(address)
+	err = netlink.AddrAdd(&link, addr)
+	if os.IsExist(err) {
+		log.Infof("interface %s already has the address: %s", iface, address)
+	} else if err != nil {
+		return err
+	}
+	err = assignAddr(address, iface)
 	if err != nil {
 		return err
 	}
+
+	// todo do a discovery
+	log.Debugf("setting MTU: %s", iface)
+	err = netlink.LinkSetMTU(&link, defaultMTU)
+	if err != nil {
+		log.Errorf("error setting MTU on interface: %s", iface)
+		return err
+	}
+
+	log.Debugf("bringing up interface: %s", iface)
+	err = netlink.LinkSetUp(&link)
+	if err != nil {
+		log.Errorf("error bringing up interface: %s", iface)
+		return err
+	}
+
+	return nil
+}
+
+// assignAddr Adds IP address to the tunnel interface
+func assignAddr(address, name string) error {
+	var err error
+	attrs := netlink.NewLinkAttrs()
+	attrs.Name = name
 
 	link := wgLink{
 		attrs: &attrs,
