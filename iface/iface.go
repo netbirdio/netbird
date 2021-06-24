f2ca2fc7c1a0ee39825e5a995a9a40a3f3029397
braginini <hello@wiretrustee.com>
2021-06-24 11:02:40 +0200

    refactort: extract method to create Wireguard interface using kernel module

--- iface/iface.go
+++ iface/iface.go
@@ -5,5 +5,5 @@ package iface
 // Create Creates a new Wireguard interface, sets a given IP and brings it up.
 // Will reuse an existing one.
 func Create(iface string, address string) error {
-	return CreateInUserspace(iface, address)
+	return CreateWithUserspace(iface, address)
 }
