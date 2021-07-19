993da1eb6dd6139a48a53a3ef967710b9116b004
braginini <hello@wiretrustee.com>
2021-07-19 13:35:51 +0200

    refactor: remove unused code

--- connection/engine_test.go
+++ connection/engine_test.go
@@ -1,16 +1,14 @@
 package connection
 
 import (
-	"bytes"
 	"context"
+	"fmt"
 	ice "github.com/pion/ice/v2"
 	log "github.com/sirupsen/logrus"
 	"github.com/wiretrustee/wiretrustee/iface"
 	sig "github.com/wiretrustee/wiretrustee/signal"
 	"golang.zx2c4.com/wireguard/wgctrl"
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
-	"io/ioutil"
-	"strings"
 	"testing"
 	"time"
 )
@@ -24,8 +22,6 @@ const ifaceName = "utun9991"
 func Test_Start(t *testing.T) {
 	level, _ := log.ParseLevel("Debug")
 	log.SetLevel(level)
-	b := bytes.NewBufferString("")
-	log.SetOutput(b)
 
 	var err error
 	testKey, err = wgtypes.GenerateKey()
@@ -65,21 +61,9 @@ func Test_Start(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	out, err := ioutil.ReadAll(b)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expectedMSG := "connected to the Signal Exchange Stream"
-	if !strings.Contains(string(out), expectedMSG) {
-		t.Fatalf("expected \"%s\" got \"%s\"", expectedMSG, string(out))
-	}
 }
 
 func TestEngine_InitializePeerWithoutRemote(t *testing.T) {
-	b := bytes.NewBufferString("")
-	log.SetOutput(b)
 	tmpKey, err := wgtypes.GenerateKey()
 	if err != nil {
 		t.Fatal(err)
@@ -106,8 +90,6 @@ func TestEngine_InitializePeerWithoutRemote(t *testing.T) {
 }
 
 func TestEngine_Initialize2PeersWithoutRemote(t *testing.T) {
-	b := bytes.NewBufferString("")
-	log.SetOutput(b)
 	tmpKey1, err := wgtypes.GenerateKey()
 	if err != nil {
 		t.Fatal(err)
@@ -146,26 +128,30 @@ func TestEngine_Initialize2PeersWithoutRemote(t *testing.T) {
 }
 
 func TestEngine_RemovePeerConnectionWithoutRemote(t *testing.T) {
-	b := bytes.NewBufferString("")
-	log.SetOutput(b)
 
+	// Let the connections initialize
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	for {
+		status := engine.GetPeerConnectionStatus(testPeer.WgPubKey)
+		err := ctx.Err()
+		if (status != nil && *status == StatusConnecting) || err != nil {
+			if err != nil {
+				t.Fatal(err)
+			}
+			break
+		}
+	}
+
+	// Let the connections close
 	err := engine.RemovePeerConnection(testPeer)
 	if err != nil {
 		t.Fatal(err)
 	}
-	// Let the connections initialize
-	if _, exists := engine.conns[testPeer.WgPubKey]; exists {
-		t.Fatal("couldn't remove peer")
-	}
-	// Let the connections close
-	time.Sleep(100 * time.Millisecond)
-	out, err := ioutil.ReadAll(b)
-	if err != nil {
-		t.Fatal(err)
-	}
-	expectedMSG := "removing connection attempt with Peer: " + testPeer.WgPubKey + ", not retrying"
-	if !strings.Contains(string(out), expectedMSG) {
-		t.Fatalf("expected \"%s\" got \"%s\"", expectedMSG, string(out))
+
+	status := engine.GetPeerConnectionStatus(testPeer.WgPubKey)
+	if status != nil {
+		t.Fatal(fmt.Errorf("wrong status %v", status))
 	}
 }
 
