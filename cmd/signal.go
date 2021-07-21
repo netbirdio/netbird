4671d6b33c75cf7362dc52357e8292847d63d5c4
braginini <hello@wiretrustee.com>
2021-07-21 13:18:32 +0200

    chore: add Signal keep alive params and policy

--- cmd/signal.go
+++ cmd/signal.go
@@ -8,11 +8,25 @@ import (
 	sig "github.com/wiretrustee/wiretrustee/signal"
 	sigProto "github.com/wiretrustee/wiretrustee/signal/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 	"net"
+	"time"
 )
 
 var (
 	signalPort int
+
+	signalKaep = grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
+		MinTime:             5 * time.Second,
+		PermitWithoutStream: true,
+	})
+
+	signalKasp = grpc.KeepaliveParams(keepalive.ServerParameters{
+		MaxConnectionIdle:     15 * time.Second,
+		MaxConnectionAgeGrace: 5 * time.Second,
+		Time:                  5 * time.Second,
+		Timeout:               2 * time.Second,
+	})
 
 	signalCmd = &cobra.Command{
 		Use:   "signal",
@@ -28,7 +42,9 @@ var (
 			if err != nil {
 				log.Fatalf("failed to listen: %v", err)
 			}
-			var opts []grpc.ServerOption
+
+			opts := []grpc.ServerOption{signalKaep, signalKasp}
+
 			grpcServer := grpc.NewServer(opts...)
 			sigProto.RegisterSignalExchangeServer(grpcServer, sig.NewServer())
 			log.Printf("started server: localhost:%v", signalPort)
