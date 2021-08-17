aa9835e92062010afcd228874d6797b3445c3c66
braginini <bangvalo@gmail.com>
2021-08-17 14:42:17 +0200

    test: validate generated config in login cmd

--- client/cmd/login_test.go
+++ client/cmd/login_test.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"bytes"
 	"fmt"
+	"github.com/wiretrustee/wiretrustee/client/internal"
+	"github.com/wiretrustee/wiretrustee/iface"
 	mgmtProto "github.com/wiretrustee/wiretrustee/management/proto"
 	mgmt "github.com/wiretrustee/wiretrustee/management/server"
 	"github.com/wiretrustee/wiretrustee/util"
@@ -17,13 +19,13 @@ var serverAddr string
 
 func Test_Start(t *testing.T) {
 	config := &mgmt.Config{}
-	_, err := util.ReadJson("../../management/server/testdata/management.json", config)
+	_, err := util.ReadJson("../testdata/management.json", config)
 	if err != nil {
 		t.Fatal(err)
 	}
 	testDir := t.TempDir()
 	config.Datadir = testDir
-	err = util.CopyFileContents("../../management/server/testdata/store.json", filepath.Join(testDir, "store.json"))
+	err = util.CopyFileContents("../testdata/store.json", filepath.Join(testDir, "store.json"))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -34,19 +36,19 @@ func Test_Start(t *testing.T) {
 func TestLogin_NoExistingConfig(t *testing.T) {
 
 	tempDir := t.TempDir()
-
+	confPath := tempDir + "/config.json"
+	mgmtURL := fmt.Sprintf("http://%s", serverAddr)
 	b := bytes.NewBufferString("")
 	rootCmd.SetOut(b)
 	rootCmd.SetErr(b)
 	rootCmd.SetArgs([]string{
 		"login",
 		"--config",
-		tempDir + "/config.json",
+		confPath,
 		"--setup-key",
 		"a2c8e62b-38f5-4553-b31e-dd66c696cebb",
-		tempDir + "/config.json",
 		"--management-url",
-		fmt.Sprintf("http://%s", serverAddr),
+		mgmtURL,
 	})
 	err := rootCmd.Execute()
 	if err != nil {
@@ -56,6 +58,26 @@ func TestLogin_NoExistingConfig(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// validate generated config
+	actualConf := &internal.Config{}
+	_, err = util.ReadJson(confPath, actualConf)
+	if err != nil {
+		t.Errorf("expected proper config file written, got broken %v", err)
+	}
+
+	if actualConf.ManagementURL.String() != mgmtURL {
+		t.Errorf("expected management URL %s got %s", mgmtURL, actualConf.ManagementURL.String())
+	}
+
+	if actualConf.WgIface != iface.WgInterfaceDefault {
+		t.Errorf("expected WgIface %s got %s", iface.WgInterfaceDefault, actualConf.WgIface)
+	}
+
+	if len(actualConf.PrivateKey) == 0 {
+		t.Errorf("expected non empty Private key, got empty")
+	}
+
 }
 
 func startManagement(config *mgmt.Config, t *testing.T) (*grpc.Server, net.Listener) {
