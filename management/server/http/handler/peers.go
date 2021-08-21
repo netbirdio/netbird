f4bdfc0f77181e26b8d029cecb2d4a4e629f8321
braginini <bangvalo@gmail.com>
2021-08-21 17:00:49 +0200

    feature: add peer update HTTP endpoint

--- management/server/http/handler/peers.go
+++ management/server/http/handler/peers.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 	"github.com/wiretrustee/wiretrustee/management/server"
@@ -8,18 +9,23 @@ import (
 	"time"
 )
 
-// Peers is a handler that returns peers of the account
+//Peers is a handler that returns peers of the account
 type Peers struct {
 	accountManager *server.AccountManager
 }
 
-// PeerResponse is a response sent to the client
+//PeerResponse is a response sent to the client
 type PeerResponse struct {
 	Name      string
 	IP        string
 	Connected bool
 	LastSeen  time.Time
 	OS        string
+}
+
+//PeerRequest is a request sent by the client
+type PeerRequest struct {
+	Name string
 }
 
 func NewPeers(accountManager *server.AccountManager) *Peers {
@@ -50,6 +56,21 @@ func (h *Peers) HandlePeer(w http.ResponseWriter, r *http.Request) {
 			http.Redirect(w, r, "/", http.StatusInternalServerError)
 			return
 		}
+		return
+	case http.MethodPost:
+		req := &PeerRequest{}
+		err = json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		peer, err = h.accountManager.RenamePeer(accountId, peer.Key, req.Name)
+		if err != nil {
+			log.Errorf("failed updating peer %s, %v", peerId, err)
+			http.Redirect(w, r, "/", http.StatusInternalServerError)
+			return
+		}
+		writeJSONObject(w, toPeerResponse(peer))
 		return
 	case http.MethodGet:
 		writeJSONObject(w, toPeerResponse(peer))
