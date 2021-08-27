c07b4ed2952b9c66322f29585cbe863cd9b0f0c5
braginini <bangvalo@gmail.com>
2021-08-27 11:34:38 +0200

    chore: adjust system info discovery methods

---
+++ client/system/info.go
@@ -0,0 +1,14 @@
+package system
+
+//Info is an object that contains machine information
+// Most of the code is taken from https://github.com/matishsiao/goInfo
+type Info struct {
+	GoOS      string
+	Kernel    string
+	Core      string
+	Platform  string
+	OS        string
+	OSVersion string
+	Hostname  string
+	CPUs      int
+}
