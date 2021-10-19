e924a6f0536d6d5a24db7a7393ec8c32ffa3d586
mlsmaycon <mlsmaycon@gmail.com>
2021-10-19 21:29:17 +0200

    ensure the steps will run only on remove

--- release_files/pre_remove.sh
+++ release_files/pre_remove.sh
@@ -8,23 +8,36 @@ else
     systemd_version=$(systemctl --version | head -1 | sed 's/systemd //g')
 fi
 
-printf "\033[32m Pre uninstall\033[0m\n"
+remove() {
+  printf "\033[32m Pre uninstall\033[0m\n"
 
-if [ "${use_systemctl}" = "True" ]; then
-  printf "\033[32m Stopping the service\033[0m\n"
-  systemctl stop wiretrustee || true
+  if [ "${use_systemctl}" = "True" ]; then
+    printf "\033[32m Stopping the service\033[0m\n"
+    systemctl stop wiretrustee || true
 
-  if [ -e /lib/systemd/system/wiretrustee.service ]; then
-    rm -f /lib/systemd/system/wiretrustee.service
-    systemctl daemon-reload
+    if [ -e /lib/systemd/system/wiretrustee.service ]; then
+      rm -f /lib/systemd/system/wiretrustee.service
+      systemctl daemon-reload || true
+    fi
+
   fi
-
-fi
-printf "\033[32m Uninstalling the service\033[0m\n"
-/usr/local/bin/wiretrustee service uninstall || true
+  printf "\033[32m Uninstalling the service\033[0m\n"
+  /usr/local/bin/wiretrustee service uninstall || true
 
 
-if [ "${use_systemctl}" = "True" ]; then
-   printf "\n\033[32m running daemon reload\033[0m\n"
-   systemctl daemon-reload
-fi
+  if [ "${use_systemctl}" = "True" ]; then
+     printf "\n\033[32m running daemon reload\033[0m\n"
+     systemctl daemon-reload || true
+  fi
+}
+
+action="$1"
+
+case "$action" in
+  "0" | "remove")
+    remove
+    ;;
+  *)
+    exit 0
+    ;;
+esac
