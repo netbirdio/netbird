c9b6c8640565df392b3c479f9240a62d31755dea
mlsmaycon <mlsmaycon@gmail.com>
2022-01-12 01:29:20 +0100

    test wireguard-windows driver package

--- iface/iface_windows.go
+++ iface/iface_windows.go
@@ -1,23 +1,44 @@
 package iface
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/sys/windows"
 	"golang.zx2c4.com/wireguard/ipc"
-	"golang.zx2c4.com/wireguard/tun"
+
+	//"golang.zx2c4.com/wireguard/ipc"
+	//"golang.zx2c4.com/wireguard/tun"
+	"golang.zx2c4.com/wireguard/windows/driver"
 	"golang.zx2c4.com/wireguard/windows/tunnel/winipcfg"
 	"net"
 )
 
+var (
+	WintunTunnelType          = "WireGuard"
+	WintunStaticRequestedGUID *windows.GUID
+	luid                      winipcfg.LUID
+)
+
 // Create Creates a new Wireguard interface, sets a given IP and brings it up.
 func Create(iface string, address string) error {
-	return CreateWithUserspace(iface, address)
+	adapter, err := driver.CreateAdapter(iface, WintunTunnelType, WintunStaticRequestedGUID)
+	if err != nil {
+		err = fmt.Errorf("error creating adapter: %w", err)
+		return err
+	}
+	luid = adapter.LUID()
+	err = adapter.SetAdapterState(driver.AdapterStateUp)
+	if err != nil {
+		return err
+	}
+	return assignAddr(iface, address)
 }
 
 // assignAddr Adds IP address to the tunnel interface and network route based on the range provided
 func assignAddr(address string, ifaceName string) error {
 
-	nativeTunDevice := tunIface.(*tun.NativeTun)
-	luid := winipcfg.LUID(nativeTunDevice.LUID())
+	//nativeTunDevice := tunIface.(*tun.NativeTun)
+	//luid := winipcfg.LUID(luid)
 
 	ip, ipnet, _ := net.ParseCIDR(address)
 
