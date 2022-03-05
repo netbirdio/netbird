071b03e790dabfff92b7ddbf1adc468fc6c1e7a3
Maycon Santos <mlsmaycon@gmail.com>
2022-03-05 11:20:04 +0100

    Updated self-hosted scripts and documentation (#249)
    * Updated self-hosted scripts and documentation
    
    Added more variables to setup.env and
    Updated the documentation.
    
    We are now configuring turn server
    with template as well.
    
    * Updated self-hosted scripts and documentation
    
    Added more variables to setup.env and
    Updated the documentation.
    
    We are now configuring turn server
    with template as well.
    
    * Updated self-hosted scripts and documentation
    
    Added more variables to setup.env and
    Updated the documentation.
    
    We are now configuring turn server
    with template as well.
    
    * Updated self-hosted scripts and documentation
    
    Added more variables to setup.env and
    Updated the documentation.
    
    We are now configuring turn server
    with template as well.

--- infrastructure_files/configure.sh
+++ infrastructure_files/configure.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-unset $(grep -v '^#' ./setup.env | sed -E 's/(.*)=.*/\1/' | xargs)
-export $(grep -v '^#' ./setup.env | xargs)
+source setup.env
+
+if [[ "x-$WIRETRUSTEE_DOMAIN" == "x-" ]]
+then
+  echo WIRETRUSTEE_DOMAIN is not set, please update your setup.env file
+  exit 1
+fi
+
+# local development or tests
+if [[ $WIRETRUSTEE_DOMAIN == "localhost" || $WIRETRUSTEE_DOMAIN == "127.0.0.1" ]]
+then
+  export WIRETRUSTEE_MGMT_API_ENDPOINT=http://$WIRETRUSTEE_DOMAIN:$WIRETRUSTEE_MGMT_API_PORT
+  unset WIRETRUSTEE_MGMT_API_CERT_FILE
+  unset WIRETRUSTEE_MGMT_API_CERT_KEY_FILE
+fi
+
+# if not provided, we generate a turn password
+if [[ "x-$TURN_PASSWORD" == "x-" ]]
+then
+  export TURN_PASSWORD=$(openssl rand -base64 32|sed 's/=//g')
+fi
 
 envsubst < docker-compose.yml.tmpl > docker-compose.yml
 envsubst < management.json.tmpl > management.json
+envsubst < turnserver.conf.tmpl > turnserver.conf
