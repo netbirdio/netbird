73e53c6b8397133888e19cf36dcb3e2f0927f981
Givi Khojanashvili <gigovich@gmail.com>
2022-05-02 20:25:32 +0400

    chore(management): add tests for save group

--- management/server/http/handler/groups_test.go
+++ management/server/http/handler/groups_test.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/gorilla/mux"
@@ -20,6 +22,9 @@ func initGroupTestData(groups ...*server.Group) *Groups {
 	return &Groups{
 		accountManager: &mock_server.MockAccountManager{
 			SaveGroupFunc: func(accountID string, group *server.Group) error {
+				if !strings.HasPrefix(group.ID, "id-") {
+					group.ID = "id-was-set"
+				}
 				return nil
 			},
 			GetGroupFunc: func(_, groupID string) (*server.Group, error) {
@@ -116,6 +121,82 @@ func TestGetGroup(t *testing.T) {
 
 			assert.Equal(t, got.ID, group.ID)
 			assert.Equal(t, got.Name, group.Name)
+		})
+	}
+}
+
+func TestSaveGroup(t *testing.T) {
+	tt := []struct {
+		name           string
+		expectedStatus int
+		expectedBody   bool
+		expectedGroup  *server.Group
+		requestType    string
+		requestPath    string
+		requestBody    io.Reader
+	}{
+		{
+			name:        "SaveGroup POST OK",
+			requestType: http.MethodPost,
+			requestPath: "/api/groups",
+			requestBody: bytes.NewBuffer(
+				[]byte(`{"Name":"Default POSTed Group"}`)),
+			expectedStatus: http.StatusOK,
+			expectedBody:   true,
+			expectedGroup: &server.Group{
+				ID:   "id-was-set",
+				Name: "Default POSTed Group",
+			},
+		},
+		{
+			name:        "SaveGroup PUT OK",
+			requestType: http.MethodPut,
+			requestPath: "/api/groups",
+			requestBody: bytes.NewBuffer(
+				[]byte(`{"ID":"id-existed","Name":"Default POSTed Group"}`)),
+			expectedStatus: http.StatusOK,
+			expectedGroup: &server.Group{
+				ID:   "id-existed",
+				Name: "Default POSTed Group",
+			},
+		},
+	}
+
+	p := initGroupTestData()
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			recorder := httptest.NewRecorder()
+			req := httptest.NewRequest(tc.requestType, tc.requestPath, tc.requestBody)
+
+			router := mux.NewRouter()
+			router.HandleFunc("/api/groups", p.CreateOrUpdateGroupHandler).Methods("PUT", "POST")
+			router.ServeHTTP(recorder, req)
+
+			res := recorder.Result()
+			defer res.Body.Close()
+
+			content, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatalf("I don't know what I expected; %v", err)
+			}
+
+			if status := recorder.Code; status != tc.expectedStatus {
+				t.Errorf("handler returned wrong status code: got %v want %v, content: %s",
+					status, tc.expectedStatus, string(content))
+				return
+			}
+
+			if !tc.expectedBody {
+				return
+			}
+
+			got := &server.Group{}
+			if err = json.Unmarshal(content, &got); err != nil {
+				t.Fatalf("Sent content is not in correct json format; %v", err)
+			}
+
+			assert.Equal(t, got, tc.expectedGroup)
 		})
 	}
 }
