4c4c40026328d4f97b8ff7e1f77aeeed636f2997
braginini <bangvalo@gmail.com>
2022-05-12 21:37:56 +0200

    Improve output of a status command

--- client/cmd/status.go
+++ client/cmd/status.go
@@ -40,7 +40,8 @@ var statusCmd = &cobra.Command{
 
 		if resp.GetStatus() == string(internal.StatusNeedsLogin) || resp.GetStatus() == string(internal.StatusLoginFailed) {
 			cmd.Printf("Status: %s\n\n", resp.GetStatus())
-			cmd.Printf("Run the \"netbird up\" command to log in.\n" +
+			cmd.Printf("Run UP command to log in with SSO (interactive login):\n\n" +
+				" netbird up \n\n" +
 				"If you are running a self-hosted version and no SSO provider has been configured in your Management Server,\n" +
 				"you can use a setup-key:\n\n netbird up --management-url <YOUR_MANAGEMENT_URL> --setup-key <YOUR_SETUP_KEY>\n\n" +
 				"More info: https://www.netbird.io/docs/overview/setup-keys\n\n")
