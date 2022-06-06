c4134d872239d761fd2181fd69a86534f9b8ebac
mlsmaycon <mlsmaycon@gmail.com>
2022-06-06 20:23:10 +0200

    Add OpenAPI generate script

---
+++ management/server/http/api/generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+which realpath > /dev/null 2>&1
+if [ $? -gt 0 ]
+then
+  echo realpath is not installed
+  echo run: brew install coreutils
+  exit 1
+fi
+
+old_pwd=$(pwd)
+script_path=$(dirname "$(realpath $0)")
+cd $script_path
+go install github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest
+oapi-codegen --config cfg.yaml openapi.yml
+cd $old_pwd
