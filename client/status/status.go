f8d3267db9a18b72024824bad3a963262876439f
mlsmaycon <mlsmaycon@gmail.com>
2022-06-21 17:53:55 +0200

    Client Status package

---
+++ client/status/status.go
@@ -0,0 +1,76 @@
+package status
+
+import (
+	"github.com/pion/ice/v2"
+	"sync"
+	"time"
+)
+
+type PeerState struct {
+	IP               string
+	PubKey           string
+	ConnStatus       string
+	ConnStatusUpdate time.Time
+	Relayed          bool
+	Direct           bool
+	IceCandidateType ice.CandidateType
+}
+
+type SignalState struct {
+	URL       string
+	Connected bool
+}
+
+type ManagementState struct {
+	URL       string
+	Connected bool
+}
+
+type FullStatus struct {
+	Peers           []PeerState
+	ManagementState ManagementState
+	SignalState     SignalState
+}
+
+type Status struct {
+	mux        sync.Mutex
+	Peers      map[string]PeerState
+	Signal     SignalState
+	Management ManagementState
+}
+
+func NewStatus() *Status {
+	return &Status{
+		Peers: make(map[string]PeerState),
+	}
+}
+
+// AddPeer adds peer to Daemon status map
+func (d *Status) AddPeer(peerState PeerState) error {
+	return nil
+}
+
+// RemovePeer removes peer from Daemon status map
+func (d *Status) RemovePeer(peerState PeerState) error {
+	return nil
+}
+
+// UpdatePeerStatus updates peer status
+func (d *Status) UpdatePeerStatus(peerState PeerState) error {
+	return nil
+}
+
+// UpdateSignalStatus updates signal status
+func (d *Status) UpdateSignalStatus(signalState SignalState) error {
+	return nil
+}
+
+// UpdateManagementStatus updates management status
+func (d *Status) UpdateManagementStatus(managementState ManagementState) error {
+	return nil
+}
+
+// GetStatus gets full status
+func (d *Status) GetStatus() FullStatus {
+	return FullStatus{}
+}
