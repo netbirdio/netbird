ae500b63a746c0c3e8f02f375da2b02721d9370d
Maycon Santos <mlsmaycon@gmail.com>
2022-11-29 11:49:18 +0100

    User custom loopback address (#589)
    We will probe a set of addresses and port
    to define the one available for our DNS service
    
    if none is available, we return an error

--- client/internal/engine_test.go
+++ client/internal/engine_test.go
@@ -71,6 +71,10 @@ func TestEngine_SSH(t *testing.T) {
 		WgPort:       33100,
 	}, nbstatus.NewRecorder())
 
+	engine.dnsServer = &dns.MockServer{
+		UpdateDNSServerFunc: func(serial uint64, update nbdns.Config) error { return nil },
+	}
+
 	var sshKeysAdded []string
 	var sshPeersRemoved []string
 
@@ -384,6 +388,10 @@ func TestEngine_Sync(t *testing.T) {
 		WgPrivateKey: key,
 		WgPort:       33100,
 	}, nbstatus.NewRecorder())
+
+	engine.dnsServer = &dns.MockServer{
+		UpdateDNSServerFunc: func(serial uint64, update nbdns.Config) error { return nil },
+	}
 
 	defer func() {
 		err := engine.Stop()
