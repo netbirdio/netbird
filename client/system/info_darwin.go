22a010deb6a0977cb6b8e283562630359f23ac73
tcskiran <63388243+tcskiran@users.noreply.github.com>
2023-01-26 14:35:26 +0530

    macos version error

--- client/system/info_darwin.go
+++ client/system/info_darwin.go
@@ -16,10 +16,15 @@ func GetInfo(ctx context.Context) *Info {
 	if err != nil {
 		fmt.Println("getInfo:", err)
 	}
+	out, err := exec.Command("sw_vers", "-productVersion").Output()
+	if err != nil {
+		fmt.Println("getInfo, macOS version error", err)
+	}
+	version := strings.TrimSpace(string(out))
 	sysName := string(bytes.Split(utsname.Sysname[:], []byte{0})[0])
 	machine := string(bytes.Split(utsname.Machine[:], []byte{0})[0])
 	release := string(bytes.Split(utsname.Release[:], []byte{0})[0])
-	gio := &Info{Kernel: sysName, OSVersion: release, Core: release, Platform: machine, OS: sysName, GoOS: runtime.GOOS, CPUs: runtime.NumCPU()}
+	gio := &Info{Kernel: sysName, OSVersion: release, Core: release, Platform: machine, OS: version, GoOS: runtime.GOOS, CPUs: runtime.NumCPU()}
 	gio.Hostname, _ = os.Hostname()
 	gio.WiretrusteeVersion = NetbirdVersion()
 	gio.UIVersion = extractUserAgent(ctx)
