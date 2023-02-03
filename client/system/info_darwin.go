d3c727b9d34de0369cad90323c2d07bba1e048b3
tcskiran <63388243+tcskiran@users.noreply.github.com>
2023-02-04 02:10:30 +0530

    Macos version error (#666)
    use sw_vers to retrieve the proper macOS version.

--- client/system/info_darwin.go
+++ client/system/info_darwin.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 	"os"
+	"os/exec"
 	"runtime"
+	"strings"
 )
 
 // GetInfo retrieves and parses the system information
@@ -19,7 +22,12 @@ func GetInfo(ctx context.Context) *Info {
 	sysName := string(bytes.Split(utsname.Sysname[:], []byte{0})[0])
 	machine := string(bytes.Split(utsname.Machine[:], []byte{0})[0])
 	release := string(bytes.Split(utsname.Release[:], []byte{0})[0])
-	gio := &Info{Kernel: sysName, OSVersion: release, Core: release, Platform: machine, OS: sysName, GoOS: runtime.GOOS, CPUs: runtime.NumCPU()}
+	version, err := exec.Command("sw_vers", "-productVersion").Output()
+	if err != nil {
+		log.Warnf("got an error while retrieving macOS version with sw_vers, error: %s. Using darwin version instead.\n", err)
+		version = []byte(release)
+	}
+	gio := &Info{Kernel: sysName, OSVersion: strings.TrimSpace(string(version)), Core: release, Platform: machine, OS: sysName, GoOS: runtime.GOOS, CPUs: runtime.NumCPU()}
 	gio.Hostname, _ = os.Hostname()
 	gio.WiretrusteeVersion = NetbirdVersion()
 	gio.UIVersion = extractUserAgent(ctx)
