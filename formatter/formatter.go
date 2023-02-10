56d15419f81b379f8417d5e40cd6703ce90e4064
Zoltan Papp <zoltan.pmail@gmail.com>
2023-02-10 11:54:19 +0100

    Add filed support for formatter

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -2,6 +2,7 @@ package formatter
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -23,5 +24,18 @@ func NewMyTextFormatter() *MyTextFormatter {
 
 // Format renders a single log entry
 func (f *MyTextFormatter) Format(entry *logrus.Entry) ([]byte, error) {
-	return []byte(fmt.Sprintf("%s %s %s: %s\n", entry.Time.Format(f.TimestampFormat), f.LevelDesc[entry.Level], entry.Data["source"], entry.Message)), nil
+	var fields string
+	keys := make([]string, 0, len(entry.Data))
+	for k, v := range entry.Data {
+		if k == "source" {
+			continue
+		}
+		keys = append(keys, fmt.Sprintf("%s: %v", k, v))
+	}
+
+	if len(keys) > 0 {
+		fields = fmt.Sprintf("[%s] ", strings.Join(keys, ", "))
+	}
+
+	return []byte(fmt.Sprintf("%s %s %s%s: %s\n", entry.Time.Format(f.TimestampFormat), f.LevelDesc[entry.Level], fields, entry.Data["source"], entry.Message)), nil
 }
