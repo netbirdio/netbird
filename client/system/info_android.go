270b8cd83f5957b3f9022c3bfd226dfa59cede5c
Zoltan Papp <zoltan.pmail@gmail.com>
2023-02-16 10:30:54 +0100

    Add Android specific system/info

---
+++ client/system/info_android.go
@@ -0,0 +1,54 @@
+//go:build android
+// +build android
+
+package system
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// GetInfo retrieves and parses the system information
+func GetInfo(ctx context.Context) *Info {
+	kernel := "android"
+	osInfo := uname()
+	if len(osInfo) == 2 {
+		kernel = osInfo[1]
+	}
+
+	gio := &Info{Kernel: kernel, Core: sdkVersion(), Platform: "unknown", OS: "android", OSVersion: sdkVersion(), GoOS: runtime.GOOS, CPUs: runtime.NumCPU()}
+	gio.Hostname, _ = os.Hostname()
+	gio.WiretrusteeVersion = NetbirdVersion()
+	gio.UIVersion = extractUserAgent(ctx)
+
+	return gio
+}
+
+func uname() []string {
+	res := run("/system/bin/uname", "-a")
+	return strings.Split(res, " ")
+}
+
+func sdkVersion() string {
+	return run("/system/bin/getprop", "ro.build.version.sdk")
+}
+
+func run(name string, arg ...string) string {
+	cmd := exec.Command(name, arg...)
+	cmd.Stdin = strings.NewReader("some")
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Errorf("getInfo: %s", err)
+	}
+	return out.String()
+}
