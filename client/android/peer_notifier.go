2cefc21be83bfcdd8352d185c820aca7eeac64b6
pzoli <zoltan.pmail@gmail.com>
2023-03-10 11:17:46 +0100

    Add comments to peer_notifier.go

--- client/android/peer_notifier.go
+++ client/android/peer_notifier.go
@@ -1,5 +1,6 @@
 package android
 
+// PeerInfo describe information about the peers. It designed for the UI usage
 type PeerInfo struct {
 	IP         string
 	FQDN       string
@@ -7,25 +8,30 @@ type PeerInfo struct {
 	Direct     bool
 }
 
+// PeerInfoCollection made for Java layer to get non default types as collection
 type PeerInfoCollection interface {
 	Add(s string) PeerInfoCollection
 	Get(i int) string
 	Size() int
 }
 
+// PeerInfoArray is the implementation of the PeerInfoCollection
 type PeerInfoArray struct {
 	items []PeerInfo
 }
 
+// Add new PeerInfo to the collection
 func (array PeerInfoArray) Add(s PeerInfo) PeerInfoArray {
 	array.items = append(array.items, s)
 	return array
 }
 
+// Get return an element of the collection
 func (array PeerInfoArray) Get(i int) *PeerInfo {
 	return &array.items[i]
 }
 
+// Size return with the size of the collection
 func (array PeerInfoArray) Size() int {
 	return len(array.items)
 }
