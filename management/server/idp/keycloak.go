63f072bc2ee80b686d27e2eaa7d09e062f155c28
Bethuel <bethuelmbaga12@gmail.com>
2023-03-11 21:18:31 +0300

    create new keycloak idp manager

--- management/server/idp/keycloak.go
+++ management/server/idp/keycloak.go
@@ -1,6 +1,14 @@
 package idp
 
-import "github.com/netbirdio/netbird/management/server/telemetry"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/netbirdio/netbird/management/server/telemetry"
+)
 
 // KeycloakManager keycloak manager client instance.
 type KeycloakManager struct {
@@ -13,16 +21,67 @@ type KeycloakManager struct {
 
 // KeycloakClientConfig keycloak manager client configurations.
 type KeycloakClientConfig struct {
-	Audience     string
-	AuthIssuer   string
-	ClientID     string
-	ClientSecret string
-	GrantType    string
+	Audience      string
+	AuthIssuer    string
+	ClientID      string
+	ClientSecret  string
+	TokenEndpoint string
+	GrantType     string
+}
+
+// KeycloakCredentials keycloak authentication information.
+type KeycloakCredentials struct {
+	clientConfig KeycloakClientConfig
+	helper       ManagerHelper
+	httpClient   ManagerHTTPClient
+	jwtToken     JWTToken
+	mux          sync.Mutex
+	appMetrics   telemetry.AppMetrics
 }
 
 // NewKeycloakManager creates a new instance of the KeycloakManager.
 func NewKeycloakManager(config KeycloakClientConfig, appMetrics telemetry.AppMetrics) (*KeycloakManager, error) {
-	return &KeycloakManager{}, nil
+	httpTransport := http.DefaultTransport.(*http.Transport).Clone()
+	httpTransport.MaxIdleConns = 5
+
+	httpClient := &http.Client{
+		Timeout:   10 * time.Second,
+		Transport: httpTransport,
+	}
+
+	helper := JsonParser{}
+
+	if config.ClientID == "" || config.ClientSecret == "" || config.GrantType == "" || config.Audience == "" || config.AuthIssuer == "" || config.TokenEndpoint == "" {
+		return nil, fmt.Errorf("keycloak idp configuration is not complete")
+	}
+
+	if config.GrantType != "client_credentials" {
+		return nil, fmt.Errorf("keycloak idp configuration failed. Grant Type should be client_credentials")
+	}
+
+	if !strings.HasPrefix(strings.ToLower(config.AuthIssuer), "https://") {
+		return nil, fmt.Errorf("keycloak idp configuration failed. AuthIssuer should contain https://")
+	}
+
+	credentials := &KeycloakCredentials{
+		clientConfig: config,
+		httpClient:   httpClient,
+		helper:       helper,
+		appMetrics:   appMetrics,
+	}
+
+	return &KeycloakManager{
+		authIssuer:  config.AuthIssuer,
+		httpClient:  httpClient,
+		credentials: credentials,
+		helper:      helper,
+		appMetrics:  appMetrics,
+	}, nil
+}
+
+// Authenticate retrieves access token to use the keycloak Management API.
+func (kc *KeycloakCredentials) Authenticate() (JWTToken, error) {
+	panic("not implemented")
 }
 
 // CreateUser creates a new user in Auth0 Idp and sends an invite.
