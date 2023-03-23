4c56578b0c6c64eaebee0730d8d3f178ab3f9a16
Givi Khojanashvili <gigovich@gmail.com>
2023-03-23 16:54:04 +0400

    Add default firewall rules and fix tests in the github flow.

--- client/internal/engine_test.go
+++ client/internal/engine_test.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/netip"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"runtime"
 	"strings"
@@ -945,6 +946,12 @@ func TestEngine_firewallManager(t *testing.T) {
 	if runtime.GOOS != "linux" {
 		t.Skipf("firewall manager not supported in the: %s", runtime.GOOS)
 		return
+	} else {
+		_, err := exec.LookPath("iptables")
+		if err != nil {
+			t.Skipf("iptables not found: %v", err)
+			return
+		}
 	}
 
 	ctx, cancel := context.WithTimeout(CtxInitState(context.Background()), 10*time.Second)
