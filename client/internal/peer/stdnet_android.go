d3c49c71f2b71c29c3c35de6eaf9b8456e593709
Zoltan Papp <zoltan.pmail@gmail.com>
2023-04-05 21:23:13 +0200

    Fix config reference in stdnet_android

--- client/internal/peer/stdnet_android.go
+++ client/internal/peer/stdnet_android.go
@@ -3,5 +3,5 @@ package peer
 import "github.com/netbirdio/netbird/client/internal/stdnet"
 
 func (conn *Conn) newStdNet() (*stdnet.Net, error) {
-	return stdnet.NewNet(conn.iFaceDiscover, e.config.IFaceBlackList)
+	return stdnet.NewNet(conn.iFaceDiscover, conn.config.InterfaceBlackList)
 }
