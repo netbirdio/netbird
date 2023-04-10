e197b89ac3c313e35a5a79fbf1a0bd9ec2e91ba6
Pascal Fischer <pascal@netbird.io>
2023-04-10 11:09:27 +0200

    remove UTC from some not store related operations

--- client/cmd/status.go
+++ client/cmd/status.go
@@ -249,7 +249,7 @@ func mapPeers(peers []*proto.PeerState) peersStateOutput {
 			IP:               pbPeerState.GetIP(),
 			PubKey:           pbPeerState.GetPubKey(),
 			Status:           pbPeerState.GetConnStatus(),
-			LastStatusUpdate: timeLocal,
+			LastStatusUpdate: timeLocal.UTC(),
 			ConnType:         connType,
 			Direct:           pbPeerState.GetDirect(),
 			IceCandidateType: iceCandidateType{
