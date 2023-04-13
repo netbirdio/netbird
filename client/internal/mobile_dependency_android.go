8543b71a3f431b5988439069e23699440c52ea51
Zoltan Papp <zoltan.pmail@gmail.com>
2023-04-13 12:10:51 +0200

    Fix imports and build flag

--- client/internal/mobile_dependency_android.go
+++ client/internal/mobile_dependency_android.go
@@ -1,5 +1,11 @@
 package internal
 
+import (
+	"github.com/netbirdio/netbird/client/internal/stdnet"
+	"github.com/netbirdio/netbird/iface"
+	mgm "github.com/netbirdio/netbird/management/client"
+)
+
 func newMobileDependency(tunAdapter iface.TunAdapter, ifaceDiscover stdnet.IFaceDiscover, mgmClient *mgm.GrpcClient) (MobileDependency, error) {
 	md := MobileDependency{
 		TunAdapter:    tunAdapter,
