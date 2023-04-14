9f352c1b7e3c771495a767375af9f5ba1dfd4d62
Bethuel <bethuelmbaga12@gmail.com>
2023-04-14 12:20:34 +0300

    validate keys for idp's with key rotation mechanism

--- management/server/config.go
+++ management/server/config.go
@@ -80,6 +80,8 @@ type HttpServerConfig struct {
 	AuthKeysLocation string
 	// OIDCConfigEndpoint is the endpoint of an IDP manager to get OIDC configuration
 	OIDCConfigEndpoint string
+	// KeyRotationEnabled identifies the signing key is currently being rotated or not
+	KeyRotationEnabled bool
 }
 
 // Host represents a Wiretrustee host (e.g. STUN, TURN, Signal)
