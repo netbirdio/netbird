35b6e83c2a0729916655745b3ce14a586d456304
Zoltan Papp <zoltan.pmail@gmail.com>
2023-04-20 10:39:20 +0200

    Fix error handling in controlfns_linux.go

--- iface/bind/controlfns_linux.go
+++ iface/bind/controlfns_linux.go
@@ -18,24 +18,27 @@ func init() {
 		// Enable receiving of the packet information (IP_PKTINFO for IPv4,
 		// IPV6_PKTINFO for IPv6) that is used to implement sticky socket support.
 		func(network, address string, c syscall.RawConn) error {
-			var err error
+			var errSocketOpt, errCtrl error
 			switch network {
 			case "udp4":
-				c.Control(func(fd uintptr) {
-					err = unix.SetsockoptInt(int(fd), unix.IPPROTO_IP, unix.IP_PKTINFO, 1)
+				errCtrl = c.Control(func(fd uintptr) {
+					errSocketOpt = unix.SetsockoptInt(int(fd), unix.IPPROTO_IP, unix.IP_PKTINFO, 1)
 				})
 			case "udp6":
-				c.Control(func(fd uintptr) {
-					err = unix.SetsockoptInt(int(fd), unix.IPPROTO_IPV6, unix.IPV6_RECVPKTINFO, 1)
-					if err != nil {
+				errCtrl = c.Control(func(fd uintptr) {
+					errSocketOpt = unix.SetsockoptInt(int(fd), unix.IPPROTO_IPV6, unix.IPV6_RECVPKTINFO, 1)
+					if errSocketOpt != nil {
 						return
 					}
-					err = unix.SetsockoptInt(int(fd), unix.IPPROTO_IPV6, unix.IPV6_V6ONLY, 1)
+					errSocketOpt = unix.SetsockoptInt(int(fd), unix.IPPROTO_IPV6, unix.IPV6_V6ONLY, 1)
 				})
 			default:
-				err = fmt.Errorf("unhandled network: %s: %w", network, unix.EINVAL)
+				errSocketOpt = fmt.Errorf("unhandled network: %s: %w", network, unix.EINVAL)
 			}
-			return err
+			if errSocketOpt != nil {
+				return errSocketOpt
+			}
+			return errCtrl
 		},
 	)
 }
