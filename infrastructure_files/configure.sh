58098036c978a8ac2369d8231ceff94d377d5fa9
Bethuel <bethuelmbaga12@gmail.com>
2023-05-09 11:50:38 +0300

    refactoring

--- infrastructure_files/configure.sh
+++ infrastructure_files/configure.sh
@@ -2,35 +2,30 @@
 
 source zitadel.sh
 
-if ! which curl > /dev/null 2>&1
-then
-    echo "This script uses curl fetch OpenID configuration from IDP."
-    echo "Please install curl and re-run the script https://curl.se/"
-    echo ""
-    exit 1
+if ! which curl >/dev/null 2>&1; then
+  echo "This script uses curl fetch OpenID configuration from IDP."
+  echo "Please install curl and re-run the script https://curl.se/"
+  echo ""
+  exit 1
 fi
 
-if ! which jq > /dev/null 2>&1
-then
-    echo "This script uses jq to load OpenID configuration from IDP."
-    echo "Please install jq and re-run the script https://stedolan.github.io/jq/"
-    echo ""
-    exit 1
+if ! which jq >/dev/null 2>&1; then
+  echo "This script uses jq to load OpenID configuration from IDP."
+  echo "Please install jq and re-run the script https://stedolan.github.io/jq/"
+  echo ""
+  exit 1
 fi
 
 source setup.env
 source base.setup.env
 
-if ! which envsubst > /dev/null 2>&1
-then
+if ! which envsubst >/dev/null 2>&1; then
   echo "envsubst is needed to run this script"
-  if [[ $(uname) == "Darwin" ]]
-  then
+  if [[ $(uname) == "Darwin" ]]; then
     echo "you can install it with homebrew (https://brew.sh):"
     echo "brew install gettext"
   else
-    if which apt-get > /dev/null 2>&1
-    then
+    if which apt-get >/dev/null 2>&1; then
       echo "you can install it by running"
       echo "apt-get update && apt-get install gettext-base"
     else
@@ -40,8 +35,7 @@ then
   exit 1
 fi
 
-if [[ "x-$NETBIRD_DOMAIN" == "x-" ]]
-then
+if [[ "x-$NETBIRD_DOMAIN" == "x-" ]]; then
   echo NETBIRD_DOMAIN is not set, please update your setup.env file
   echo If you are migrating from old versions, you migh need to update your variables prefixes from
   echo WIRETRUSTEE_.. TO NETBIRD_
@@ -49,8 +43,7 @@ then
 fi
 
 # local development or tests
-if [[ $NETBIRD_DOMAIN == "localhost" || $NETBIRD_DOMAIN == "127.0.0.1" ]]
-then
+if [[ $NETBIRD_DOMAIN == "localhost" || $NETBIRD_DOMAIN == "127.0.0.1" ]]; then
   export NETBIRD_MGMT_SINGLE_ACCOUNT_MODE_DOMAIN="netbird.selfhosted"
   export NETBIRD_MGMT_API_ENDPOINT=http://$NETBIRD_DOMAIN:$NETBIRD_MGMT_API_PORT
   unset NETBIRD_MGMT_API_CERT_FILE
@@ -58,9 +51,8 @@ then
 fi
 
 # if not provided, we generate a turn password
-if [[ "x-$TURN_PASSWORD" == "x-" ]]
-then
-  export TURN_PASSWORD=$(openssl rand -base64 32|sed 's/=//g')
+if [[ "x-$TURN_PASSWORD" == "x-" ]]; then
+  export TURN_PASSWORD=$(openssl rand -base64 32 | sed 's/=//g')
 fi
 
 MGMT_VOLUMENAME="${VOLUME_PREFIX}${MGMT_VOLUMESUFFIX}"
@@ -69,13 +61,13 @@ LETSENCRYPT_VOLUMENAME="${VOLUME_PREFIX}${LETSENCRYPT_VOLUMESUFFIX}"
 # if volume with wiretrustee- prefix already exists, use it, else create new with netbird-
 OLD_PREFIX='wiretrustee-'
 if docker volume ls | grep -q "${OLD_PREFIX}${MGMT_VOLUMESUFFIX}"; then
-    MGMT_VOLUMENAME="${OLD_PREFIX}${MGMT_VOLUMESUFFIX}"
+  MGMT_VOLUMENAME="${OLD_PREFIX}${MGMT_VOLUMESUFFIX}"
 fi
 if docker volume ls | grep -q "${OLD_PREFIX}${SIGNAL_VOLUMESUFFIX}"; then
-    SIGNAL_VOLUMENAME="${OLD_PREFIX}${SIGNAL_VOLUMESUFFIX}"
+  SIGNAL_VOLUMENAME="${OLD_PREFIX}${SIGNAL_VOLUMESUFFIX}"
 fi
 if docker volume ls | grep -q "${OLD_PREFIX}${LETSENCRYPT_VOLUMESUFFIX}"; then
-    LETSENCRYPT_VOLUMENAME="${OLD_PREFIX}${LETSENCRYPT_VOLUMESUFFIX}"
+  LETSENCRYPT_VOLUMENAME="${OLD_PREFIX}${LETSENCRYPT_VOLUMESUFFIX}"
 fi
 
 export MGMT_VOLUMENAME
@@ -85,47 +77,45 @@ export LETSENCRYPT_VOLUMENAME
 #backwards compatibility after migrating to generic OIDC with Auth0
 if [[ -z "${NETBIRD_AUTH_OIDC_CONFIGURATION_ENDPOINT}" ]]; then
 
-    if [[ -z "${NETBIRD_AUTH0_DOMAIN}" ]]; then
-       # not a backward compatible state
-       echo "NETBIRD_AUTH_OIDC_CONFIGURATION_ENDPOINT property must be set in the setup.env file"
-       exit 1
-    fi
+  if [[ -z "${NETBIRD_AUTH0_DOMAIN}" ]]; then
+    # not a backward compatible state
+    echo "NETBIRD_AUTH_OIDC_CONFIGURATION_ENDPOINT property must be set in the setup.env file"
+    exit 1
+  fi
 
-    echo "It seems like you provided an old setup.env file."
-    echo "Since the release of v0.8.10, we introduced a new set of properties."
-    echo "The script is backward compatible and will continue automatically."
-    echo "In the future versions it will be deprecated. Please refer to the documentation to learn about the changes http://netbird.io/docs/getting-started/self-hosting"
+  echo "It seems like you provided an old setup.env file."
+  echo "Since the release of v0.8.10, we introduced a new set of properties."
+  echo "The script is backward compatible and will continue automatically."
+  echo "In the future versions it will be deprecated. Please refer to the documentation to learn about the changes http://netbird.io/docs/getting-started/self-hosting"
 
-    export NETBIRD_AUTH_OIDC_CONFIGURATION_ENDPOINT="https://${NETBIRD_AUTH0_DOMAIN}/.well-known/openid-configuration"
-    export NETBIRD_USE_AUTH0="true"
-    export NETBIRD_AUTH_AUDIENCE=${NETBIRD_AUTH0_AUDIENCE}
-    export NETBIRD_AUTH_CLIENT_ID=${NETBIRD_AUTH0_CLIENT_ID}
+  export NETBIRD_AUTH_OIDC_CONFIGURATION_ENDPOINT="https://${NETBIRD_AUTH0_DOMAIN}/.well-known/openid-configuration"
+  export NETBIRD_USE_AUTH0="true"
+  export NETBIRD_AUTH_AUDIENCE=${NETBIRD_AUTH0_AUDIENCE}
+  export NETBIRD_AUTH_CLIENT_ID=${NETBIRD_AUTH0_CLIENT_ID}
 fi
 
 echo "loading OpenID configuration from ${NETBIRD_AUTH_OIDC_CONFIGURATION_ENDPOINT} to the openid-configuration.json file"
 curl "${NETBIRD_AUTH_OIDC_CONFIGURATION_ENDPOINT}" -q -o openid-configuration.json
 
-export NETBIRD_AUTH_AUTHORITY=$( jq -r  '.issuer' openid-configuration.json )
-export NETBIRD_AUTH_JWT_CERTS=$( jq -r  '.jwks_uri' openid-configuration.json )
-export NETBIRD_AUTH_SUPPORTED_SCOPES=$( jq -r '.scopes_supported | join(" ")' openid-configuration.json )
-export NETBIRD_AUTH_TOKEN_ENDPOINT=$( jq -r  '.token_endpoint' openid-configuration.json )
-export NETBIRD_AUTH_DEVICE_AUTH_ENDPOINT=$( jq -r  '.device_authorization_endpoint' openid-configuration.json )
+export NETBIRD_AUTH_AUTHORITY=$(jq -r '.issuer' openid-configuration.json)
+export NETBIRD_AUTH_JWT_CERTS=$(jq -r '.jwks_uri' openid-configuration.json)
+export NETBIRD_AUTH_SUPPORTED_SCOPES=$(jq -r '.scopes_supported | join(" ")' openid-configuration.json)
+export NETBIRD_AUTH_TOKEN_ENDPOINT=$(jq -r '.token_endpoint' openid-configuration.json)
+export NETBIRD_AUTH_DEVICE_AUTH_ENDPOINT=$(jq -r '.device_authorization_endpoint' openid-configuration.json)
 
-if [ $NETBIRD_USE_AUTH0 == "true" ]
-then
-    export NETBIRD_AUTH_SUPPORTED_SCOPES="openid profile email offline_access api email_verified"
+if [ $NETBIRD_USE_AUTH0 == "true" ]; then
+  export NETBIRD_AUTH_SUPPORTED_SCOPES="openid profile email offline_access api email_verified"
 else
-    export NETBIRD_AUTH_SUPPORTED_SCOPES="openid profile email offline_access api"
+  export NETBIRD_AUTH_SUPPORTED_SCOPES="openid profile email offline_access api"
 fi
 
 if [[ ! -z "${NETBIRD_AUTH_DEVICE_AUTH_CLIENT_ID}" ]]; then
-    # user enabled Device Authorization Grant feature
-    export NETBIRD_AUTH_DEVICE_AUTH_PROVIDER="hosted"
+  # user enabled Device Authorization Grant feature
+  export NETBIRD_AUTH_DEVICE_AUTH_PROVIDER="hosted"
 fi
 
 # Check if letsencrypt was disabled
-if [[ "$NETBIRD_DISABLE_LETSENCRYPT" == "true" ]]
-then
+if [[ "$NETBIRD_DISABLE_LETSENCRYPT" == "true" ]]; then
   export NETBIRD_DASHBOARD_ENDPOINT="https://$NETBIRD_DOMAIN:443"
   export NETBIRD_SIGNAL_ENDPOINT="https://$NETBIRD_DOMAIN:$NETBIRD_SIGNAL_PORT"
 
@@ -149,17 +139,14 @@ fi
 
 env | grep NETBIRD
 
-
 MGMT_JSON_TEMPLATE="management.json.tmpl"
 
 # configure zitadel IdP
-if [[ "$NETBIRD_IDP_PROVIDER" == "zitadel" ]]
-then
-  
+if [[ "$NETBIRD_IDP_PROVIDER" == "zitadel" ]]; then
+
   configure_zitadel_instance
 
-  if [ -n "$APPLICATION_CLIENT_ID" ]
-  then
+  if [ -n "$APPLICATION_CLIENT_ID" ]; then
     export NETBIRD_AUTH_CLIENT_ID="$APPLICATION_CLIENT_ID"
     export NETBIRD_AUTH_AUDIENCE="$APPLICATION_CLIENT_ID"
     export NETBIRD_AUTH_DEVICE_AUTH_CLIENT_ID="$APPLICATION_CLIENT_ID"
@@ -172,6 +159,6 @@ then
   MGMT_JSON_TEMPLATE="management.json.zitadel.tmpl"
 fi
 
-envsubst < docker-compose.yml.tmpl > docker-compose.yml
-envsubst < turnserver.conf.tmpl > turnserver.conf
-envsubst < $MGMT_JSON_TEMPLATE > management.json
+envsubst <docker-compose.yml.tmpl >docker-compose.yml
+envsubst <turnserver.conf.tmpl >turnserver.conf
+envsubst <$MGMT_JSON_TEMPLATE >management.json
--- infrastructure_files/zitadel.sh
+++ infrastructure_files/zitadel.sh
@@ -1,17 +1,79 @@
 #!/bin/bash
 
+set -e
+
+request_jwt_token() {
+  INSTANCE_URL=$1
+  BODY="grant_type=client_credentials&scope=urn:zitadel:iam:org:project:id:zitadel:aud&client_id=$ZITADEL_CLIENT_ID&client_secret=$ZITADEL_CLIENT_SECRET"
+
+  RESPONSE=$(
+    curl -X POST "$INSTANCE_URL/oauth/v2/token" \
+      -H "Content-Type: application/x-www-form-urlencoded" \
+      -d "$BODY"
+  )
+  echo "$RESPONSE" | jq -r '.access_token'
+}
+
+create_new_project() {
+  INSTANCE_URL=$1
+  ACCESS_TOKEN=$2
+  PROJECT_NAME="NETBIRD"
+
+  RESPONSE=$(
+    curl -X POST "$INSTANCE_URL/management/v1/projects" \
+      -H "Authorization: Bearer $ACCESS_TOKEN" \
+      -H "Content-Type: application/json" \
+      -d '{"name": "'"$PROJECT_NAME"'"}'
+  )
+  echo "$RESPONSE" | jq -r '.id'
+}
+
+create_new_application() {
+  INSTANCE_URL=$1
+  ACCESS_TOKEN=$2
+  APPLICATION_NAME="netbird"
+
+  RESPONSE=$(
+    curl -X POST "$INSTANCE_URL/management/v1/projects/$PROJECT_ID/apps/oidc" \
+      -H "Authorization: Bearer $ACCESS_TOKEN" \
+      -H "Content-Type: application/json" \
+      -d '{
+    "name": "'"$APPLICATION_NAME"'",
+    "redirectUris": [
+      "'"$BASE_REDIRECT_URL"'/auth"
+    ],
+    "RESPONSETypes": [
+      "OIDC_RESPONSE_TYPE_CODE"
+    ],
+    "grantTypes": [
+      "OIDC_GRANT_TYPE_AUTHORIZATION_CODE"
+    ],
+    "appType": "OIDC_APP_TYPE_USER_AGENT",
+    "authMethodType": "OIDC_AUTH_METHOD_TYPE_NONE",
+    "postLogoutRedirectUris": [
+      "'"$BASE_REDIRECT_URL"'/silent-auth"
+    ],
+    "version": "OIDC_VERSION_1_0",
+    "devMode": '"$ZITADEL_DEV_MODE"',
+    "accessTokenType": "OIDC_TOKEN_TYPE_JWT",
+    "accessTokenRoleAssertion": true,
+    "skipNativeAppSuccessPage": true
+  }'
+  )
+  echo "$RESPONSE" | jq -r '.clientId'
+}
+
 configure_zitadel_instance() {
-   # extract zitadel instance url
+  # extract zitadel instance url
   INSTANCE_URL=$(echo "$NETBIRD_AUTH_OIDC_CONFIGURATION_ENDPOINT" | sed 's/\/\.well-known\/openid-configuration//')
   DOC_URL="https://netbird.io/docs/integrations/identity-providers/self-hosted/using-netbird-with-zitadel#step-4-create-a-service-user"
-  
+
   echo ""
   printf "configuring zitadel instance: $INSTANCE_URL \n \
   before proceeding, please create a new service account for authorization by following the instructions (step 4 and 5
-  ) in the documentation at %s\n"  "$DOC_URL"
+  ) in the documentation at %s\n" "$DOC_URL"
   echo "Please ensure that the new service account has 'Org Owner' permission in order for this to work."
   echo ""
-  
 
   read -n 1 -s -r -p "press any key to continue..."
   echo ""
@@ -25,27 +87,24 @@ configure_zitadel_instance() {
   read -r -p "enter service account ClientSecret: " ZITADEL_CLIENT_SECRET
   echo ""
 
-  PROJECT_NAME="NETBIRD"
-  APPLICATION_NAME="netbird"
- 
   # get an access token from zitadel
   echo "retrieving access token from zitadel"
-  ACCESS_TOKEN=$(curl -X POST "$INSTANCE_URL/oauth/v2/token" \
-    -H "Content-Type: application/x-www-form-urlencoded" \
-    -d "grant_type=client_credentials&scope=urn:zitadel:iam:org:project:id:zitadel:aud&client_id=$ZITADEL_CLIENT_ID&client_secret=$ZITADEL_CLIENT_SECRET" \
-    | jq -r '.access_token')
+  ACCESS_TOKEN=$(request_jwt_token "$INSTANCE_URL")
+  if [ "$ACCESS_TOKEN" = "null" ]; then
+    echo "failed requesting access token"
+    exit 1
+  fi
 
-  # create the zitadel project
-  echo "creating $PROJECT_NAME project in zitadel"
-  PROJECT_ID=$(curl -X POST "$INSTANCE_URL/management/v1/projects" \
-    -H "Authorization: Bearer $ACCESS_TOKEN" \
-    -H "Content-Type: application/json" \
-    -d '{"name": "'"$PROJECT_NAME"'"}' \
-    | jq -r '.id')
+  #  create the zitadel project
+  echo "creating new zitadel project"
+  PROJECT_ID=$(create_new_project "$INSTANCE_URL" "$ACCESS_TOKEN")
+  if [ "$PROJECT_ID" = "null" ]; then
+    echo "failed creating new zitadel project"
+    exit 1
+  fi
 
   ZITADEL_DEV_MODE=false
-  if [[ $NETBIRD_DOMAIN == *"localhost"* ]]
-  then
+  if [[ $NETBIRD_DOMAIN == *"localhost"* ]]; then
     BASE_REDIRECT_URL="http://$NETBIRD_DOMAIN"
     ZITADEL_DEV_MODE=true
   else
@@ -53,33 +112,10 @@ configure_zitadel_instance() {
   fi
 
   # create zitadel spa application
-  echo "creating $APPLICATION_NAME spa application in zitadel"
-  # shellcheck disable=SC2034
-  APPLICATION_CLIENT_ID=$(curl -X POST  "$INSTANCE_URL/management/v1/projects/$PROJECT_ID/apps/oidc" \
-    -H "Authorization: Bearer $ACCESS_TOKEN" \
-    -H "Content-Type: application/json" \
-    -d '{
-  "name": "'"$APPLICATION_NAME"'",
-  "redirectUris": [
-    "'"$BASE_REDIRECT_URL"'/auth"
-  ],
-  "responseTypes": [
-    "OIDC_RESPONSE_TYPE_CODE"
-  ],
-  "grantTypes": [
-    "OIDC_GRANT_TYPE_AUTHORIZATION_CODE"
-  ],
-  "appType": "OIDC_APP_TYPE_USER_AGENT",
-  "authMethodType": "OIDC_AUTH_METHOD_TYPE_NONE",
-  "postLogoutRedirectUris": [
-    "'"$BASE_REDIRECT_URL"'/silent-auth"
-  ],
-  "version": "OIDC_VERSION_1_0",
-  "devMode": '"$ZITADEL_DEV_MODE"',
-  "accessTokenType": "OIDC_TOKEN_TYPE_JWT",
-  "accessTokenRoleAssertion": true,
-  "skipNativeAppSuccessPage": true
-}' \
-    | jq -r '.clientId'
-)
+  echo "creating new zitadel spa application"
+  APPLICATION_CLIENT_ID=$(create_new_application "$INSTANCE_URL" "$ACCESS_TOKEN")
+  if [ "$APPLICATION_CLIENT_ID" = "null" ]; then
+    echo "failed creating new zitadel spa application"
+    exit 1
+  fi
 }
