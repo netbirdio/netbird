e27a0fcdbc6c348a599aec2ef54740c01652c23d
Givi Khojanashvili <gigovich@gmail.com>
2023-05-10 13:21:46 +0400

    Review fix

--- iface/tun_wrapper_unix.go
+++ iface/tun_wrapper_unix.go
@@ -1,8 +1,6 @@
 package iface
 
 import (
-	"os"
-
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"golang.zx2c4.com/wireguard/tun"
@@ -19,20 +17,20 @@ type PacketFilter interface {
 
 // TunWrapper to override Read or Write of packets
 type TunWrapper struct {
-	device tun.Device
+	tun.Device
 	filter PacketFilter
 }
 
 // newTunInjection constructor function
 func newTunInjection(device tun.Device) *TunWrapper {
 	return &TunWrapper{
-		device: device,
+		Device: device,
 	}
 }
 
 // Read wraps read method with filtering feature
 func (t *TunWrapper) Read(bufs [][]byte, sizes []int, offset int) (n int, err error) {
-	if n, err = t.device.Read(bufs, sizes, offset); err != nil {
+	if n, err = t.Device.Read(bufs, sizes, offset); err != nil {
 		return 0, err
 	}
 	if t.filter == nil {
@@ -57,7 +55,7 @@ func (t *TunWrapper) Read(bufs [][]byte, sizes []int, offset int) (n int, err er
 // Write wraps write method with filtering feature
 func (t *TunWrapper) Write(bufs [][]byte, offset int) (int, error) {
 	if t.filter == nil {
-		return t.device.Write(bufs, offset)
+		return t.Device.Write(bufs, offset)
 	}
 
 	filteredBufs := make([][]byte, 0, len(bufs))
@@ -72,39 +70,7 @@ func (t *TunWrapper) Write(bufs [][]byte, offset int) (int, error) {
 		}
 	}
 
-	n, err := t.device.Write(filteredBufs, offset)
+	n, err := t.Device.Write(filteredBufs, offset)
 	n += dropped
 	return n, err
-}
-
-// File returns the file descriptor of the device.
-func (t *TunWrapper) File() *os.File {
-	return t.device.File()
-}
-
-// MTU returns the MTU of the Device.
-func (t *TunWrapper) MTU() (int, error) {
-	return t.device.MTU()
-}
-
-// Name returns the current name of the Device.
-func (t *TunWrapper) Name() (string, error) {
-	return t.device.Name()
-}
-
-// Events returns a channel of type Event, which is fed Device events.
-func (t *TunWrapper) Events() <-chan tun.Event {
-	return t.device.Events()
-}
-
-// Close stops the Device and closes the Event channel.
-func (t *TunWrapper) Close() error {
-	return t.device.Close()
-}
-
-// BatchSize returns the preferred/max number of packets that can be read or
-// written in a single read/write call. BatchSize must not change over the
-// lifetime of a Device.
-func (t *TunWrapper) BatchSize() int {
-	return t.device.BatchSize()
 }
