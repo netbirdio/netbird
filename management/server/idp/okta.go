b330e16e023ba2ef944cc577a56f33eab41effc7
Bethuel <bethuelmbaga12@gmail.com>
2023-05-13 18:27:30 +0300

    fetch okta jwt token

--- management/server/idp/okta.go
+++ management/server/idp/okta.go
@@ -2,8 +2,13 @@ package idp
 
 import (
 	"fmt"
+	"github.com/golang-jwt/jwt"
 	"github.com/netbirdio/netbird/management/server/telemetry"
+	log "github.com/sirupsen/logrus"
+	"io"
 	"net/http"
+	"net/url"
+	"strings"
 	"sync"
 	"time"
 )
@@ -19,6 +24,7 @@ type OktaManager struct {
 type OktaClientConfig struct {
 	ClientID      string
 	ClientSecret  string
+	Issuer        string
 	TokenEndpoint string
 	GrantType     string
 }
@@ -45,6 +51,7 @@ func NewOktaManager(oidcConfig OIDCConfig, config OktaClientConfig,
 	}
 
 	helper := JsonParser{}
+	config.Issuer = oidcConfig.Issuer
 	config.TokenEndpoint = oidcConfig.TokenEndpoint
 	config.GrantType = "client_credentials"
 
@@ -71,9 +78,109 @@ func NewOktaManager(oidcConfig OIDCConfig, config OktaClientConfig,
 	}, nil
 }
 
+// jwtStillValid returns true if the token still valid and have enough time to be used and get a response from keycloak.
+func (oc *OktaCredentials) jwtStillValid() bool {
+	return !oc.jwtToken.expiresInTime.IsZero() && time.Now().Add(5*time.Second).Before(oc.jwtToken.expiresInTime)
+}
+
+// requestJWTToken performs request to get jwt token.
+func (oc *OktaCredentials) requestJWTToken() (*http.Response, error) {
+	data := url.Values{}
+	data.Set("client_id", oc.clientConfig.ClientID)
+	data.Set("client_secret", oc.clientConfig.ClientSecret)
+	data.Set("grant_type", oc.clientConfig.GrantType)
+	data.Set("scope", "api")
+
+	payload := strings.NewReader(data.Encode())
+	tokenURL := oc.clientConfig.Issuer + "/oauth2/default/v1/token"
+	req, err := http.NewRequest(http.MethodPost, tokenURL, payload)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+
+	log.Debug("requesting new jwt token for okta idp manager")
+
+	resp, err := oc.httpClient.Do(req)
+	if err != nil {
+		if oc.appMetrics != nil {
+			oc.appMetrics.IDPMetrics().CountRequestError()
+		}
+
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unable to get okta token, statusCode %d", resp.StatusCode)
+	}
+
+	return resp, nil
+}
+
+// parseRequestJWTResponse parses jwt raw response body and extracts token and expires in seconds
+func (oc *OktaCredentials) parseRequestJWTResponse(rawBody io.ReadCloser) (JWTToken, error) {
+	jwtToken := JWTToken{}
+	body, err := io.ReadAll(rawBody)
+	if err != nil {
+		return jwtToken, err
+	}
+
+	err = oc.helper.Unmarshal(body, &jwtToken)
+	if err != nil {
+		return jwtToken, err
+	}
+
+	if jwtToken.ExpiresIn == 0 && jwtToken.AccessToken == "" {
+		return jwtToken, fmt.Errorf("error while reading response body, expires_in: %d and access_token: %s",
+			jwtToken.ExpiresIn,
+			jwtToken.AccessToken,
+		)
+	}
+
+	data, err := jwt.DecodeSegment(strings.Split(jwtToken.AccessToken, ".")[1])
+	if err != nil {
+		return jwtToken, err
+	}
+
+	// Exp maps into exp from jwt token
+	var IssuedAt struct{ Exp int64 }
+	err = oc.helper.Unmarshal(data, &IssuedAt)
+	if err != nil {
+		return jwtToken, err
+	}
+	jwtToken.expiresInTime = time.Unix(IssuedAt.Exp, 0)
+
+	return jwtToken, nil
+}
+
 func (oc *OktaCredentials) Authenticate() (JWTToken, error) {
-	//TODO implement me
-	panic("implement me")
+	oc.mux.Lock()
+	defer oc.mux.Unlock()
+
+	if oc.appMetrics != nil {
+		oc.appMetrics.IDPMetrics().CountAuthenticate()
+	}
+
+	// reuse the token without requesting a new one if it is not expired,
+	// and if expiry time is sufficient time available to make a request.
+	if oc.jwtStillValid() {
+		return oc.jwtToken, nil
+	}
+
+	resp, err := oc.requestJWTToken()
+	if err != nil {
+		return oc.jwtToken, err
+	}
+	defer resp.Body.Close()
+
+	jwtToken, err := oc.parseRequestJWTResponse(resp.Body)
+	if err != nil {
+		return oc.jwtToken, err
+	}
+
+	oc.jwtToken = jwtToken
+
+	return oc.jwtToken, nil
 }
 
 func (om *OktaManager) CreateUser(email string, name string, accountID string) (*UserData, error) {
