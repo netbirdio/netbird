fe8c33463c583a55f7eb54836f2cc28ddb88f00e
braginini <bangvalo@gmail.com>
2023-05-18 19:30:32 +0200

    Fix doc

--- client/main.go
+++ client/main.go
@@ -1,50 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"math/rand"
-	"net"
-	"time"
+	"os"
 
-	"github.com/netbirdio/netbird/iface/bind"
+	"github.com/netbirdio/netbird/client/cmd"
 )
 
 func main() {
-
-	udp, err := net.ListenUDP("udp4", nil)
-	if err != nil {
-		panic(err)
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
 	}
-	muxDefault := bind.NewUniversalUDPMuxDefault(bind.UniversalUDPMuxParams{UDPConn: udp, XORMappedAddrCacheTTL: 25 * time.Second})
-
-	go func() {
-		muxDefault.ReadFromConn(context.TODO())
-	}()
-
-	addr, err := net.ResolveUDPAddr("udp4", "18.198.11.240:5555")
-	if err != nil {
-		panic(err)
-	}
-	for i := 0; i < 2; i++ {
-		go func() {
-			millis := rand.Intn(400) + 50
-			time.Sleep(time.Duration(millis) * time.Millisecond)
-
-			for {
-				a, err := muxDefault.GetXORMappedAddr(addr, 3*time.Second)
-				if err != nil {
-					//fmt.Println(time.Now())
-					fmt.Println(time.Now().String() + ": " + err.Error())
-					continue
-				}
-				//fmt.Println(time.Now())
-				fmt.Println(a)
-				/*millis = rand.Intn(1000-300) + 300
-				time.Sleep(time.Duration(millis) * time.Millisecond)*/
-			}
-		}()
-	}
-
-	select {}
 }
