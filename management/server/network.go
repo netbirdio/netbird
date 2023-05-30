ff744245f91cde7e99357958f9df130e2b4456f0
Givi Khojanashvili <gigovich@gmail.com>
2023-05-30 17:27:49 +0400

    Exclude second last IP from allocation to use it in the Fake DNS

--- management/server/network.go
+++ management/server/network.go
@@ -119,14 +119,15 @@ func generateIPs(ipNet *net.IPNet, exclusions map[string]struct{}) ([]net.IP, in
 		}
 	}
 
-	// remove network address and broadcast address
+	// remove network address, broadcast and Fake DNS resolver address
 	lenIPs := len(ips)
 	switch {
 	case lenIPs < 2:
 		return ips, lenIPs
-
-	default:
+	case lenIPs < 3:
 		return ips[1 : len(ips)-1], lenIPs - 2
+	default:
+		return ips[1 : len(ips)-2], lenIPs - 3
 	}
 }
 
