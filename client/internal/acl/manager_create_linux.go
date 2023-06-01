f04ba3bcebae05a8f14ab41e80f20148c7864a9c
Givi Khojanashvili <gigovich@gmail.com>
2023-06-01 19:51:13 +0400

    Use by default nftables on the linux systems (#922)

--- client/internal/acl/manager_create_linux.go
+++ client/internal/acl/manager_create_linux.go
@@ -19,11 +19,11 @@ func Create(iface iFaceMapper) (manager *DefaultManager, err error) {
 			return nil, err
 		}
 	} else {
-		if fm, err = iptables.Create(iface.Name()); err != nil {
-			log.Debugf("failed to create iptables manager: %s", err)
-			// fallback to nftables
-			if fm, err = nftables.Create(iface.Name()); err != nil {
-				log.Errorf("failed to create nftables manager: %s", err)
+		if fm, err = nftables.Create(iface.Name()); err != nil {
+			log.Debugf("failed to create nftables manager: %s", err)
+			// fallback to iptables
+			if fm, err = iptables.Create(iface.Name()); err != nil {
+				log.Errorf("failed to create iptables manager: %s", err)
 				return nil, err
 			}
 		}
