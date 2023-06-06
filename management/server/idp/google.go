654dd3652573c5e479ffd66f1f9fd8244061c144
Bethuel <bethuelmbaga12@gmail.com>
2023-06-06 20:52:23 +0300

    fetch access token

--- management/server/idp/google.go
+++ management/server/idp/google.go
@@ -2,8 +2,13 @@ package idp
 
 import (
 	"fmt"
+	"github.com/golang-jwt/jwt"
 	"github.com/netbirdio/netbird/management/server/telemetry"
+	log "github.com/sirupsen/logrus"
+	"io"
 	"net/http"
+	"net/url"
+	"strings"
 	"sync"
 	"time"
 )
@@ -34,9 +39,104 @@ type GoogleCredentials struct {
 	appMetrics   telemetry.AppMetrics
 }
 
+// jwtStillValid returns true if the token still valid and have enough time to be used and get a response from azure.
+func (gc *GoogleCredentials) jwtStillValid() bool {
+	return !gc.jwtToken.expiresInTime.IsZero() && time.Now().Add(5*time.Second).Before(gc.jwtToken.expiresInTime)
+}
+
+// requestJWTToken performs request to get jwt token.
+func (gc *GoogleCredentials) requestJWTToken() (*http.Response, error) {
+	data := url.Values{}
+	data.Set("client_id", gc.clientConfig.ClientID)
+	data.Set("client_secret", gc.clientConfig.ClientSecret)
+	data.Set("grant_type", gc.clientConfig.GrantType)
+
+	payload := strings.NewReader(data.Encode())
+	req, err := http.NewRequest(http.MethodPost, gc.clientConfig.TokenEndpoint, payload)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+
+	log.Debug("requesting new jwt token for google idp manager")
+
+	resp, err := gc.httpClient.Do(req)
+	if err != nil {
+		if gc.appMetrics != nil {
+			gc.appMetrics.IDPMetrics().CountRequestError()
+		}
+
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unable to get google token, statusCode %d", resp.StatusCode)
+	}
+
+	return resp, nil
+}
+
+// parseRequestJWTResponse parses jwt raw response body and extracts token and expires in seconds
+func (gc *GoogleCredentials) parseRequestJWTResponse(rawBody io.ReadCloser) (JWTToken, error) {
+	jwtToken := JWTToken{}
+	body, err := io.ReadAll(rawBody)
+	if err != nil {
+		return jwtToken, err
+	}
+
+	err = gc.helper.Unmarshal(body, &jwtToken)
+	if err != nil {
+		return jwtToken, err
+	}
+
+	if jwtToken.ExpiresIn == 0 && jwtToken.AccessToken == "" {
+		return jwtToken, fmt.Errorf("error while reading response body, expires_in: %d and gccess_token: %s", jwtToken.ExpiresIn, jwtToken.AccessToken)
+	}
+
+	data, err := jwt.DecodeSegment(strings.Split(jwtToken.AccessToken, ".")[1])
+	if err != nil {
+		return jwtToken, err
+	}
+
+	// Exp maps into exp from jwt token
+	var IssuedAt struct{ Exp int64 }
+	err = gc.helper.Unmarshal(data, &IssuedAt)
+	if err != nil {
+		return jwtToken, err
+	}
+	jwtToken.expiresInTime = time.Unix(IssuedAt.Exp, 0)
+
+	return jwtToken, nil
+}
+
 func (gc *GoogleCredentials) Authenticate() (JWTToken, error) {
-	//TODO implement me
-	panic("implement me")
+	gc.mux.Lock()
+	defer gc.mux.Unlock()
+
+	if gc.appMetrics != nil {
+		gc.appMetrics.IDPMetrics().CountAuthenticate()
+	}
+
+	// reuse the token without requesting a new one if it is not expired,
+	// and if expiry time is sufficient time available to make a request.
+	if gc.jwtStillValid() {
+		return gc.jwtToken, nil
+	}
+
+	resp, err := gc.requestJWTToken()
+	if err != nil {
+		return gc.jwtToken, err
+	}
+	defer resp.Body.Close()
+
+	jwtToken, err := gc.parseRequestJWTResponse(resp.Body)
+	if err != nil {
+		return gc.jwtToken, err
+	}
+
+	gc.jwtToken = jwtToken
+
+	return gc.jwtToken, nil
 }
 
 // NewGoogleManager creates a new instance of the GoogleManager.
@@ -91,7 +191,7 @@ func (gm *GoogleManager) GetUserDataByID(userID string, appMetadata AppMetadata)
 	panic("implement me")
 }
 
-func (gm *GoogleManager) GetAccount(accountID string) ([]*UserData, error) {
+func (gm *GoogleManager) GetAccount(gccountID string) ([]*UserData, error) {
 	//TODO implement me
 	panic("implement me")
 }
@@ -101,7 +201,7 @@ func (gm *GoogleManager) GetAllAccounts() (map[string][]*UserData, error) {
 	panic("implement me")
 }
 
-func (gm *GoogleManager) CreateUser(email string, name string, accountID string) (*UserData, error) {
+func (gm *GoogleManager) CreateUser(email string, name string, gccountID string) (*UserData, error) {
 	//TODO implement me
 	panic("implement me")
 }
