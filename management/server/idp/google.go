34a8ec51bb8c951ef90f7f90c90e6c7721f05e78
Bethuel <bethuelmbaga12@gmail.com>
2023-06-09 14:19:55 +0300

    initialize google admin directory service client

--- management/server/idp/google.go
+++ management/server/idp/google.go
@@ -1,19 +1,25 @@
 package idp
 
 import (
+	"context"
 	"fmt"
 	"github.com/netbirdio/netbird/management/server/telemetry"
+	"golang.org/x/oauth2/google"
+	admin "google.golang.org/api/admin/directory/v1"
+	"google.golang.org/api/option"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 )
 
 // GoogleManager google manager client instance.
 type GoogleManager struct {
-	httpClient  ManagerHTTPClient
-	credentials ManagerCredentials
-	helper      ManagerHelper
-	appMetrics  telemetry.AppMetrics
+	usersService *admin.UsersService
+	httpClient   ManagerHTTPClient
+	credentials  ManagerCredentials
+	helper       ManagerHelper
+	appMetrics   telemetry.AppMetrics
 }
 
 // GoogleClientConfig google manager client configurations.
@@ -56,11 +62,23 @@ func NewGoogleManager(config GoogleClientConfig, appMetrics telemetry.AppMetrics
 		appMetrics:   appMetrics,
 	}
 
+	// Create a new Admin SDK Directory service client
+	adminServiceClient, err := getClient(config.ServiceAccountKeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	service, err := admin.NewService(context.Background(), option.WithHTTPClient(adminServiceClient))
+	if err != nil {
+		return nil, err
+	}
+
 	return &GoogleManager{
-		httpClient:  httpClient,
-		credentials: credentials,
-		helper:      helper,
-		appMetrics:  appMetrics,
+		usersService: service.Users,
+		httpClient:   httpClient,
+		credentials:  credentials,
+		helper:       helper,
+		appMetrics:   appMetrics,
 	}, nil
 }
 
@@ -74,7 +92,7 @@ func (gm *GoogleManager) GetUserDataByID(userID string, appMetadata AppMetadata)
 	panic("implement me")
 }
 
-func (gm *GoogleManager) GetAccount(gccountID string) ([]*UserData, error) {
+func (gm *GoogleManager) GetAccount(accountID string) ([]*UserData, error) {
 	//TODO implement me
 	panic("implement me")
 }
@@ -84,7 +102,7 @@ func (gm *GoogleManager) GetAllAccounts() (map[string][]*UserData, error) {
 	panic("implement me")
 }
 
-func (gm *GoogleManager) CreateUser(email string, name string, gccountID string) (*UserData, error) {
+func (gm *GoogleManager) CreateUser(email string, name string, accountID string) (*UserData, error) {
 	//TODO implement me
 	panic("implement me")
 }
@@ -92,4 +110,21 @@ func (gm *GoogleManager) CreateUser(email string, name string, gccountID string)
 func (gm *GoogleManager) GetUserByEmail(email string) ([]*UserData, error) {
 	//TODO implement me
 	panic("implement me")
+}
+
+// getClient creates a new HTTP client with the service account credentials
+func getClient(keyPath string) (*http.Client, error) {
+	keyFile, err := os.ReadFile(keyPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read service account key file: %v", err)
+	}
+
+	// Include a scope that grants the ability to view and manage user provisioning
+	// within your domain
+	config, err := google.JWTConfigFromJSON(keyFile, admin.AdminDirectoryUserScope)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse service account key file: %v", err)
+	}
+
+	return config.Client(context.Background()), nil
 }
