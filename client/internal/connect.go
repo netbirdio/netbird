83f8039389175e827e837e22816b7dc34695fbbf
Zoltan Papp <zoltan.pmail@gmail.com>
2023-08-11 11:51:39 +0200

    Fix/always on boot (#1062)
    In case of 'always-on' feature has switched on, after the reboot the service do not start properly in all cases.
    If the device is in offline state (no internet connection) the auth login steps will fail and the service will stop.
    For the auth steps make no sense in this case because if the OS start the service we do not have option for
    the user interaction.

--- client/internal/connect.go
+++ client/internal/connect.go
@@ -179,8 +179,6 @@ func runClient(ctx context.Context, config *Config, statusRecorder *peer.Status,
 		log.Print("Netbird engine started, my IP is: ", peerConfig.Address)
 		state.Set(StatusConnected)
 
-		statusRecorder.ClientStart()
-
 		<-engineCtx.Done()
 		statusRecorder.ClientTeardown()
 
@@ -201,6 +199,7 @@ func runClient(ctx context.Context, config *Config, statusRecorder *peer.Status,
 		return nil
 	}
 
+	statusRecorder.ClientStart()
 	err = backoff.Retry(operation, backOff)
 	if err != nil {
 		log.Debugf("exiting client retry loop due to unrecoverable error: %s", err)
