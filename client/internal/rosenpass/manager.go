a2ba4058153727fd3f1e27fb0a49500de254d43a
Pascal Fischer <pascal@netbird.io>
2023-08-18 18:05:10 +0200

    initial package integration

---
+++ client/internal/rosenpass/manager.go
@@ -0,0 +1,62 @@
+package rosenpass
+
+import (
+	"errors"
+	"fmt"
+
+	rp "cunicu.li/go-rosenpass"
+	"cunicu.li/go-rosenpass/config"
+
+	"github.com/netbirdio/netbird/management/proto"
+)
+
+type Manager struct {
+	spk []byte
+	ssk []byte
+}
+
+func NewManager() *Manager {
+	return &Manager{}
+}
+
+func (m *Manager) GenerateKeyPair() error {
+	spk, ssk, err := rp.GenerateKeyPair()
+	if err != nil {
+		return err
+	}
+
+	m.spk = spk
+	m.ssk = ssk
+
+	return nil
+}
+
+func (m *Manager) generateConfig(peers []*proto.RemotePeerConfig) (cfg config.File, err error) {
+
+	cfg := config.File{}
+	cfg.SecretKey = string(m.ssk)
+	cfg.PublicKey = string(m.spk)
+
+	// own local host and port to listen for handshake
+	cfg.ListenAddrs = []string{"0.0.0.0:9999"}
+
+	// Checks
+	if cfg.PublicKey == "" {
+		return cfg, errors.New("missing public key for rosenpass")
+	} else if cfg.SecretKey == "" {
+		return cfg, errors.New("missing secret key for rosenpass")
+	}
+
+	for _, peer := range peers {
+		var pc config.PeerSection
+		allowedIP := peer.GetAllowedIps()
+		pc.PublicKey =
+		// pc.PresharedKey =
+		pc.Endpoint = &allowedIP[0]
+		pc.KeyOut = "bal"
+
+		cfg.Peers = append(cfg.Peers, pc)
+	}
+
+	return cfg, nil
+}
