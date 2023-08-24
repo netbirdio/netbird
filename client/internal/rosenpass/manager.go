0363ac1cf7d232fa34b7892f8184bd9061de9873
braginini <bangvalo@gmail.com>
2023-08-24 17:08:44 +0200

    Fix rosenpass key exchange

--- client/internal/rosenpass/manager.go
+++ client/internal/rosenpass/manager.go
@@ -1,25 +1,29 @@
 package rosenpass
 
 import (
-	"crypto/md5"
+	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"log/slog"
 	"net"
+	"os"
 	"sync"
 
 	rp "cunicu.li/go-rosenpass"
 )
 
 type rpConn struct {
-	key     []byte
-	wgIP    string
-	peerKey string
+	key       []byte
+	wgIP      string
+	peerKey   string
+	rpKeyHash string
 }
 
 type Manager struct {
 	spk           []byte
 	ssk           []byte
+	rpKeyHash     string
 	rpConnections map[string]*rpConn
 	server        *rp.Server
 	lock          sync.Mutex
@@ -30,27 +34,27 @@ func NewManager() (*Manager, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Manager{spk: public, ssk: secret, rpConnections: make(map[string]*rpConn), lock: sync.Mutex{}}, nil
+	hasher := sha256.New()
+	hasher.Write(public)
+	rpKeyHash := hex.EncodeToString(hasher.Sum(nil))
+	log.Infof("generated new rosenpass key pair with public key %s", rpKeyHash)
+	return &Manager{rpKeyHash: rpKeyHash, spk: public, ssk: secret, rpConnections: make(map[string]*rpConn), lock: sync.Mutex{}}, nil
 }
 
 func (m *Manager) GetPubKey() []byte {
+	hasher := sha256.New()
+	hasher.Write(m.spk)
+	rpKeyHash := hex.EncodeToString(hasher.Sum(nil))
+	log.Infof("getting my rosenpass key %s", rpKeyHash)
 	return m.spk
 }
 
-func (m *Manager) GenerateKeyPair() error {
-	spk, ssk, err := rp.GenerateKeyPair()
-	if err != nil {
-		return err
-	}
-
-	m.spk = spk
-	m.ssk = ssk
-
-	return nil
-}
-
 func (m *Manager) generateConfig() (rp.Config, error) {
-	cfg := rp.Config{}
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}
+	logger := slog.New(slog.NewTextHandler(os.Stdout, opts))
+	cfg := rp.Config{Logger: logger}
 	udpAddr := &net.UDPAddr{}
 	var err error
 	if udpAddr, err = net.ResolveUDPAddr("udp", "0.0.0.0:9999"); err != nil {
@@ -77,13 +81,15 @@ func (m *Manager) generateConfig() (rp.Config, error) {
 func (m *Manager) OnConnected(peerKey string, rpPubKey []byte, wgIP string) {
 	m.lock.Lock()
 	defer m.lock.Unlock()
-	hasher := md5.New()
+	hasher := sha256.New()
 	hasher.Write(rpPubKey)
-	log.Debugf("received remote rosenpass key %s", hex.EncodeToString(hasher.Sum(nil)))
+	rpKeyHash := hex.EncodeToString(hasher.Sum(nil))
+	log.Debugf("received remote rosenpass key %s, my key %s", rpKeyHash, m.rpKeyHash)
 	m.rpConnections[peerKey] = &rpConn{
-		key:     rpPubKey,
-		wgIP:    wgIP,
-		peerKey: peerKey,
+		key:       rpPubKey,
+		wgIP:      wgIP,
+		peerKey:   peerKey,
+		rpKeyHash: rpKeyHash,
 	}
 
 	conf, err := m.generateConfig()
