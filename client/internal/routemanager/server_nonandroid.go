884dcd8ad3c19696d4bb95aab01d8b6a315a1b64
Maycon Santos <mlsmaycon@gmail.com>
2023-09-03 13:11:14 +0200

    using check fw for routemanager

--- client/internal/routemanager/server_nonandroid.go
+++ client/internal/routemanager/server_nonandroid.go
@@ -22,10 +22,7 @@ type defaultServerRouter struct {
 }
 
 func newServerRouter(ctx context.Context, wgInterface *iface.WGIface) (serverRouter, error) {
-	firewall, err := newFirewall(ctx)
-	if err != nil {
-		return nil, err
-	}
+	firewall := newFirewall(ctx)
 
 	return &defaultServerRouter{
 		ctx:         ctx,
