542e548ba7f37e73972584ed6827da0606375f40
Zoltan Papp <zoltan.pmail@gmail.com>
2023-10-26 16:57:39 +0200

    Download link based on installation type (#1243)
    Based on the registry, determine the installation source (MSI or EXE)
    and open the appropriate link in the update notification menu

--- version/url.go
+++ version/url.go
@@ -1,47 +1,5 @@
 package version
 
-import (
-	"os/exec"
-	"runtime"
-)
-
 const (
 	downloadURL = "https://app.netbird.io/install"
-	macIntelURL = "https://pkgs.netbird.io/macos/amd64"
-	macM1M2URL  = "https://pkgs.netbird.io/macos/arm64"
 )
-
-// DownloadUrl return with the proper download link
-func DownloadUrl() string {
-	switch runtime.GOOS {
-	case "windows":
-		return downloadURL
-	case "darwin":
-		return darwinDownloadUrl()
-	case "linux":
-		return downloadURL
-	default:
-		return downloadURL
-	}
-}
-
-func darwinDownloadUrl() string {
-	cmd := exec.Command("brew", "list --formula | grep -i netbird")
-	if err := cmd.Start(); err != nil {
-		goto PKGINSTALL
-	}
-
-	if err := cmd.Wait(); err == nil {
-		return downloadURL
-	}
-
-PKGINSTALL:
-	switch runtime.GOARCH {
-	case "amd64":
-		return macIntelURL
-	case "arm64":
-		return macM1M2URL
-	default:
-		return downloadURL
-	}
-}
