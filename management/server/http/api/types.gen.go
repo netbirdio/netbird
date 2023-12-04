de542959e65c8c1744b701b6db7d6c3ce79b37c3
Maycon Santos <mlsmaycon@gmail.com>
2023-12-04 19:02:16 +0100

    update API doc

--- management/server/http/api/types.gen.go
+++ management/server/http/api/types.gen.go
@@ -144,7 +144,7 @@ type Account struct {
 
 // AccountExtraSettings defines model for AccountExtraSettings.
 type AccountExtraSettings struct {
-	// PeerApprovalEnabled Enables or disables peer approval globally. If enabled, all peers added will be in pending state until approved by an admin.
+	// PeerApprovalEnabled (Cloud only) Enables or disables peer approval globally. If enabled, all peers added will be in pending state until approved by an admin.
 	PeerApprovalEnabled *bool `json:"peer_approval_enabled,omitempty"`
 }
 
