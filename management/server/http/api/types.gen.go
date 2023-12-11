d275d411aaee600c5586264e2adc5d4635a25790
Bethuel Mmbaga <bethuelmbaga12@gmail.com>
2023-12-11 18:59:15 +0300

    Enable JWT group-based user authorization (#1368)
    * Extend management API to support list of allowed JWT groups (#1366)
    
    * Add JWTAllowGroups settings to account management
    
    * Return an empty group list if jwt allow groups is not set
    
    * Add JwtAllowGroups to account settings in handler test
    
    * Add JWT group-based user authorization (#1373)
    
    * Add JWTAllowGroups settings to account management
    
    * Return an empty group list if jwt allow groups is not set
    
    * Add JwtAllowGroups to account settings in handler test
    
    * Implement user access validation authentication based on JWT groups
    
    * Remove the slices package import due to compatibility issues with the gitHub workflow(s) Go version
    
    * Refactor auth middleware and test for extracted claim handling
    
    * Optimize JWT group check in auth middleware to cover nil and empty allowed groups

--- management/server/http/api/types.gen.go
+++ management/server/http/api/types.gen.go
@@ -160,6 +160,9 @@ type AccountSettings struct {
 	// GroupsPropagationEnabled Allows propagate the new user auto groups to peers that belongs to the user
 	GroupsPropagationEnabled *bool `json:"groups_propagation_enabled,omitempty"`
 
+	// JwtAllowGroups List of groups to which users are allowed access
+	JwtAllowGroups *[]string `json:"jwt_allow_groups,omitempty"`
+
 	// JwtGroupsClaimName Name of the claim from which we extract groups names to add it to account groups.
 	JwtGroupsClaimName *string `json:"jwt_groups_claim_name,omitempty"`
 
