987fc6f362e807608f7ff98000d03e0b161fdea1
Maycon Santos <mlsmaycon@gmail.com>
2023-12-27 20:56:04 +0100

    Update cloud management URL to https://api.netbird.io:443 (#1402)
    With this change we are updating client configuration files to use the new domain

--- client/ui/client_ui.go
+++ client/ui/client_ui.go
@@ -563,8 +563,8 @@ func (s *serviceClient) getSrvClient(timeout time.Duration) (proto.DaemonService
 
 // getSrvConfig from the service to show it in the settings window.
 func (s *serviceClient) getSrvConfig() {
-	s.managementURL = "https://api.wiretrustee.com:33073"
-	s.adminURL = "https://app.netbird.io"
+	s.managementURL = internal.DefaultManagementURL
+	s.adminURL = internal.DefaultAdminURL
 
 	conn, err := s.getSrvClient(failFastTimeout)
 	if err != nil {
