679e162a6cc0f58f8e4b2a53f13fe7017465d5a1
Viktor Liu <viktor@netbird.io>
2024-01-17 10:36:06 +0100

    Fix probe docs

--- client/internal/probe.go
+++ client/internal/probe.go
@@ -4,8 +4,8 @@ import "context"
 
 // Probe allows to run on-demand callbacks from different code locations.
 // Pass the probe to a receiving and a sending end. The receiving end starts listening
-// to requests with Receive and executes a callback when then sending end requests it
-// by calling Probe
+// to requests with Receive and executes a callback when the sending end requests it
+// by calling Probe.
 type Probe struct {
 	request chan struct{}
 	result  chan bool
@@ -20,7 +20,7 @@ func NewProbe() *Probe {
 	}
 }
 
-// Probe requests the callback to be run and returns a bool indicating.
+// Probe requests the callback to be run and returns a bool indicating success.
 // It always returns true as long as the receiver is not ready.
 func (p *Probe) Probe() bool {
 	if !p.ready {
@@ -33,7 +33,7 @@ func (p *Probe) Probe() bool {
 
 // Receive starts listening for probe requests. On such a request it runs the supplied
 // callback func which must return a bool indicating success.
-// Blocks until the context is cancelled.
+// Blocks until the passed context is cancelled.
 func (p *Probe) Receive(ctx context.Context, callback func() bool) {
 	p.ready = true
 	defer func() {
