52969fc4dc0537cb7b7e852b99c1342535a1660c
Yury Gargay <yury.gargay@gmail.com>
2024-01-18 15:18:32 +0100

    Implement OSVersionCheck

--- management/server/posture/os_version.go
+++ management/server/posture/os_version.go
@@ -1,6 +1,9 @@
 package posture
 
 import (
+	"fmt"
+
+	"github.com/hashicorp/go-version"
 	nbpeer "github.com/netbirdio/netbird/management/server/peer"
 )
 
@@ -18,10 +21,45 @@ type OSVersionCheck struct {
 
 var _ Check = (*OSVersionCheck)(nil)
 
-func (n *OSVersionCheck) Check(peer nbpeer.Peer) error {
+func (c *OSVersionCheck) Check(peer nbpeer.Peer) error {
+	peerGoOS := peer.Meta.GoOS
+	switch peerGoOS {
+	case "android":
+		return checkMinVersion(peerGoOS, peer.Meta.Core, c.Android)
+	case "darwin":
+		return checkMinVersion(peerGoOS, peer.Meta.Core, c.Darwin)
+	case "ios":
+		return checkMinVersion(peerGoOS, peer.Meta.Core, c.Ios)
+	case "linux":
+		return checkMinVersion(peerGoOS, peer.Meta.Core, c.Linux)
+	case "windows":
+		return checkMinVersion(peerGoOS, peer.Meta.Core, c.Windows)
+	}
 	return nil
 }
 
-func (n *OSVersionCheck) Name() string {
+func (c *OSVersionCheck) Name() string {
 	return OSVersionCheckName
+}
+
+func checkMinVersion(peerGoOS, peerVersion string, check *MinVersionCheck) error {
+	if check == nil {
+		return nil
+	}
+
+	peerNBVersion, err := version.NewVersion(peerVersion)
+	if err != nil {
+		return err
+	}
+
+	constraints, err := version.NewConstraint(">= " + check.MinVersion)
+	if err != nil {
+		return err
+	}
+
+	if constraints.Check(peerNBVersion) {
+		return nil
+	}
+
+	return fmt.Errorf("peer %s version %s is older than minimum allowed version %s", peerGoOS, peerVersion, check.MinVersion)
 }
