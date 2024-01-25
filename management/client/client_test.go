e8b2a66c19eb118a8de95a4c10bfdb4e7222e620
bcmmbaga <bethuelmbaga12@gmail.com>
2024-01-25 10:47:41 +0300

    Fix tests

--- management/client/client_test.go
+++ management/client/client_test.go
@@ -352,6 +352,7 @@ func Test_SystemMetaDataFromClient(t *testing.T) {
 		Platform:           info.Platform,
 		OS:                 info.OS,
 		WiretrusteeVersion: info.WiretrusteeVersion,
+		KernelVersion:      info.KernelVersion,
 	}
 
 	assert.Equal(t, ValidKey, actualValidKey)
