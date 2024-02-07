8a660310b3b4898fcebfe9914959aa453cbcc974
Maycon Santos <mlsmaycon@gmail.com>
2024-02-07 16:04:15 +0100

    remove redundant external cache check
    Update logs and error

--- management/server/account.go
+++ management/server/account.go
@@ -1232,8 +1232,8 @@ func (am *DefaultAccountManager) lookupUserInCache(userID string, account *Accou
 	key := user.IntegrationReference.CacheKey(account.Id, userID)
 	ud, err := am.externalCacheManager.Get(am.ctx, key)
 	if err == nil {
-		log.Debugf("user %s found in external cache", userID)
-		return ud, nil
+		log.Errorf("failed to get externalCache for key: %s, error: %s", key, err)
+		return ud, status.Errorf(status.NotFound, "user %s not found in the IdP", userID)
 	}
 
 	log.Infof("user %s not found in any cache", userID)
