e76dd92c5634bb638913632b02246b4b58ffc979
Pascal Fischer <pascal@netbird.io>
2024-02-09 18:37:07 +0100

    parallelize IBM checks

--- client/system/detect_cloud/ibmcloud.go
+++ client/system/detect_cloud/ibmcloud.go
@@ -3,27 +3,49 @@ package detect_cloud
 import "net/http"
 
 func detectIBMCloud() string {
-	req, err := http.NewRequest("PUT", "http://api.metadata.cloud.ibm.com/instance_identity/v1/token", nil)
+	v1ResultChan := make(chan bool, 1)
+	v2ResultChan := make(chan bool, 1)
+
+	go func() {
+		v1ResultChan <- detectIBMSecure()
+	}()
+
+	go func() {
+		v2ResultChan <- detectIBM()
+	}()
+
+	v1Result, v2Result := <-v1ResultChan, <-v2ResultChan
+
+	if v1Result || v2Result {
+		return "Amazon Web Services"
+	}
+	return ""
+}
+
+func detectIBMSecure() bool {
+	req, err := http.NewRequest("PUT", "https://api.metadata.cloud.ibm.com/instance_identity/v1/token", nil)
 	if err != nil {
-		return ""
+		return false
 	}
 
 	resp, err := hc.Do(req)
 	if err != nil {
-		req, err = http.NewRequest("PUT", "https://api.metadata.cloud.ibm.com/instance_identity/v1/token", nil)
-		if err != nil {
-			return ""
-		}
-		resp, err = hc.Do(req)
-		if err != nil {
-			return ""
-		}
-		defer resp.Body.Close()
+		return false
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode == http.StatusOK
+}
+
+func detectIBM() bool {
+	req, err := http.NewRequest("PUT", "http://api.metadata.cloud.ibm.com/instance_identity/v1/token", nil)
+	if err != nil {
+		return false
 	}
 
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusOK {
-		return "IBM Cloud"
+	resp, err := hc.Do(req)
+	if err != nil {
+		return false
 	}
-	return ""
+	defer resp.Body.Close()
+	return resp.StatusCode == http.StatusOK
 }
