9ea26a1f4198620e5d186cf897e534dc7aa93f84
Viktor Liu <viktor@netbird.io>
2024-02-21 10:23:47 +0100

    Use transaction to read stats from sqlite

--- management/server/sqlite_store.go
+++ management/server/sqlite_store.go
@@ -504,37 +504,41 @@ func (s *SqliteStore) GetStoreEngine() StoreEngine {
 func (s *SqliteStore) CalculateUsageStats(ctx context.Context, accountID string, start time.Time, end time.Time) (*AccountUsageStats, error) {
 	stats := &AccountUsageStats{}
 
-	err := s.db.Model(&nbpeer.Peer{}).
-		WithContext(ctx).
-		Where("account_id = ? AND peer_status_last_seen BETWEEN ? AND ?", accountID, start, end).
-		Distinct("user_id").
-		Count(&stats.ActiveUsers).Error
-	if err != nil {
-		return nil, fmt.Errorf("get active users: %w", err)
-	}
+	err := s.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		err := tx.Model(&nbpeer.Peer{}).
+			Where("account_id = ? AND peer_status_last_seen BETWEEN ? AND ?", accountID, start, end).
+			Distinct("user_id").
+			Count(&stats.ActiveUsers).Error
+		if err != nil {
+			return fmt.Errorf("get active users: %w", err)
+		}
 
-	err = s.db.Model(&User{}).
-		WithContext(ctx).
-		Where("account_id = ?", accountID).
-		Count(&stats.TotalUsers).Error
-	if err != nil {
-		return nil, fmt.Errorf("get total users: %w", err)
-	}
+		err = tx.Model(&User{}).
+			Where("account_id = ?", accountID).
+			Count(&stats.TotalUsers).Error
+		if err != nil {
+			return fmt.Errorf("get total users: %w", err)
+		}
 
-	err = s.db.Model(&nbpeer.Peer{}).
-		WithContext(ctx).
-		Where("account_id = ? AND peer_status_last_seen BETWEEN ? AND ?", accountID, start, end).
-		Count(&stats.ActivePeers).Error
-	if err != nil {
-		return nil, fmt.Errorf("get active peers: %w", err)
-	}
+		err = tx.Model(&nbpeer.Peer{}).
+			Where("account_id = ? AND peer_status_last_seen BETWEEN ? AND ?", accountID, start, end).
+			Count(&stats.ActivePeers).Error
+		if err != nil {
+			return fmt.Errorf("get active peers: %w", err)
+		}
 
-	err = s.db.Model(&nbpeer.Peer{}).
-		WithContext(ctx).
-		Where("account_id = ?", accountID).
-		Count(&stats.TotalPeers).Error
+		err = tx.Model(&nbpeer.Peer{}).
+			Where("account_id = ?", accountID).
+			Count(&stats.TotalPeers).Error
+		if err != nil {
+			return fmt.Errorf("get total peers: %w", err)
+		}
+
+		return nil
+	})
+
 	if err != nil {
-		return nil, fmt.Errorf("get total peers: %w", err)
+		return nil, fmt.Errorf("transaction: %w", err)
 	}
 
 	return stats, nil
