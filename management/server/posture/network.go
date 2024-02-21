be1158fa91c931ea843f93f324262ab07d915b02
bcmmbaga <bethuelmbaga12@gmail.com>
2024-02-21 20:46:42 +0300

    Add network check functionality to posture package

--- management/server/posture/network.go
+++ management/server/posture/network.go
@@ -1,6 +1,7 @@
 package posture
 
 import (
+	"fmt"
 	"net/netip"
 
 	nbpeer "github.com/netbirdio/netbird/management/server/peer"
@@ -14,7 +15,29 @@ type PrivateNetworkCheck struct {
 var _ Check = (*PrivateNetworkCheck)(nil)
 
 func (p *PrivateNetworkCheck) Check(peer nbpeer.Peer) (bool, error) {
-	return false, nil
+	for _, prefix := range p.Prefixes {
+		for _, peerNetAddr := range peer.Meta.NetworkAddresses {
+			if prefix.Masked() == peerNetAddr.NetIP.Masked() {
+				switch p.Action {
+				case CheckActionDeny:
+					return false, nil
+				case CheckActionAllow:
+					return true, nil
+				default:
+					return false, fmt.Errorf("invalid private network check action: %s", p.Action)
+				}
+			}
+		}
+	}
+
+	if p.Action == CheckActionDeny {
+		return true, nil
+	}
+	if p.Action == CheckActionAllow {
+		return false, nil
+	}
+
+	return false, fmt.Errorf("invalid private network action: %s", p.Action)
 }
 
 func (p *PrivateNetworkCheck) Name() string {
