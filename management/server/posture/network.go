7d59132c90bad200c9edc37ee6f8712e591b7f2f
bcmmbaga <bethuelmbaga12@gmail.com>
2024-02-22 12:52:54 +0300

    Refactor network address handling in PrivateNetworkCheck

--- management/server/posture/network.go
+++ management/server/posture/network.go
@@ -3,29 +3,38 @@ package posture
 import (
 	"fmt"
 	"net/netip"
+	"slices"
 
 	nbpeer "github.com/netbirdio/netbird/management/server/peer"
 )
 
 type PrivateNetworkCheck struct {
-	Action   string
-	Prefixes []netip.Prefix
+	Action string
+	Ranges []netip.Prefix
 }
 
 var _ Check = (*PrivateNetworkCheck)(nil)
 
 func (p *PrivateNetworkCheck) Check(peer nbpeer.Peer) (bool, error) {
-	for _, prefix := range p.Prefixes {
-		for _, peerNetAddr := range peer.Meta.NetworkAddresses {
-			if prefix.Masked() == peerNetAddr.NetIP.Masked() {
-				switch p.Action {
-				case CheckActionDeny:
-					return false, nil
-				case CheckActionAllow:
-					return true, nil
-				default:
-					return false, fmt.Errorf("invalid private network check action: %s", p.Action)
-				}
+	if len(peer.Meta.NetworkAddresses) == 0 {
+		return false, fmt.Errorf("peer's does not contain private network addresses")
+	}
+
+	maskedPrefixes := make([]netip.Prefix, 0, len(p.Ranges))
+	for _, prefix := range p.Ranges {
+		maskedPrefixes = append(maskedPrefixes, prefix.Masked())
+	}
+
+	for _, peerNetAddr := range peer.Meta.NetworkAddresses {
+		peerMaskedPrefix := peerNetAddr.NetIP.Masked()
+		if slices.Contains(maskedPrefixes, peerMaskedPrefix) {
+			switch p.Action {
+			case CheckActionDeny:
+				return false, nil
+			case CheckActionAllow:
+				return true, nil
+			default:
+				return false, fmt.Errorf("invalid private network check action: %s", p.Action)
 			}
 		}
 	}
@@ -37,7 +46,7 @@ func (p *PrivateNetworkCheck) Check(peer nbpeer.Peer) (bool, error) {
 		return false, nil
 	}
 
-	return false, fmt.Errorf("invalid private network action: %s", p.Action)
+	return false, fmt.Errorf("invalid private network check action: %s", p.Action)
 }
 
 func (p *PrivateNetworkCheck) Name() string {
