a443720a6880c9c9b37535664dee30b0e14198f3
bcmmbaga <bethuelmbaga12@gmail.com>
2024-02-25 15:35:27 +0300

    load geo lite binary database

--- management/server/geolocation/db_loader.go
+++ management/server/geolocation/db_loader.go
@@ -1,7 +1,11 @@
 package geolocation
 
 import (
+	"fmt"
+	"net/url"
+	"os"
 	"path"
+	"strings"
 )
 
 const (
@@ -11,8 +15,8 @@ const (
 	geoLiteCitySha256ZipURL = "https://pkgs.netbird.io/geolocation-dbs/GeoLite2-City-CSV/download?suffix=zip.sha256"
 )
 
-// loadMaxMindDatabases loads the MaxMind databases.
-func loadMaxMindDatabases(dataDir string) error {
+// LoadMaxMindDatabases loads the MaxMind databases.
+func LoadMaxMindDatabases(dataDir string) error {
 	var reload bool
 
 	_, err := fileExists(path.Join(dataDir, MMDBFileName))
@@ -26,8 +30,57 @@ func loadMaxMindDatabases(dataDir string) error {
 	}
 
 	if reload {
-		// download and load databases
+		if err := loadGeoLiteBinaryDatabase(dataDir); err != nil {
+			return fmt.Errorf("error loading geolite binary database: %v", err)
+		}
 	}
 
 	return nil
+}
+
+func loadGeoLiteBinaryDatabase(dataDir string) error {
+	temp, err := os.MkdirTemp(os.TempDir(), strings.TrimPrefix(MMDBFileName, ".mmdb"))
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(temp)
+
+	checksumFile := path.Join(temp, getFileName(geoLiteCitySha256TarURL))
+	err = downloadFile(geoLiteCitySha256TarURL, checksumFile)
+	if err != nil {
+		return err
+	}
+
+	sha256sum, err := loadChecksumFromFile(checksumFile)
+	if err != nil {
+		return err
+	}
+
+	binaryDbFile := path.Join(temp, getFileName(geoLiteCityTarGZURL))
+	err = downloadFile(geoLiteCityTarGZURL, binaryDbFile)
+	if err != nil {
+		return err
+	}
+
+	if err := verifyChecksum(binaryDbFile, sha256sum); err != nil {
+		return err
+	}
+
+	if err := decompressTarGzFile(binaryDbFile, temp); err != nil {
+		return err
+	}
+
+	// move the extracted db file to management data directory
+	return os.Rename(path.Join(temp, MMDBFileName), path.Join(dataDir, MMDBFileName))
+}
+
+func getFileName(urlStr string) string {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		panic(err)
+	}
+
+	ext := u.Query().Get("suffix")
+	fileName := fmt.Sprintf("%s.%s", path.Base(u.Path), ext)
+	return fileName
 }
