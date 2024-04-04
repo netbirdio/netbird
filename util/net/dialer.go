b3201855a076691db931f259e5165bcf50621f10
Viktor Liu <viktor@netbird.io>
2024-04-04 23:32:21 +0200

    Expect correct conn type

--- util/net/dialer.go
+++ util/net/dialer.go
@@ -1,10 +1,7 @@
 package net
 
 import (
-	"fmt"
 	"net"
-
-	log "github.com/sirupsen/logrus"
 )
 
 // Dialer extends the standard net.Dialer with the ability to execute hooks before
@@ -21,44 +18,4 @@ func NewDialer() *Dialer {
 	dialer.init()
 
 	return dialer
-}
-
-func DialUDP(network string, laddr, raddr *net.UDPAddr) (*net.UDPConn, error) {
-	dialer := NewDialer()
-	dialer.LocalAddr = laddr
-
-	conn, err := dialer.Dial(network, raddr.String())
-	if err != nil {
-		return nil, fmt.Errorf("dialing UDP %s: %w", raddr.String(), err)
-	}
-
-	udpConn, ok := conn.(*net.UDPConn)
-	if !ok {
-		if err := conn.Close(); err != nil {
-			log.Errorf("Failed to close connection: %v", err)
-		}
-		return nil, fmt.Errorf("expected UDP connection, got different type")
-	}
-
-	return udpConn, nil
-}
-
-func DialTCP(network string, laddr, raddr *net.TCPAddr) (*net.TCPConn, error) {
-	dialer := NewDialer()
-	dialer.LocalAddr = laddr
-
-	conn, err := dialer.Dial(network, raddr.String())
-	if err != nil {
-		return nil, fmt.Errorf("dialing TCP %s: %w", raddr.String(), err)
-	}
-
-	tcpConn, ok := conn.(*net.TCPConn)
-	if !ok {
-		if err := conn.Close(); err != nil {
-			log.Errorf("Failed to close connection: %v", err)
-		}
-		return nil, fmt.Errorf("expected TCP connection, got different type")
-	}
-
-	return tcpConn, nil
 }
