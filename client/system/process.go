9430901bb6435bc08927c4514a9f875ed7e3a603
bcmmbaga <bethuelmbaga12@gmail.com>
2024-04-09 21:57:53 +0300

    skip check file and process for android and ios

--- client/system/process.go
+++ client/system/process.go
@@ -1,10 +1,8 @@
-//go:build windows || linux || darwin || !ios || !android
-// +build windows linux darwin !ios !android
-
 package system
 
 import (
 	"os"
+	"runtime"
 	"slices"
 
 	"github.com/shirou/gopsutil/process"
@@ -35,6 +33,10 @@ func getRunningProcesses() ([]string, error) {
 
 // checkFileAndProcess checks if the file path exists and if a process is running at that path.
 func checkFileAndProcess(paths []string) ([]File, error) {
+	if runtime.GOOS == "android" || runtime.GOOS == "ios" {
+		return []File{}, nil
+	}
+
 	files := make([]File, len(paths))
 	if len(paths) == 0 {
 		return files, nil
