a10c0d200ffe8f0595efa8cc9d777375a1d913cc
Pascal Fischer <pascal@netbird.io>
2024-04-23 12:31:52 +0200

    exclude custom routing for iOS

--- util/net/net.go
+++ util/net/net.go
@@ -2,6 +2,7 @@ package net
 
 import (
 	"os"
+	"runtime"
 
 	"github.com/google/uuid"
 )
@@ -23,5 +24,8 @@ func GenerateConnID() ConnectionID {
 }
 
 func CustomRoutingDisabled() bool {
+	if runtime.GOOS == "ios" {
+		return true
+	}
 	return os.Getenv(envDisableCustomRouting) == "true"
 }
