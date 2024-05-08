de8882f036da4dbe2d7a0fe20efc600cdf3f84ab
Pascal Fischer <pascal@netbird.io>
2024-05-08 10:17:28 +0200

    keep newRoutes for android case

--- client/internal/routemanager/notifier.go
+++ client/internal/routemanager/notifier.go
@@ -1,6 +1,7 @@
 package routemanager
 
 import (
+	"runtime"
 	"sort"
 	"strings"
 	"sync"
@@ -47,8 +48,15 @@ func (n *notifier) onNewRoutes(idMap route.HAMap) {
 	}
 
 	sort.Strings(newNets)
-	if !n.hasDiff(n.routeRanges, newNets) {
-		return
+	switch runtime.GOOS {
+	case "android":
+		if !n.hasDiff(n.initialRouteRanges, newNets) {
+			return
+		}
+	default:
+		if !n.hasDiff(n.routeRanges, newNets) {
+			return
+		}
 	}
 
 	n.routeRanges = newNets
