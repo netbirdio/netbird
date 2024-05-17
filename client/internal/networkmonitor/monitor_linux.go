af4dc6e010d23cb6254de68c30e529b3b6358e35
Naman Mudgal <naman.mudgal2002@gmail.com>
2024-05-17 10:16:00 +0530

    l

--- client/internal/networkmonitor/monitor_linux.go
+++ client/internal/networkmonitor/monitor_linux.go
@@ -10,3 +10,19 @@ func checkChange(ctx context.Context, nexthopv4 netip.Addr, intfv4 *net.Interfac
 	if err := netlink.LinkSubscribe(linkChan, done); err != nil {
 		return fmt.Errorf("subscribe to link updates: %v", err)
 	}
+	routeChan := make(chan netlink.RouteUpdate)
+	if err := netlink.RouteSubscribe(routeChan, done); err != nil {
+		return fmt.Errorf("subscribe to route updates: %v", err)
+	}
+
+	log.Info("Network monitor: started")
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+
+		// handle interface state changes
+		case update := <-linkChan:
+			if (intfv4 == nil || update.Index != int32(intfv4.Index)) && (intfv6 == nil || update.Index != int32(intfv6.Index)) {
+				continue
+			}
