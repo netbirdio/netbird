84b46f9e13379363b2b4b9151c5a7359a29b8272
Naman Mudgal <naman.mudgal2002@gmail.com>
2024-05-17 10:07:24 +0530

    netwrok generic

--- client/internal/networkmonitor/monitor_generic.go
+++ client/internal/networkmonitor/monitor_generic.go
@@ -3,3 +3,33 @@ func (nw *NetworkWatcher) Start(ctx context.Context, callback func()) {
 		log.Warn("Network monitor: already running, stopping previous watcher")
 		nw.Stop()
 	}
+	if ctx.Err() != nil {
+		log.Info("Network monitor: not starting, context is already cancelled")
+		return
+	}
+
+	ctx, nw.cancel = context.WithCancel(ctx)
+	defer nw.Stop()
+
+	var nexthop4, nexthop6 netip.Addr
+	var intf4, intf6 *net.Interface
+
+	operation := func() error {
+		var errv4, errv6 error
+		nexthop4, intf4, errv4 = routemanager.GetNextHop(netip.IPv4Unspecified())
+		nexthop6, intf6, errv6 = routemanager.GetNextHop(netip.IPv6Unspecified())
+
+		if errv4 != nil && errv6 != nil {
+			return errors.New("failed to get default next hops")
+		}
+
+		if errv4 == nil {
+			log.Debugf("Network monitor: IPv4 default route: %s, interface: %s", nexthop4, intf4.Name)
+		}
+		if errv6 == nil {
+			log.Debugf("Network monitor: IPv6 default route: %s, interface: %s", nexthop6, intf6.Name)
+		}
+
+		// continue if either route was found
+		return nil
+	}
