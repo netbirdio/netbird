a72fa9c887d91f4c735040ec81e4e94c72e097f7
Naman Mudgal <naman.mudgal2002@gmail.com>
2024-05-17 10:14:13 +0530

    l

--- client/internal/networkmonitor/monitor_linux.go
+++ client/internal/networkmonitor/monitor_linux.go
@@ -2,4 +2,11 @@ func checkChange(ctx context.Context, nexthopv4 netip.Addr, intfv4 *net.Interfac
 	if intfv4 == nil && intfv6 == nil {
 		return errors.New("no interfaces available")
 	}
-	//
+	
+	linkChan := make(chan netlink.LinkUpdate)
+	done := make(chan struct{})
+	defer close(done)
+
+	if err := netlink.LinkSubscribe(linkChan, done); err != nil {
+		return fmt.Errorf("subscribe to link updates: %v", err)
+	}
