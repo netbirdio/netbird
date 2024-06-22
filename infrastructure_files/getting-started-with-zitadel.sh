33c0cb9edd946ba460bfb42f6ea9eaf214790e70
Robert Neumann <robert.neumann01@gmail.com>
2024-06-22 18:20:00 +0200

    update crdb init and wait to new container name zdb

--- infrastructure_files/getting-started-with-zitadel.sh
+++ infrastructure_files/getting-started-with-zitadel.sh
@@ -50,7 +50,7 @@ check_jq() {
 wait_crdb() {
   set +e
   while true; do
-    if $DOCKER_COMPOSE_COMMAND exec -T crdb curl -sf -o /dev/null 'http://localhost:8080/health?ready=1'; then
+    if $DOCKER_COMPOSE_COMMAND exec -T zdb curl -sf -o /dev/null 'http://localhost:8080/health?ready=1'; then
       break
     fi
     echo -n " ."
@@ -63,12 +63,12 @@ wait_crdb() {
 init_crdb() {
   if [[ $ZITADEL_DATABASE == "cockroach" ]]; then
     echo -e "\nInitializing Zitadel's CockroachDB\n\n"
-    $DOCKER_COMPOSE_COMMAND up -d crdb
+    $DOCKER_COMPOSE_COMMAND up -d zrdb
     echo ""
     # shellcheck disable=SC2028
     echo -n "Waiting cockroachDB  to become ready "
     wait_crdb
-    $DOCKER_COMPOSE_COMMAND exec -T crdb /bin/bash -c "cp /cockroach/certs/* /zitadel-certs/ && cockroach cert create-client --overwrite --certs-dir /zitadel-certs/ --ca-key /zitadel-certs/ca.key zitadel_user && chown -R 1000:1000 /zitadel-certs/"
+    $DOCKER_COMPOSE_COMMAND exec -T zrdb /bin/bash -c "cp /cockroach/certs/* /zitadel-certs/ && cockroach cert create-client --overwrite --certs-dir /zitadel-certs/ --ca-key /zitadel-certs/ca.key zitadel_user && chown -R 1000:1000 /zitadel-certs/"
     handle_request_command_status $? "init_crdb failed" ""
   fi
 }
