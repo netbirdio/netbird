704dc00314dd847015efb952108ba0daa61283a9
Maycon Santos <mlsmaycon@gmail.com>
2024-06-25 16:44:48 +0200

    set EDNS0 only there is no extra options

--- client/internal/dns/upstream.go
+++ client/internal/dns/upstream.go
@@ -79,7 +79,9 @@ func (u *upstreamResolverBase) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 
 	log.WithField("question", r.Question[0]).Trace("received an upstream question")
 	// set the EDNS0 buffer size to 4096 bytes to support larger dns records
-	r.SetEdns0(4096, true)
+	if r.Extra == nil {
+		r.SetEdns0(4096, true)
+	}
 
 	select {
 	case <-u.ctx.Done():
