faeae523292e0492065ab1569e6c6877d24eac16
Zoltán Papp <zoltan.pmail@gmail.com>
2024-06-28 11:44:50 +0200

    Support exit node in ws client

--- relay/client/dialer/ws/ws.go
+++ relay/client/dialer/ws/ws.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"net/http"
 
 	log "github.com/sirupsen/logrus"
 	"nhooyr.io/websocket"
+
+	nbnet "github.com/netbirdio/netbird/util/net"
 )
 
 func Dial(address string) (net.Conn, error) {
@@ -21,7 +24,8 @@ func Dial(address string) (net.Conn, error) {
 
 	url := fmt.Sprintf("ws://%s:%d", addr.IP.String(), addr.Port)
 	opts := &websocket.DialOptions{
-		Host: hostName,
+		Host:       hostName,
+		HTTPClient: httpClientNbDialer(),
 	}
 
 	wsConn, _, err := websocket.Dial(context.Background(), url, opts)
@@ -33,4 +37,18 @@ func Dial(address string) (net.Conn, error) {
 	conn := NewConn(wsConn, addr)
 
 	return conn, nil
+}
+
+func httpClientNbDialer() *http.Client {
+	customDialer := nbnet.NewDialer()
+
+	customTransport := &http.Transport{
+		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			return customDialer.DialContext(ctx, network, addr)
+		},
+	}
+
+	return &http.Client{
+		Transport: customTransport,
+	}
 }
