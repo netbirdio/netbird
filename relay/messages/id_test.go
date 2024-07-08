931f165c9a48b20a06aeaf4130bdf0ff4f57621a
Zoltán Papp <zoltan.pmail@gmail.com>
2024-07-08 17:38:23 +0200

    Remove garbage

--- relay/messages/id_test.go
+++ relay/messages/id_test.go
@@ -1,10 +1,7 @@
 package messages
 
 import (
-	"encoding/binary"
 	"testing"
-
-	log "github.com/sirupsen/logrus"
 )
 
 func TestHashID(t *testing.T) {
@@ -13,14 +10,4 @@ func TestHashID(t *testing.T) {
 	if enc != hashedStringId {
 		t.Errorf("expected %s, got %s", hashedStringId, enc)
 	}
-
-	var magicHeader uint32 = 0x2112A442 // size 4 byte
-
-	msg := make([]byte, 4)
-	binary.BigEndian.PutUint32(msg, magicHeader)
-
-	magicHeader2 := []byte{0x21, 0x12, 0xA4, 0x42}
-
-	log.Infof("msg: %v, %v", msg, magicHeader2)
-
 }
