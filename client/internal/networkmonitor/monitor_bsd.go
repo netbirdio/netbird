940cac3cd5984fd78219f25ccae2a7e36180a179
Pascal Fischer <pascal@netbird.io>
2024-07-16 16:31:12 +0200

    extend logging for error handling

--- client/internal/networkmonitor/monitor_bsd.go
+++ client/internal/networkmonitor/monitor_bsd.go
@@ -20,22 +20,19 @@ func checkChange(ctx context.Context, nexthopv4, nexthopv6 systemops.Nexthop, ca
 	if err != nil {
 		return fmt.Errorf("failed to open routing socket: %v", err)
 	}
-	fdClosed := false
 	defer func() {
-		if fdClosed {
-			return
-		}
-		if err := unix.Close(fd); err != nil {
+		err := unix.Close(fd)
+		if err != nil && !errors.Is(err, unix.EBADF) {
 			log.Errorf("Network monitor: failed to close routing socket: %v", err)
 		}
 	}()
 
 	go func() {
 		<-ctx.Done()
-		if err := unix.Close(fd); err == nil {
+		err := unix.Close(fd)
+		if err != nil && !errors.Is(err, unix.EBADF) {
 			log.Debugf("Network monitor: closed routing socket")
 		}
-		fdClosed = true
 	}()
 
 	for {
@@ -45,10 +42,8 @@ func checkChange(ctx context.Context, nexthopv4, nexthopv6 systemops.Nexthop, ca
 		default:
 			buf := make([]byte, 2048)
 			n, err := unix.Read(fd, buf)
-			if err != nil {
-				if !fdClosed {
-					log.Errorf("Network monitor: failed to read from routing socket: %v", err)
-				}
+			if err != nil && !errors.Is(err, unix.EBADF) && !errors.Is(err, unix.EINVAL) {
+				log.Errorf("Network monitor: failed to read from routing socket: %v", err)
 				continue
 			}
 			if n < unix.SizeofRtMsghdr {
