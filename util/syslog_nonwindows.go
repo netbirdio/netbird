1d6f5482ddae3ae6805f279e180034a3b127c013
Jakub Kołodziejczak <31549762+mrl5@users.noreply.github.com>
2024-07-16 10:19:58 +0200

    feat(client): send logs to syslog (#2259)

---
+++ util/syslog_nonwindows.go
@@ -0,0 +1,20 @@
+//go:build !windows
+// +build !windows
+
+package util
+
+import (
+	"log/syslog"
+
+	log "github.com/sirupsen/logrus"
+	lSyslog "github.com/sirupsen/logrus/hooks/syslog"
+)
+
+func AddSyslogHook() {
+	hook, err := lSyslog.NewSyslogHook("", "", syslog.LOG_INFO, "")
+
+	if err != nil {
+		log.Errorf("Failed creating syslog hook: %s", err)
+	}
+	log.AddHook(hook)
+}
---
+++ util/syslog_windows.go
@@ -0,0 +1,3 @@
+package util
+
+func AddSyslogHook() {}
