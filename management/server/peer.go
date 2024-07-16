30bf0287073dcb8d085baa94c13c21cb1f954c8c
bcmmbaga <bethuelmbaga12@gmail.com>
2024-07-16 15:31:30 +0300

    Run peer updates in a separate goroutine

--- management/server/peer.go
+++ management/server/peer.go
@@ -934,6 +934,6 @@ func (am *DefaultAccountManager) updateAccountPeers(ctx context.Context, account
 		postureChecks := am.getPeerPostureChecks(account, peer)
 		remotePeerNetworkMap := account.GetPeerNetworkMap(ctx, peer.ID, am.dnsDomain, approvedPeersMap)
 		update := toSyncResponse(ctx, nil, peer, nil, remotePeerNetworkMap, am.GetDNSDomain(), postureChecks)
-		am.peersUpdateManager.SendUpdate(ctx, peer.ID, &UpdateMessage{Update: update, NetworkMap: remotePeerNetworkMap, Checks: postureChecks})
+		go am.peersUpdateManager.SendUpdate(ctx, peer.ID, &UpdateMessage{Update: update, NetworkMap: remotePeerNetworkMap, Checks: postureChecks})
 	}
 }
