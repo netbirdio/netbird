7279d58110148208f30724e33cae3977c5f59184
Zoltan Papp <zoltan.pmail@gmail.com>
2024-07-25 00:20:03 +0200

    Fix sonar issue

--- client/internal/peer/stdnet_android.go
+++ client/internal/peer/stdnet_android.go
@@ -3,5 +3,5 @@ package peer
 import "github.com/netbirdio/netbird/client/internal/stdnet"
 
 func (w *WorkerICE) newStdNet() (*stdnet.Net, error) {
-	return stdnet.NewNetWithDiscover(w.iFaceDiscover, w.configICE.InterfaceBlackList)
+	return stdnet.NewNetWithDiscover(w.iFaceDiscover, w.config.ICEConfig.InterfaceBlackList)
 }
