e1ee73500a8de2763510b2e51fd49f5fcda1b514
Zoltán Papp <zoltan.pmail@gmail.com>
2024-07-25 11:03:54 +0200

    Remove unused codes

--- relay/client/dialer/tcp/tcp.go
+++
@@ -1,7 +0,0 @@
-package tcp
-
-import "net"
-
-func Dial(address string) (net.Conn, error) {
-	return net.Dial("tcp", address)
-}
--- relay/server/server.go
+++ relay/server/server.go
@@ -3,8 +3,6 @@ package server
 import (
 	"context"
 	"crypto/tls"
-	"errors"
-	"sync"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -12,7 +10,6 @@ import (
 
 	"github.com/netbirdio/netbird/relay/auth"
 	"github.com/netbirdio/netbird/relay/server/listener"
-	"github.com/netbirdio/netbird/relay/server/listener/udp"
 	"github.com/netbirdio/netbird/relay/server/listener/ws"
 )
 
@@ -22,9 +19,8 @@ type ListenerConfig struct {
 }
 
 type Server struct {
-	relay       *Relay
-	uDPListener listener.Listener
-	wSListener  listener.Listener
+	relay      *Relay
+	wSListener listener.Listener
 }
 
 func NewServer(meter metric.Meter, exposedAddress string, tlsSupport bool, authValidator auth.Validator) (*Server, error) {
@@ -38,9 +34,6 @@ func NewServer(meter metric.Meter, exposedAddress string, tlsSupport bool, authV
 }
 
 func (r *Server) Listen(cfg ListenerConfig) error {
-	wg := sync.WaitGroup{}
-	wg.Add(2)
-
 	r.wSListener = &ws.Listener{
 		Address:   cfg.Address,
 		TLSConfig: cfg.TLSConfig,
@@ -48,46 +41,26 @@ func (r *Server) Listen(cfg ListenerConfig) error {
 
 	var wslErr error
 	go func() {
-		defer wg.Done()
 		wslErr = r.wSListener.Listen(r.relay.Accept)
 		if wslErr != nil {
 			log.Errorf("failed to bind ws server: %s", wslErr)
 		}
 	}()
 
-	r.uDPListener = udp.NewListener(cfg.Address)
-	var udpLErr error
-	go func() {
-		defer wg.Done()
-		udpLErr = r.uDPListener.Listen(r.relay.Accept)
-		if udpLErr != nil {
-			log.Errorf("failed to bind ws server: %s", udpLErr)
-		}
-	}()
-
-	err := errors.Join(wslErr, udpLErr)
-	return err
+	return wslErr
 }
 
-func (r *Server) Close() error {
-	var wErr error
+func (r *Server) Close() (err error) {
 	// stop service new connections
 	if r.wSListener != nil {
-		wErr = r.wSListener.Close()
-	}
-
-	var uErr error
-	if r.uDPListener != nil {
-		uErr = r.uDPListener.Close()
+		err = r.wSListener.Close()
 	}
 
 	// close accepted connections gracefully
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	r.relay.Close(ctx)
-
-	err := errors.Join(wErr, uErr)
-	return err
+	return
 }
 
 func (r *Server) InstanceURL() string {
