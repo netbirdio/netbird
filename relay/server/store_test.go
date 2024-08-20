3a4511665f6bc06c517064fb512b1543188473fa
Zoltán Papp <zoltan.pmail@gmail.com>
2024-08-20 18:00:18 +0200

    Fix store_test
    Add missing metrics parameter

--- relay/server/store_test.go
+++ relay/server/store_test.go
@@ -1,12 +1,20 @@
 package server
 
 import (
+	"context"
 	"testing"
+
+	"go.opentelemetry.io/otel"
+
+	"github.com/netbirdio/netbird/relay/metrics"
 )
 
 func TestStore_DeletePeer(t *testing.T) {
 	s := NewStore()
-	p := NewPeer([]byte("peer_one"), nil, nil)
+
+	m, _ := metrics.NewMetrics(context.Background(), otel.Meter(""))
+
+	p := NewPeer(m, []byte("peer_one"), nil, nil)
 	s.AddPeer(p)
 	s.DeletePeer(p)
 	if _, ok := s.Peer(p.String()); ok {
@@ -17,8 +25,10 @@ func TestStore_DeletePeer(t *testing.T) {
 func TestStore_DeleteDeprecatedPeer(t *testing.T) {
 	s := NewStore()
 
-	p1 := NewPeer([]byte("peer_id"), nil, nil)
-	p2 := NewPeer([]byte("peer_id"), nil, nil)
+	m, _ := metrics.NewMetrics(context.Background(), otel.Meter(""))
+
+	p1 := NewPeer(m, []byte("peer_id"), nil, nil)
+	p2 := NewPeer(m, []byte("peer_id"), nil, nil)
 
 	s.AddPeer(p1)
 	s.AddPeer(p2)
