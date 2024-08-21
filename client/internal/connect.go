c6e48b590d9fea730965aea4d7cbc9751ce7a4f5
Maycon Santos <mlsmaycon@gmail.com>
2024-08-21 15:07:22 +0200

    Update client/internal/connect.go
    Co-authored-by: Viktor Liu <17948409+lixmal@users.noreply.github.com>

--- client/internal/connect.go
+++ client/internal/connect.go
@@ -430,6 +430,6 @@ func closeConnWithLog(conn *net.UDPConn) {
 		log.Warnf("closing probe port %d failed: %v. NetBird will still attempt to use this port for connection.", conn.LocalAddr().(*net.UDPAddr).Port, err)
 	}
 	if time.Since(startClosing) > time.Second {
-		log.Warnf("closing the testing port %d took %s. Usually is safe to ignore, but continuous warnings may indicate a problem.", conn.LocalAddr().(*net.UDPAddr).Port, time.Since(startClosing))
+		log.Warnf("closing the testing port %d took %s. Usually it is safe to ignore, but continuous warnings may indicate a problem.", conn.LocalAddr().(*net.UDPAddr).Port, time.Since(startClosing))
 	}
 }
