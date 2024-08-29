80c172ffafc9e54eb3531c916101c5f36db7ded7
Zoltán Papp <zoltan.pmail@gmail.com>
2024-08-29 16:39:54 +0200

    Change Error level to Debug

--- relay/auth/hmac/validator.go
+++ relay/auth/hmac/validator.go
@@ -1,8 +1,9 @@
 package hmac
 
 import (
-	log "github.com/sirupsen/logrus"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type TimedHMACValidator struct {
@@ -20,7 +21,7 @@ func (a *TimedHMACValidator) Validate(credentials any) error {
 	b := credentials.([]byte)
 	c, err := unmarshalToken(b)
 	if err != nil {
-		log.Errorf("failed to unmarshal token: %s", err)
+		log.Debugf("failed to unmarshal token: %s", err)
 		return err
 	}
 	return a.TimedHMAC.Validate(c)
