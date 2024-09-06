6091a80b1761b939f99f5b0904fb6823699bd379
Maycon Santos <mlsmaycon@gmail.com>
2024-09-06 18:29:33 +0200

    Support exposed address with scheme
    This allows relay to run behind proxy doing TLS termination

--- relay/server/relay.go
+++ relay/server/relay.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"net/url"
+	"strings"
 	"sync"
 
 	log "github.com/sirupsen/logrus"
@@ -61,17 +62,39 @@ func NewRelay(meter metric.Meter, exposedAddress string, tlsSupport bool, valida
 		store:         NewStore(),
 	}
 
-	if tlsSupport {
-		r.instanceURL = fmt.Sprintf("rels://%s", exposedAddress)
-	} else {
-		r.instanceURL = fmt.Sprintf("rel://%s", exposedAddress)
-	}
-	_, err = url.ParseRequestURI(r.instanceURL)
+	r.instanceURL, err = getInstanceURL(exposedAddress, tlsSupport)
 	if err != nil {
-		return nil, fmt.Errorf("invalid exposed address: %v", err)
+		metricsCancel()
+		return nil, fmt.Errorf("get instance URL: %v", err)
 	}
 
 	return r, nil
+}
+
+// getInstanceURL checks if user supplied a URL scheme otherwise adds to the
+// provided address according to TLS definition and parses the address before returning it
+func getInstanceURL(exposedAddress string, tlsSupported bool) (string, error) {
+	addr := exposedAddress
+	split := strings.Split(exposedAddress, "://")
+	switch {
+	case len(split) == 1 && tlsSupported:
+		addr = "rels://" + exposedAddress
+	case len(split) == 1 && !tlsSupported:
+		addr = "rel://" + exposedAddress
+	case len(split) > 2:
+		return "", fmt.Errorf("invalid exposed address: %s", exposedAddress)
+	}
+
+	parsedURL, err := url.ParseRequestURI(addr)
+	if err != nil {
+		return "", fmt.Errorf("invalid exposed address: %v", err)
+	}
+
+	if parsedURL.Scheme != "rel" && parsedURL.Scheme != "rels" {
+		return "", fmt.Errorf("invalid scheme: %s", parsedURL.Scheme)
+	}
+
+	return parsedURL.String(), nil
 }
 
 // Accept start to handle a new peer connection
