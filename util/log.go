94bf102ca7d3754d05a23845596a105eb8366bd1
Maycon Santos <mlsmaycon@gmail.com>
2024-09-12 19:16:18 +0200

    Support configurable max log size with var NB_LOG_MAX_SIZE_MB

--- util/log.go
+++ util/log.go
@@ -5,12 +5,15 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
+	"strconv"
 
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/natefinch/lumberjack.v2"
 
 	"github.com/netbirdio/netbird/formatter"
 )
+
+const defaultLogSize = 5
 
 // InitLog parses and sets log-level input
 func InitLog(logLevel string, logPath string) error {
@@ -19,13 +22,14 @@ func InitLog(logLevel string, logPath string) error {
 		log.Errorf("Failed parsing log-level %s: %s", logLevel, err)
 		return err
 	}
-	customOutputs := []string{"console", "syslog"};
+	customOutputs := []string{"console", "syslog"}
 
 	if logPath != "" && !slices.Contains(customOutputs, logPath) {
+		maxLogSize := getLogMaxSize()
 		lumberjackLogger := &lumberjack.Logger{
 			// Log file absolute path, os agnostic
 			Filename:   filepath.ToSlash(logPath),
-			MaxSize:    5, // MB
+			MaxSize:    maxLogSize, // MB
 			MaxBackups: 10,
 			MaxAge:     30, // days
 			Compress:   true,
@@ -45,4 +49,17 @@ func InitLog(logLevel string, logPath string) error {
 	}
 	log.SetLevel(level)
 	return nil
+}
+
+func getLogMaxSize() int {
+	if sizeVar, ok := os.LookupEnv("NB_LOG_MAX_SIZE_MB"); ok {
+		size, err := strconv.ParseInt(sizeVar, 10, 64)
+		if err != nil {
+			log.Errorf("Failed parsing log-size %s: %s", sizeVar, err)
+			return defaultLogSize
+		}
+
+		return int(size)
+	}
+	return defaultLogSize
 }
