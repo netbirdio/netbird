10856d36abaa82c33916ff01ecb16150f7d71bee
Zoltán Papp <zoltan.pmail@gmail.com>
2024-09-19 11:05:53 +0200

    Exit from processConnResults after all tries
    If all server is unavailable then the server picker never return
    because we never close the result channel.
    Count the number of the results and exit when we reached the
    expected size

---
+++ relay/client/picker_test.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestServerPicker_UnavailableServers(t *testing.T) {
+	sp := ServerPicker{
+		TokenStore: nil,
+		PeerID:     "test",
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	go func() {
+		_, err := sp.PickServer(ctx, []string{"rel://dummy1", "rel://dummy2"})
+		if err == nil {
+			t.Error(err)
+		}
+		cancel()
+	}()
+
+	select {
+	case <-ctx.Done():
+		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+			t.Errorf("PickServer() took too long to complete")
+		}
+	}
+}
