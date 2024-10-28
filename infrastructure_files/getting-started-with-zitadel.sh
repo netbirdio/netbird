3a0822625006a15b557e73e0a094c0974722f044
Stefano <stefano@stefanocoding.me>
2024-10-28 10:08:17 +0100

    [misc] Update Zitadel from v2.54.10 to v2.64.1

--- infrastructure_files/getting-started-with-zitadel.sh
+++ infrastructure_files/getting-started-with-zitadel.sh
@@ -873,7 +873,7 @@ services:
   zitadel:
     restart: 'always'
     networks: [netbird]
-    image: 'ghcr.io/zitadel/zitadel:v2.54.10'
+    image: 'ghcr.io/zitadel/zitadel:v2.64.1'
     command: 'start-from-init --masterkeyFromEnv --tlsMode $ZITADEL_TLS_MODE'
     env_file:
       - ./zitadel.env
