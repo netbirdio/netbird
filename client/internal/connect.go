3ccebeed1e5a1307faaf3c829dee9054a4d1f81d
Viktor Liu <17948409+lixmal@users.noreply.github.com>
2024-11-15 22:59:49 +0100

    Fix error state race on mgmt connection error (#2892)

--- client/internal/connect.go
+++ client/internal/connect.go
@@ -157,7 +157,8 @@ func (c *ConnectClient) run(mobileDependency MobileDependency, probes *ProbeHold
 
 		engineCtx, cancel := context.WithCancel(c.ctx)
 		defer func() {
-			c.statusRecorder.MarkManagementDisconnected(state.err)
+			_, err := state.Status()
+			c.statusRecorder.MarkManagementDisconnected(err)
 			c.statusRecorder.CleanLocalPeerState()
 			cancel()
 		}()
