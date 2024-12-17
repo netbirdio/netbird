4a7988b0c5eebecbabe3bf71b5bfdc582ab75ae5
Pascal Fischer <pascal@netbird.io>
2024-12-17 01:27:04 +0100

    fix accountid

--- management/server/grpcserver.go
+++ management/server/grpcserver.go
@@ -484,7 +484,7 @@ func (s *GRPCServer) Login(ctx context.Context, req *proto.EncryptedMessage) (*p
 		}
 	}
 
-	settings, err := s.settingsManager.GetSettings(ctx, peer.AccountID, userID)
+	settings, err := s.settingsManager.GetSettings(ctx, accountID, userID)
 	if err != nil {
 		log.WithContext(ctx).Errorf("failed to get settings for account %s and user %s: %v", accountID, userID, err)
 		return nil, mapError(ctx, err)
