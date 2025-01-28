9d83f8b2f03b9dc0387c11d4d14b4c5b1e13eb94
bcmmbaga <bethuelmbaga12@gmail.com>
2025-01-28 18:17:36 +0300

    Extend peers manager to support retrieving all peers
    Signed-off-by: bcmmbaga <bethuelmbaga12@gmail.com>

--- management/server/peers/manager.go
+++ management/server/peers/manager.go
@@ -12,6 +12,7 @@ import (
 
 type Manager interface {
 	GetPeer(ctx context.Context, accountID, userID, peerID string) (*peer.Peer, error)
+	GetAllPeers(ctx context.Context, accountID, userID string) ([]*peer.Peer, error)
 }
 
 type managerImpl struct {
@@ -40,4 +41,17 @@ func (m *managerImpl) GetPeer(ctx context.Context, accountID, userID, peerID str
 	}
 
 	return m.store.GetPeerByID(ctx, store.LockingStrengthShare, accountID, peerID)
+}
+
+func (m *managerImpl) GetAllPeers(ctx context.Context, accountID, userID string) ([]*peer.Peer, error) {
+	allowed, err := m.permissionsManager.ValidateUserPermissions(ctx, accountID, userID, permissions.Peers, permissions.Read)
+	if err != nil {
+		return nil, fmt.Errorf("failed to validate user permissions: %w", err)
+	}
+
+	if !allowed {
+		return nil, status.NewPermissionDeniedError()
+	}
+
+	return m.store.GetAccountPeers(ctx, store.LockingStrengthShare, accountID)
 }
