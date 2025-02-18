a010728b935d625c7ccd3815457988c8c83bd782
Pascal Fischer <pascal@netbird.io>
2025-02-18 11:39:21 +0100

    change dbname

--- management/server/store/store.go
+++ management/server/store/store.go
@@ -445,9 +445,9 @@ func newReusedMysqlStore(ctx context.Context, store *SqlStore, kind Engine) (*Sq
 }
 
 func createRandomDB(dsn string, db *gorm.DB, engine Engine) (string, func(), error) {
-	dbName := fmt.Sprintf("test_db_%s", uuid.New().String())
+	dbName := fmt.Sprintf("test_db_%s", strings.Replace(uuid.New().String(), "-", "_", -1))
 
-	if err := db.Exec(fmt.Sprintf("CREATE DATABASE '%s'", dbName)).Error; err != nil {
+	if err := db.Exec(fmt.Sprintf("CREATE DATABASE %s", dbName)).Error; err != nil {
 		return "", nil, fmt.Errorf("failed to create database: %v", err)
 	}
 
@@ -461,10 +461,10 @@ func createRandomDB(dsn string, db *gorm.DB, engine Engine) (string, func(), err
 	cleanup := func() {
 		switch engine {
 		case PostgresStoreEngine:
-			err = db.Exec(fmt.Sprintf("DROP DATABASE '%s' WITH (FORCE)", dbName)).Error
+			err = db.Exec(fmt.Sprintf("DROP DATABASE %s WITH (FORCE)", dbName)).Error
 		case MysqlStoreEngine:
 			// err = killMySQLConnections(dsn, dbName)
-			err = db.Exec(fmt.Sprintf("DROP DATABASE '%s'", dbName)).Error
+			err = db.Exec(fmt.Sprintf("DROP DATABASE %s", dbName)).Error
 		}
 		if err != nil {
 			log.Errorf("failed to drop database %s: %v", dbName, err)
