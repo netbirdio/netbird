76d716a79c1be6516d521886494b9a266d6023f3
Pascal Fischer <pascal@netbird.io>
2025-02-18 11:25:02 +0100

    extract store creation in test

--- management/server/nameserver_test.go
+++ management/server/nameserver_test.go
@@ -376,12 +376,12 @@ func TestCreateNameServerGroup(t *testing.T) {
 		},
 	}
 	for _, testCase := range testCases {
-		t.Run(testCase.name, func(t *testing.T) {
-			am, err := createNSManager(t)
-			if err != nil {
-				t.Fatalf("failed to create account manager: %s", err)
-			}
+		am, err := createNSManager(t)
+		if err != nil {
+			t.Fatalf("failed to create account manager: %s", err)
+		}
 
+		t.Run(testCase.name, func(t *testing.T) {
 			account, err := initTestNSAccount(t, am)
 			if err != nil {
 				t.Fatalf("failed to init testing account: %s", err)
