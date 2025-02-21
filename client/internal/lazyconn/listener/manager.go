ironman-ccp/netbird-frk

Initial concept

client/internal/lazyconn/listener/manager.go
@@ -1,0 +1,135 @@
+package listener
+
+import (
+	"fmt"
+	"net"
+
+	log "github.com/sirupsen/logrus"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+
+	"github.com/netbirdio/netbird/client/internal/lazyconn/wgiface"
+)
+
+type portGenerator struct {
+	nextFreePort uint16
+}
+
+func newPortGenerator() *portGenerator {
+	return &portGenerator{
+		nextFreePort: 1,
+	}
+}
+
+func (p *portGenerator) nextPort() int {
+	port := p.nextFreePort
+	p.nextFreePort++
+	if p.nextFreePort == 0 {
+		p.nextFreePort = 1
+	}
+	return int(port)
+}
+
+type Manager struct {
+	TrafficStartChan chan wgtypes.Key
+
+	wgIface wgiface.WGIface
+
+	portGenerator *portGenerator
+	peers         map[wgtypes.Key]*Listener
+	done          chan struct{}
+}
+
+func NewManager(wgIface wgiface.WGIface) *Manager {
+	m := &Manager{
+		TrafficStartChan: make(chan wgtypes.Key, 1),
+		wgIface:          wgIface,
+		portGenerator:    newPortGenerator(),
+		done:             make(chan struct{}),
+	}
+	return m
+}
+
+func (m *Manager) CreateFakePeers(peers []wgiface.PeerConfig) error {
+	for _, peerCfg := range peers {
+		if _, ok := m.peers[peerCfg.PublicKey]; ok {
+			continue
+		}
+
+		if err := m.createFakePeer(peerCfg); err != nil {
+			return err
+		}
+		log.Debugf("created lazy connection listener for: %s", peerCfg.PublicKey.String())
+	}
+	return nil
+}
+
+func (m *Manager) RemovePeer(peerID wgtypes.Key) {
+	listener, ok := m.peers[peerID]
+	if !ok {
+		return
+	}
+
+	listener.Close()
+
+	if err := m.wgIface.RemovePeer(peerID); err != nil {
+		log.Warnf("failed to remove fake peer: %v", err)
+	}
+
+	delete(m.peers, peerID)
+}
+
+func (m *Manager) Close() {
+	close(m.done)
+	for peerID, listener := range m.peers {
+		listener.Close()
+		delete(m.peers, peerID)
+	}
+}
+
+func (m *Manager) createFakePeer(peerCfg wgiface.PeerConfig) error {
+	var (
+		listener *Listener
+		err      error
+		addr     *net.UDPAddr
+	)
+	for i := 0; i < 100; i++ {
+		addr = &net.UDPAddr{
+			Port: m.portGenerator.nextPort(),
+			IP:   net.ParseIP("127.0.0.254"),
+		}
+		listener, err = NewListener(peerCfg.PublicKey, addr)
+		if err != nil {
+			log.Debugf("failed to allocate port: %d: %v", addr.Port, err)
+			continue
+		}
+	}
+
+	if listener == nil {
+		return fmt.Errorf("failed to allocate lazy connection port for: %s", peerCfg.PublicKey.String())
+	}
+
+	if err := m.createEndpoint(peerCfg, addr); err != nil {
+		log.Errorf("failed to create endpoint for %s: %v", peerCfg.PublicKey.String(), err)
+		listener.Close()
+		return err
+	}
+
+	go listener.ReadPackets(m.onTrigger)
+
+	m.peers[peerCfg.PublicKey] = listener
+	return nil
+}
+
+func (m *Manager) onTrigger(peerID wgtypes.Key) {
+	if err := m.wgIface.RemovePeer(peerID); err != nil {
+		log.Errorf("failed to remove peer: %v", err)
+	}
+	select {
+	case <-m.done:
+	case m.TrafficStartChan <- peerID:
+	}
+}
+
+func (m *Manager) createEndpoint(peerCfg wgiface.PeerConfig, endpoint *net.UDPAddr) error {
+	return m.wgIface.UpdatePeer(peerCfg.PublicKey.String(), peerCfg.AllowedIP.String(), 0, endpoint, nil)
+}

client/internal/lazyconn/watcher/watcher.go
@@ -1,0 +1,104 @@
+package watcher
+
+import (
+	"context"
+	"sync"
+
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+
+	"github.com/netbirdio/netbird/client/iface/configurer"
+	"github.com/netbirdio/netbird/client/internal/lazyconn/wgiface"
+)
+
+const (
+	checkPeriod       = 75 * time.Second // 3 * keep alive time (25s)
+	expectedMinimumRx = 90 * 2           // 2x keep alive packets
+)
+
+type rxHistory struct {
+	received int64
+}
+
+type Watcher struct {
+	PeerTimedOutChan chan wgtypes.Key
+
+	wgIface wgiface.WGIface
+
+	peers   map[wgtypes.Key]*rxHistory
+	peersMu sync.Mutex
+}
+
+func NewWatcher(wgIface wgiface.WGIface) *Watcher {
+	return &Watcher{
+		PeerTimedOutChan: make(chan wgtypes.Key, 1),
+		wgIface:          wgIface,
+		peers:            make(map[wgtypes.Key]*rxHistory),
+	}
+}
+
+func (m *Watcher) Watch(ctx context.Context) {
+	timer := time.NewTimer(checkPeriod)
+	defer timer.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-timer.C:
+			stats, err := m.wgIface.Transfers()
+			if err != nil {
+				log.Errorf("failed to get peer stats: %s", err)
+				continue
+			}
+			m.checkTimeouts(ctx, stats)
+		}
+	}
+}
+
+func (m *Watcher) AddPeer(peerID wgtypes.Key) {
+	m.peersMu.Lock()
+	defer m.peersMu.Unlock()
+
+	m.peers[peerID] = &rxHistory{}
+}
+
+func (m *Watcher) RemovePeer(id wgtypes.Key) {
+	m.peersMu.Lock()
+	defer m.peersMu.Unlock()
+
+	delete(m.peers, id)
+}
+
+// Todo: this is a naive implementation, we must to finish it
+func (m *Watcher) checkTimeouts(ctx context.Context, allPeersStats map[wgtypes.Key]configurer.WGStats) {
+	m.peersMu.Lock()
+	defer m.peersMu.Unlock()
+
+	for p, rxh := range m.peers {
+		s, ok := allPeersStats[p]
+		if !ok {
+			log.Warnf("no stats for peer %s", p)
+		}
+
+		// received bytes since last check
+		received := s.RxBytes - rxh.received
+		if received >= expectedMinimumRx {
+			rxh.received = s.RxBytes
+			continue
+		}
+
+		// todo handle that case when swtich from P2P to Relay and the endpoint has been reseted.
+
+		// peer timed out
+		delete(m.peers, p)
+
+		select {
+		case <-ctx.Done():
+			return
+		case m.PeerTimedOutChan <- p:
+		}
+	}
+}

netbirdio/netbird

client/internal/lazyconn/listener/manager.go
@@ -1,0 +1,133 @@
+package listener
+
+import (
+	"fmt"
+	"net"
+
+	log "github.com/sirupsen/logrus"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+
+	"github.com/netbirdio/netbird/client/internal/lazyconn"
+)
+
+type portGenerator struct {
+	nextFreePort uint16
+}
+
+func newPortGenerator() *portGenerator {
+	return &portGenerator{
+		nextFreePort: 65535,
+	}
+}
+
+func (p *portGenerator) nextPort() int {
+	port := p.nextFreePort
+	p.nextFreePort--
+	if p.nextFreePort == 0 {
+		p.nextFreePort = 65535
+	}
+	return int(port)
+}
+
+type Manager struct {
+	TrafficStartChan chan wgtypes.Key
+
+	wgIface lazyconn.WGIface
+
+	portGenerator *portGenerator
+	peers         map[wgtypes.Key]*Listener
+	done          chan struct{}
+}
+
+func NewManager(wgIface lazyconn.WGIface) *Manager {
+	m := &Manager{
+		TrafficStartChan: make(chan wgtypes.Key, 1),
+		wgIface:          wgIface,
+		portGenerator:    newPortGenerator(),
+		done:             make(chan struct{}),
+	}
+	return m
+}
+
+func (m *Manager) CreateFakePeers(peerCfg lazyconn.PeerConfig) error {
+	if _, ok := m.peers[peerCfg.PublicKey]; ok {
+		return nil
+	}
+
+	if err := m.createFakePeer(peerCfg); err != nil {
+		return err
+	}
+	log.Debugf("created lazy connection listener for: %s", peerCfg.PublicKey.String())
+	return nil
+}
+
+func (m *Manager) RemovePeer(peerID wgtypes.Key) {
+	listener, ok := m.peers[peerID]
+	if !ok {
+		return
+	}
+
+	listener.Close()
+
+	if err := m.wgIface.RemovePeer(peerID); err != nil {
+		log.Warnf("failed to remove fake peer: %v", err)
+	}
+
+	delete(m.peers, peerID)
+}
+
+func (m *Manager) Close() {
+	close(m.done)
+	for peerID, listener := range m.peers {
+		listener.Close()
+		delete(m.peers, peerID)
+	}
+}
+
+func (m *Manager) createFakePeer(peerCfg lazyconn.PeerConfig) error {
+	var (
+		listener *Listener
+		err      error
+		addr     *net.UDPAddr
+	)
+	for i := 0; i < 100; i++ {
+		addr = &net.UDPAddr{
+			Port: m.portGenerator.nextPort(),
+			IP:   net.ParseIP("127.0.0.254"),
+		}
+		listener, err = NewListener(peerCfg.PublicKey, addr)
+		if err != nil {
+			log.Debugf("failed to allocate port: %d: %v", addr.Port, err)
+			continue
+		}
+	}
+
+	if listener == nil {
+		return fmt.Errorf("failed to allocate lazy connection port for: %s", peerCfg.PublicKey.String())
+	}
+
+	if err := m.createEndpoint(peerCfg, addr); err != nil {
+		log.Errorf("failed to create endpoint for %s: %v", peerCfg.PublicKey.String(), err)
+		listener.Close()
+		return err
+	}
+
+	go listener.ReadPackets(m.onTrigger)
+
+	m.peers[peerCfg.PublicKey] = listener
+	return nil
+}
+
+func (m *Manager) onTrigger(peerID wgtypes.Key) {
+	if err := m.wgIface.RemovePeer(peerID); err != nil {
+		log.Errorf("failed to remove peer: %v", err)
+	}
+	select {
+	case <-m.done:
+	case m.TrafficStartChan <- peerID:
+	}
+}
+
+func (m *Manager) createEndpoint(peerCfg lazyconn.PeerConfig, endpoint *net.UDPAddr) error {
+	return m.wgIface.UpdatePeer(peerCfg.PublicKey.String(), peerCfg.AllowedIP.String(), 0, endpoint, nil)
+}

client/internal/lazyconn/watcher/watcher.go
@@ -1,0 +1,103 @@
+package watcher
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+
+	"github.com/netbirdio/netbird/client/iface/configurer"
+	"github.com/netbirdio/netbird/client/internal/lazyconn"
+)
+
+const (
+	checkPeriod       = 75 * time.Second // 3 * keep alive time (25s)
+	expectedMinimumRx = 90 * 2           // 2x keep alive packets
+)
+
+type rxHistory struct {
+	received int64
+}
+
+type Watcher struct {
+	PeerTimedOutChan chan wgtypes.Key
+
+	wgIface lazyconn.WGIface
+
+	peers   map[wgtypes.Key]*rxHistory
+	peersMu sync.Mutex
+}
+
+func NewWatcher(wgIface lazyconn.WGIface) *Watcher {
+	return &Watcher{
+		PeerTimedOutChan: make(chan wgtypes.Key, 1),
+		wgIface:          wgIface,
+		peers:            make(map[wgtypes.Key]*rxHistory),
+	}
+}
+
+func (m *Watcher) Watch(ctx context.Context) {
+	timer := time.NewTimer(checkPeriod)
+	defer timer.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-timer.C:
+			stats, err := m.wgIface.Transfers()
+			if err != nil {
+				log.Errorf("failed to get peer stats: %s", err)
+				continue
+			}
+			m.checkTimeouts(ctx, stats)
+		}
+	}
+}
+
+func (m *Watcher) AddPeer(peerID wgtypes.Key) {
+	m.peersMu.Lock()
+	defer m.peersMu.Unlock()
+
+	m.peers[peerID] = &rxHistory{}
+}
+
+func (m *Watcher) RemovePeer(id wgtypes.Key) {
+	m.peersMu.Lock()
+	defer m.peersMu.Unlock()
+
+	delete(m.peers, id)
+}
+
+// Todo: this is a naive implementation, we must to finish it
+func (m *Watcher) checkTimeouts(ctx context.Context, allPeersStats map[wgtypes.Key]configurer.WGStats) {
+	m.peersMu.Lock()
+	defer m.peersMu.Unlock()
+
+	for p, rxh := range m.peers {
+		s, ok := allPeersStats[p]
+		if !ok {
+			log.Warnf("no stats for peer %s", p)
+		}
+
+		// received bytes since last check
+		received := s.RxBytes - rxh.received
+		if received >= expectedMinimumRx {
+			rxh.received = s.RxBytes
+			continue
+		}
+
+		// todo handle that case when swtich from P2P to Relay and the endpoint has been reseted.
+
+		// peer timed out
+		delete(m.peers, p)
+
+		select {
+		case <-ctx.Done():
+			return
+		case m.PeerTimedOutChan <- p:
+		}
+	}
+}