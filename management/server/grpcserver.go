f443c7a51c108a6019cddd4831a92d23d6892ffe
Pascal Fischer <pascal@netbird.io>
2025-02-26 12:17:03 +0100

    make enabled flag not optional

--- management/server/grpcserver.go
+++ management/server/grpcserver.go
@@ -595,16 +595,14 @@ func toNetbirdConfig(config *Config, turnCredentials *Token, relayToken *Token, 
 	}
 
 	var flowCfg *proto.FlowConfig
-	if config.Flow != nil {
+	if config.Flow != nil && flowEnabled != nil && relayToken != nil {
 		flowCfg = &proto.FlowConfig{
 			Url:      config.Flow.Address,
 			Interval: durationpb.New(config.Flow.Interval.Duration),
-			Enabled:  flowEnabled,
-		}
-		if relayToken != nil {
+			Enabled:  *flowEnabled,
 			// for now we intend to use the relay secret for the flow
-			flowCfg.TokenPayload = relayToken.Payload
-			flowCfg.TokenSignature = relayToken.Signature
+			TokenPayload:   relayToken.Payload,
+			TokenSignature: relayToken.Signature,
 		}
 	}
 
