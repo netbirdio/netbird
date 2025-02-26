59f0c88d91499ac631cb8005a13d0d13bf64ea15
Pascal Fischer <pascal@netbird.io>
2025-02-26 11:43:02 +0100

    add extra settings interface

---
+++ management/server/integrations/flow/manager.go
@@ -0,0 +1,12 @@
+package flow
+
+import (
+	"context"
+
+	"github.com/netbirdio/netbird/management/server/account"
+)
+
+type Manager interface {
+	GetExtraSettings(ctx context.Context, accountID string) (*account.ExtraSettings, error)
+	UpdateExtraSettings(ctx context.Context, accountID string, extraSettings *account.ExtraSettings) error
+}
