994b923d56c6b7f5b470c5a941ca81140dbce086
Viktor Liu <17948409+lixmal@users.noreply.github.com>
2025-02-27 12:52:33 +0100

    Move proto and rename port and icmp info (#3399)

--- flow/proto/generate.sh
+++ flow/proto/generate.sh
@@ -13,5 +13,5 @@ script_path=$(dirname $(realpath "$0"))
 cd "$script_path"
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
-protoc -I ./ ./management.proto --go_out=../ --go-grpc_out=../
+protoc -I ./ ./flow.proto --go_out=../ --go-grpc_out=../
 cd "$old_pwd"
