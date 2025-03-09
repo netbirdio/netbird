036cecbf4650c82f02366be859cdd6577f26e4e4
Maycon Santos <mlsmaycon@gmail.com>
2025-03-09 18:47:05 +0100

    update integrations and go mod

--- management/server/grpcserver.go
+++ management/server/grpcserver.go
@@ -12,7 +12,7 @@ import (
 	pb "github.com/golang/protobuf/proto" // nolint
 	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/realip"
-	"github.com/netbirdio/management-integrations/integrations"
+	integrationsConfig "github.com/netbirdio/management-integrations/integrations/config"
 	log "github.com/sirupsen/logrus"
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 	"google.golang.org/grpc/codes"
@@ -605,7 +605,7 @@ func toNetbirdConfig(config *Config, turnCredentials *Token, relayToken *Token, 
 		Relay: relayCfg,
 	}
 
-	integrations.ExtendNetBirdConfig(nbConfig, extraSettings)
+	integrationsConfig.ExtendNetBirdConfig(nbConfig, extraSettings)
 
 	return nbConfig
 }
