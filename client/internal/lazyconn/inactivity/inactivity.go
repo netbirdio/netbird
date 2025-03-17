20de3b894ae8945e03fe9fbcc9bdc6b1140d6331
Zoltán Papp <zoltan.pmail@gmail.com>
2025-03-17 14:58:48 +0100

    Read inactivity threshold from env

--- client/internal/lazyconn/inactivity/inactivity.go
+++ client/internal/lazyconn/inactivity/inactivity.go
@@ -2,32 +2,36 @@ package inactivity
 
 import (
 	"context"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/netbirdio/netbird/client/internal/peer"
 )
 
 const (
-	inactivityThreshold = 60 * time.Minute // idle after 1 hour inactivity
+	defaultInactivityThreshold = 60 * time.Minute // idle after 1 hour inactivity
 )
 
 type Monitor struct {
-	id     peer.ConnID
-	timer  *time.Timer
-	cancel context.CancelFunc
+	id                  peer.ConnID
+	timer               *time.Timer
+	cancel              context.CancelFunc
+	inactivityThreshold time.Duration
 }
 
 func NewInactivityMonitor(peerID peer.ConnID) *Monitor {
 	i := &Monitor{
-		id:    peerID,
-		timer: time.NewTimer(0),
+		id:                  peerID,
+		timer:               time.NewTimer(0),
+		inactivityThreshold: inactivityThreshold(),
 	}
 	i.timer.Stop()
 	return i
 }
 
 func (i *Monitor) Start(ctx context.Context, timeoutChan chan peer.ConnID) {
-	i.timer.Reset(inactivityThreshold)
+	i.timer.Reset(i.inactivityThreshold)
 	defer i.timer.Stop()
 
 	ctx, i.cancel = context.WithCancel(ctx)
@@ -57,5 +61,19 @@ func (i *Monitor) PauseTimer() {
 }
 
 func (i *Monitor) ResetTimer() {
-	i.timer.Reset(inactivityThreshold)
+	i.timer.Reset(i.inactivityThreshold)
+}
+
+func inactivityThreshold() time.Duration {
+	envValue := os.Getenv("NB_INACTIVITY_THRESHOLD")
+	if envValue == "" {
+		return defaultInactivityThreshold
+	}
+
+	parsedMinutes, err := strconv.Atoi(envValue)
+	if err != nil || parsedMinutes <= 0 {
+		return defaultInactivityThreshold
+	}
+
+	return time.Duration(parsedMinutes) * time.Minute
 }
