7b5d370fc9f73d48af7626b1eb56767f60f7ced0
Zoltán Papp <zoltan.pmail@gmail.com>
2025-03-21 11:26:46 +0100

    Remove unused function

--- client/internal/lazyconn/wgiface.go
+++ client/internal/lazyconn/wgiface.go
@@ -6,12 +6,9 @@ import (
 	"time"
 
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
-
-	"github.com/netbirdio/netbird/client/iface/configurer"
 )
 
 type WGIface interface {
 	RemovePeer(peerKey string) error
 	UpdatePeer(peerKey string, allowedIps []netip.Prefix, keepAlive time.Duration, endpoint *net.UDPAddr, preSharedKey *wgtypes.Key) error
-	GetStats() (map[string]configurer.WGStats, error)
 }
