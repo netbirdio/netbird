50986a2ad9c88c2791d4f0f1a945f6ecf602600f
crn4 <vladimir@netbird.io>
2025-04-08 21:59:26 +0200

    remove unwanted bench

--- management/server/updatechannel_test.go
+++ management/server/updatechannel_test.go
@@ -2,12 +2,8 @@ package server
 
 import (
 	"context"
-	"fmt"
-	"math/rand/v2"
 	"testing"
 	"time"
-
-	log "github.com/sirupsen/logrus"
 
 	"github.com/netbirdio/netbird/management/proto"
 )
@@ -79,64 +75,5 @@ func TestCloseChannel(t *testing.T) {
 	peersUpdater.CloseChannel(context.Background(), peer)
 	if _, ok := peersUpdater.peerChannels[peer]; ok {
 		t.Error("Error closing the channel")
-	}
-}
-
-func BenchmarkUpdate(b *testing.B) {
-	updater := NewPeersUpdateManager(nil)
-	const peers = 1 << 20 // 1m
-	bulkpeer := "bulkpeer"
-	msg := &UpdateMessage{
-		Update: &proto.SyncResponse{
-			NetworkMap: &proto.NetworkMap{
-				Serial: 0,
-			},
-		},
-	}
-	for i := range peers {
-		_ = updater.CreateChannel(context.Background(), fmt.Sprintf("%s-%d", bulkpeer, i))
-	}
-	ctx := context.Background()
-	b.ResetTimer()
-	b.Run("read mutex (new)", func(b *testing.B) {
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				updater.SendUpdate(ctx, fmt.Sprintf("%s-%d", bulkpeer, rand.IntN(peers)), msg)
-			}
-		})
-	})
-	b.Run("write mutex (old)", func(b *testing.B) {
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				updater.sendUpdateMxW(ctx, fmt.Sprintf("%s-%d", bulkpeer, rand.IntN(peers)), msg)
-			}
-		})
-	})
-}
-
-func (p *PeersUpdateManager) sendUpdateMxW(ctx context.Context, peerID string, update *UpdateMessage) {
-	start := time.Now()
-	var found, dropped bool
-
-	p.channelsMux.Lock()
-
-	defer func() {
-		p.channelsMux.Unlock()
-		if p.metrics != nil {
-			p.metrics.UpdateChannelMetrics().CountSendUpdateDuration(time.Since(start), found, dropped)
-		}
-	}()
-
-	if channel, ok := p.peerChannels[peerID]; ok {
-		found = true
-		select {
-		case channel <- update:
-			log.WithContext(ctx).Debugf("update was sent to channel for peer %s", peerID)
-		default:
-			dropped = true
-			log.WithContext(ctx).Warnf("channel for peer %s is %d full or closed", peerID, len(channel))
-		}
-	} else {
-		log.WithContext(ctx).Debugf("peer %s has no channel", peerID)
 	}
 }
