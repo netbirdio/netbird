e30389f15f81b7013bb5094a968f9282aeb1cc6f
Viktor Liu <17948409+lixmal@users.noreply.github.com>
2025-06-05 13:54:15 +0200

    [client] Add systemd netbird logs to debug bundle (#3917)

--- client/internal/debug/debug.go
+++ client/internal/debug/debug.go
@@ -274,10 +274,15 @@ func (g *BundleGenerator) createArchive() error {
 		log.Errorf("Failed to add wg show output: %v", err)
 	}
 
-	if g.logFile != "console" {
+	if g.logFile != "console" && g.logFile != "" {
 		if err := g.addLogfile(); err != nil {
-			return fmt.Errorf("add log file: %w", err)
+			log.Errorf("Failed to add log file to debug bundle: %v", err)
+			if err := g.trySystemdLogFallback(); err != nil {
+				log.Errorf("Failed to add systemd logs as fallback: %v", err)
+			}
 		}
+	} else if err := g.trySystemdLogFallback(); err != nil {
+		log.Errorf("Failed to add systemd logs: %v", err)
 	}
 
 	return nil
