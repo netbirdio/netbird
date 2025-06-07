bc72cca5bd1289118709cb716a7f37fa7aa16a3e
Maycon Santos <mlsmaycon@gmail.com>
2025-06-08 01:36:23 +0200

    remove extra runs and log output

--- client/system/process_test.go
+++ client/system/process_test.go
@@ -1,8 +1,6 @@
 package system
 
 import (
-	"os"
-	"slices"
 	"testing"
 
 	"github.com/shirou/gopsutil/v3/process"
@@ -31,29 +29,10 @@ func Benchmark_getRunningProcesses(b *testing.B) {
 			}
 		}
 	})
-	b.Run("getting PIDs only", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			_, err := process.Pids()
-			if err != nil && !os.IsNotExist(err) && !slices.Contains([]string{"no such process", "process not found"}, err.Error()) {
-				b.Fatalf("unexpected error when getting PIDs: %v", err)
-			}
-		}
-	})
-	pids, err := process.Pids()
-	if err != nil {
-		b.Fatalf("unexpected error when getting PIDs: %v", err)
-	}
-	b.Run("getting exe", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			for _, pid := range pids {
-				proc := &process.Process{Pid: pid}
-				_, err := proc.Exe()
-				if err != nil && !os.IsNotExist(err) && !slices.Contains([]string{"no such process", "process not found"}, err.Error()) {
-					b.Fatalf("unexpected error when getting exe: %v", err)
-				}
-			}
-		}
-	})
+	s, _ := getRunningProcesses()
+	b.Logf("getRunningProcesses returned %d processes", len(s))
+	s, _ = getRunningProcessesOld()
+	b.Logf("getRunningProcessesOld returned %d processes", len(s))
 }
 
 func getRunningProcessesOld() ([]string, error) {
