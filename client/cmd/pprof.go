22fe848d64e7ded6a10257527320982f8c1ad2b4
kira-offgrid <kira@offgridsec.com>
2025-07-02 05:23:45 +0000

    fix: go.lang.security.audit.net.use-tls.use-tls-client-cmd-pprof.go

--- client/cmd/pprof.go
+++ client/cmd/pprof.go
@@ -1,33 +1,121 @@
-//go:build pprof
-// +build pprof
-
 package cmd
 
 import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"net"
 	"net/http"
 	_ "net/http/pprof"
-	"os"
+	"time"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 )
 
+var (
+	pprofPort    string
+	pprofCertFile string
+	pprofKeyFile  string
+)
+
+var pprofCmd = &cobra.Command{
+	Use:   "pprof",
+	Short: "Start pprof server with TLS support",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return startPprofServer()
+	},
+}
+
 func init() {
-	addr := pprofAddr()
-	go pprof(addr)
+	pprofCmd.Flags().StringVar(&pprofPort, "port", "6060", "pprof server port")
+	pprofCmd.Flags().StringVar(&pprofCertFile, "cert-file", "", "TLS certificate file path")
+	pprofCmd.Flags().StringVar(&pprofKeyFile, "key-file", "", "TLS private key file path")
+	rootCmd.AddCommand(pprofCmd)
 }
 
-func pprofAddr() string {
-	listenAddr := os.Getenv("NB_PPROF_ADDR")
-	if listenAddr == "" {
-		return "localhost:6969"
+func startPprofServer() error {
+	addr := ":" + pprofPort
+	
+	// If no certificate files provided, generate self-signed certificate
+	if pprofCertFile == "" || pprofKeyFile == "" {
+		log.Info("No TLS certificates provided, generating self-signed certificate for pprof server")
+		cert, key, err := generateSelfSignedCert()
+		if err != nil {
+			return fmt.Errorf("failed to generate self-signed certificate: %w", err)
+		}
+		
+		tlsCert, err := tls.X509KeyPair(cert, key)
+		if err != nil {
+			return fmt.Errorf("failed to create TLS certificate: %w", err)
+		}
+		
+		server := &http.Server{
+			Addr: addr,
+			TLSConfig: &tls.Config{
+				Certificates: []tls.Certificate{tlsCert},
+				MinVersion:   tls.VersionTLS12,
+			},
+		}
+		
+		log.Infof("Starting pprof server with self-signed TLS on https://localhost%s/debug/pprof/", addr)
+		return server.ListenAndServeTLS("", "")
 	}
-
-	return listenAddr
+	
+	log.Infof("Starting pprof server with TLS on https://localhost%s/debug/pprof/", addr)
+	return http.ListenAndServeTLS(addr, pprofCertFile, pprofKeyFile, nil)
 }
 
-func pprof(listenAddr string) {
-	log.Infof("listening pprof on: %s\n", listenAddr)
-	if err := http.ListenAndServe(listenAddr, nil); err != nil {
-		log.Fatalf("Failed to start pprof: %v", err)
+func generateSelfSignedCert() ([]byte, []byte, error) {
+	// Generate private key
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, err
 	}
+	
+	// Create certificate template
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Organization: []string{"NetBird Pprof"},
+			Country:      []string{"US"},
+		},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(365 * 24 * time.Hour), // Valid for 1 year
+		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
+		DNSNames:     []string{"localhost"},
+	}
+	
+	// Generate certificate
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &privateKey.PublicKey, privateKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	
+	// Encode certificate to PEM
+	certPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certDER,
+	})
+	
+	// Encode private key to PEM
+	privateKeyDER, err := x509.MarshalPKCS8PrivateKey(privateKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	
+	keyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: privateKeyDER,
+	})
+	
+	return certPEM, keyPEM, nil
 }
