fbd9b504b873fc5ed3fc5b164fcaaa861d1eb22d
Pedro Costa <550684+pnmcosta@users.noreply.github.com>
2025-07-16 14:45:23 +0100

    [management] scheduler cancel all jobs

--- management/server/scheduler.go
+++ management/server/scheduler.go
@@ -11,14 +11,16 @@ import (
 // Scheduler is an interface which implementations can schedule and cancel jobs
 type Scheduler interface {
 	Cancel(ctx context.Context, IDs []string)
+	CancelAll(ctx context.Context)
 	Schedule(ctx context.Context, in time.Duration, ID string, job func() (nextRunIn time.Duration, reschedule bool))
 	IsSchedulerRunning(ID string) bool
 }
 
 // MockScheduler is a mock implementation of  Scheduler
 type MockScheduler struct {
-	CancelFunc   func(ctx context.Context, IDs []string)
-	ScheduleFunc func(ctx context.Context, in time.Duration, ID string, job func() (nextRunIn time.Duration, reschedule bool))
+	CancelFunc    func(ctx context.Context, IDs []string)
+	CancelAllFunc func(ctx context.Context)
+	ScheduleFunc  func(ctx context.Context, in time.Duration, ID string, job func() (nextRunIn time.Duration, reschedule bool))
 }
 
 // Cancel mocks the Cancel function of the Scheduler interface
@@ -28,6 +30,15 @@ func (mock *MockScheduler) Cancel(ctx context.Context, IDs []string) {
 		return
 	}
 	log.WithContext(ctx).Warnf("MockScheduler doesn't have Cancel function defined ")
+}
+
+// CancelAll mocks the CancelAll function of the Scheduler interface
+func (mock *MockScheduler) CancelAll(ctx context.Context) {
+	if mock.CancelAllFunc != nil {
+		mock.CancelAllFunc(ctx)
+		return
+	}
+	log.WithContext(ctx).Warnf("MockScheduler doesn't have CancelAll function defined ")
 }
 
 // Schedule mocks the Schedule function of the Scheduler interface
@@ -50,6 +61,15 @@ type DefaultScheduler struct {
 	// jobs map holds cancellation channels indexed by the job ID
 	jobs map[string]chan struct{}
 	mu   *sync.Mutex
+}
+
+func (wm *DefaultScheduler) CancelAll(ctx context.Context) {
+	wm.mu.Lock()
+	defer wm.mu.Unlock()
+
+	for id := range wm.jobs {
+		wm.cancel(ctx, id)
+	}
 }
 
 // NewDefaultScheduler creates an instance of a DefaultScheduler
