409203fd3d631bd616e5a8fa37bd5536ab975336
Viktor Liu <viktor@netbird.io>
2025-08-10 20:57:17 +0200

    Fix typo

--- client/net/listener_init_windows.go
+++ client/net/listener_init_windows.go
@@ -5,7 +5,7 @@ import (
 )
 
 func (l *ListenerConfig) init() {
-	// TODO: this will select a single source interface, but for UDP we can have variouss ource interfaces and IP addresses.
+	// TODO: this will select a single source interface, but for UDP we can have various source interfaces and IP addresses.
 	// For now we stick to the one that matches the request IP address, which can be the unspecified IP. In this case
 	// the interface will be selected that serves the default route.
 	l.ListenConfig.Control = func(network, address string, c syscall.RawConn) error {
