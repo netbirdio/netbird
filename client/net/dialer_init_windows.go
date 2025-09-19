0749025ea1d1f6e3d6665df626ad7081c0493126
Viktor Liu <viktor@netbird.io>
2025-09-19 19:14:20 +0200

    Fix another lint

--- client/net/dialer_init_windows.go
+++ client/net/dialer_init_windows.go
@@ -1,11 +1,5 @@
 package net
 
-import (
-	"syscall"
-)
-
 func (d *Dialer) init() {
-	d.Dialer.Control = func(network, address string, c syscall.RawConn) error {
-		return applyUnicastIFToSocket(network, address, c)
-	}
+	d.Dialer.Control = applyUnicastIFToSocket
 }
