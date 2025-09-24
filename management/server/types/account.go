d0c8d04f949bf06ad84bf0b7b9912fba3ab78aa6
crn4 <vladimir.klementyev@gmail.com>
2025-09-24 15:12:28 +0200

    minor log

--- management/server/types/account.go
+++ management/server/types/account.go
@@ -298,9 +298,10 @@ func (a *Account) GetPeerNetworkMap(
 		ServiceEnable: dnsManagementStatus,
 	}
 
+	log.WithContext(ctx).Tracef("peer %s dns management status: %v", peerID, dnsManagementStatus)
 	if dnsManagementStatus {
 		var zones []nbdns.CustomZone
-		// log.WithContext(ctx).Tracef("peer %s has dns management enabled with peersCustomZone: %v, and %d peers to connect, peer[0] %v", peerID, peersCustomZone, len(peersToConnect), peersToConnect[0])
+		log.WithContext(ctx).Tracef("peer %s has dns management enabled with peersCustomZone: %v, and %d peers to connect, peer[0] %v", peerID, peersCustomZone, len(peersToConnect), peersToConnect[0])
 		if peersCustomZone.Domain != "" {
 			records := filterZoneRecordsForPeers(peer, peersCustomZone, peersToConnect)
 			zones = append(zones, nbdns.CustomZone{
