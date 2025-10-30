cf11c28ebe15e7370f05469f42b58782581406de
Zoltán Papp <zoltan.pmail@gmail.com>
2025-10-30 12:16:59 +0100

    Fix code duplication

--- client/internal/networkmonitor/check_change_darwin.go
+++ client/internal/networkmonitor/check_change_darwin.go
@@ -10,10 +10,8 @@ import (
 	"os/exec"
 	"syscall"
 	"time"
-	"unsafe"
 
 	log "github.com/sirupsen/logrus"
-	"golang.org/x/net/route"
 	"golang.org/x/sys/unix"
 
 	"github.com/netbirdio/netbird/client/internal/routemanager/systemops"
@@ -36,7 +34,7 @@ func checkChange(ctx context.Context, nexthopv4, nexthopv6 systemops.Nexthop) er
 
 	routeChanged := make(chan struct{})
 	go func() {
-		routeCheck(ctx, fd, nexthopv4, nexthopv6)
+		_ = routeCheck(ctx, fd, nexthopv4, nexthopv6)
 		close(routeChanged)
 	}()
 
@@ -50,82 +48,18 @@ func checkChange(ctx context.Context, nexthopv4, nexthopv6 systemops.Nexthop) er
 	case <-ctx.Done():
 		return ctx.Err()
 	case <-routeChanged:
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
 		log.Infof("route change detected")
 		return nil
 	case <-wakeUp:
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
 		log.Infof("wakeup detected")
 		return nil
 	}
-}
-
-func routeCheck(ctx context.Context, fd int, nexthopv4 systemops.Nexthop, nexthopv6 systemops.Nexthop) {
-	for {
-		if ctx.Err() != nil {
-			return
-		}
-
-		buf := make([]byte, 2048)
-		n, err := unix.Read(fd, buf)
-		if err != nil {
-			if !errors.Is(err, unix.EBADF) && !errors.Is(err, unix.EINVAL) {
-				log.Warnf("Network monitor: failed to read from routing socket: %v", err)
-			}
-			continue
-		}
-		if n < unix.SizeofRtMsghdr {
-			log.Debugf("Network monitor: read from routing socket returned less than expected: %d bytes", n)
-			continue
-		}
-
-		msg := (*unix.RtMsghdr)(unsafe.Pointer(&buf[0]))
-
-		switch msg.Type {
-		// handle route changes
-		case unix.RTM_ADD, syscall.RTM_DELETE:
-			route, err := parseRouteMessage(buf[:n])
-			if err != nil {
-				log.Debugf("Network monitor: error parsing routing message: %v", err)
-				continue
-			}
-
-			if route.Dst.Bits() != 0 {
-				continue
-			}
-
-			intf := "<nil>"
-			if route.Interface != nil {
-				intf = route.Interface.Name
-			}
-			switch msg.Type {
-			case unix.RTM_ADD:
-				log.Infof("Network monitor: default route changed: via %s, interface %s", route.Gw, intf)
-				return
-			case unix.RTM_DELETE:
-				if nexthopv4.Intf != nil && route.Gw.Compare(nexthopv4.IP) == 0 || nexthopv6.Intf != nil && route.Gw.Compare(nexthopv6.IP) == 0 {
-					log.Infof("Network monitor: default route removed: via %s, interface %s", route.Gw, intf)
-					return
-				}
-			}
-		}
-	}
-}
-
-func parseRouteMessage(buf []byte) (*systemops.Route, error) {
-	msgs, err := route.ParseRIB(route.RIBTypeRoute, buf)
-	if err != nil {
-		return nil, fmt.Errorf("parse RIB: %v", err)
-	}
-
-	if len(msgs) != 1 {
-		return nil, fmt.Errorf("unexpected RIB message msgs: %v", msgs)
-	}
-
-	msg, ok := msgs[0].(*route.RouteMessage)
-	if !ok {
-		return nil, fmt.Errorf("unexpected RIB message type: %T", msgs[0])
-	}
-
-	return systemops.MsgToRoute(msg)
 }
 
 func wakeUpListen(ctx context.Context) {
