3d85791e0c17ad38ef4a6b6e3807c562ecf6ae3f
Zoltán Papp <zoltan.pmail@gmail.com>
2025-11-05 09:58:29 +0100

    Revert embedded function calls in Relay lib

--- shared/relay/client/dialer/ws/conn.go
+++ shared/relay/client/dialer/ws/conn.go
@@ -24,7 +24,7 @@ func NewConn(wsConn *websocket.Conn, serverAddress string) net.Conn {
 }
 
 func (c *Conn) Read(b []byte) (n int, err error) {
-	t, ioReader, err := c.Reader(c.ctx)
+	t, ioReader, err := c.Conn.Reader(c.ctx)
 	if err != nil {
 		// todo use ErrClosedByServer
 		return 0, err
@@ -62,5 +62,5 @@ func (c *Conn) SetDeadline(t time.Time) error {
 }
 
 func (c *Conn) Close() error {
-	return c.CloseNow()
+	return c.Conn.CloseNow()
 }
