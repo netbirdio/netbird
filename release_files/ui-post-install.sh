81e7ce512d71d428c0715dd39f94fdbb919e5178
Dennis Schridde <63082+devurandom@users.noreply.github.com>
2025-11-19 00:39:59 +0100

    Address feedback on su invocation

--- release_files/ui-post-install.sh
+++ release_files/ui-post-install.sh
@@ -11,5 +11,5 @@ then
   username="$(id -nu "${uid}")"
   # Only re-run if it was already running
   pkill -x -f /usr/bin/netbird-ui >/dev/null 2>&1
-  su -l - "${username}" -c 'nohup /usr/bin/netbird-ui > /dev/null 2>&1 &'
+  su - "${username}" -c 'nohup /usr/bin/netbird-ui > /dev/null 2>&1 &'
 fi
