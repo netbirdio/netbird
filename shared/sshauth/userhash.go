2cc05abb39dc291f368a768f9b4294fd42e50224
Zoltán Papp <zoltan.pmail@gmail.com>
2025-12-18 11:07:09 +0100

    Use BLAKE2b-256
    # Conflicts:
    #	shared/management/proto/management.pb.go

--- shared/sshauth/userhash.go
+++ shared/sshauth/userhash.go
@@ -1,24 +1,21 @@
 package sshauth
 
 import (
-	"fmt"
-	"hash/fnv"
+	"encoding/hex"
+
+	"golang.org/x/crypto/blake2b"
 )
 
-// UserIDHash represents a hashed user ID (FNV-1a 64-bit)
-type UserIDHash uint64
+// UserIDHash represents a hashed user ID (BLAKE2b-256)
+type UserIDHash [32]byte
 
-// HashUserID hashes a user ID using FNV-1a (64-bit) and returns the hash value
+// HashUserID hashes a user ID using BLAKE2b-256 and returns the hash value
 // This function must produce the same hash on both client and management server
 func HashUserID(userID string) (UserIDHash, error) {
-	h := fnv.New64a()
-	if _, err := h.Write([]byte(userID)); err != nil {
-		return 0, fmt.Errorf("hash user ID: %w", err)
-	}
-	return UserIDHash(h.Sum64()), nil
+	return blake2b.Sum256([]byte(userID)), nil
 }
 
 // String returns the hexadecimal string representation of the hash
 func (h UserIDHash) String() string {
-	return fmt.Sprintf("%016x", uint64(h))
+	return hex.EncodeToString(h[:])
 }
