e13a7375502c80ccfb4e7e234caedd61db6102b9
braginini <bangvalo@gmail.com>
2025-12-21 13:42:02 -0500

    Add yaml to dex sdk

--- idp/sdk/sdk.go
+++ idp/sdk/sdk.go
@@ -11,8 +11,9 @@ import (
 
 // DexIdP wraps the Dex provider with a builder pattern
 type DexIdP struct {
-	provider *dex.Provider
-	config   *dex.Config
+	provider   *dex.Provider
+	config     *dex.Config
+	yamlConfig *dex.YAMLConfig
 }
 
 // Option configures a DexIdP instance
@@ -38,6 +39,11 @@ func WithDevMode(dev bool) Option {
 	return func(c *dex.Config) { c.DevMode = dev }
 }
 
+// WithGRPCAddr sets the gRPC API address
+func WithGRPCAddr(addr string) Option {
+	return func(c *dex.Config) { c.GRPCAddr = addr }
+}
+
 // New creates a new DexIdP instance with the given options
 func New(opts ...Option) (*DexIdP, error) {
 	config := &dex.Config{
@@ -52,14 +58,32 @@ func New(opts ...Option) (*DexIdP, error) {
 	return &DexIdP{config: config}, nil
 }
 
+// NewFromConfigFile creates a new DexIdP instance from a YAML config file
+func NewFromConfigFile(path string) (*DexIdP, error) {
+	yamlConfig, err := dex.LoadConfig(path)
+	if err != nil {
+		return nil, err
+	}
+	return &DexIdP{yamlConfig: yamlConfig}, nil
+}
+
+// NewFromYAMLConfig creates a new DexIdP instance from a YAMLConfig
+func NewFromYAMLConfig(yamlConfig *dex.YAMLConfig) (*DexIdP, error) {
+	return &DexIdP{yamlConfig: yamlConfig}, nil
+}
+
 // Start initializes and starts the embedded OIDC provider
 func (d *DexIdP) Start(ctx context.Context) error {
-	provider, err := dex.NewProvider(ctx, d.config)
+	var err error
+	if d.yamlConfig != nil {
+		d.provider, err = dex.NewProviderFromYAML(ctx, d.yamlConfig)
+	} else {
+		d.provider, err = dex.NewProvider(ctx, d.config)
+	}
 	if err != nil {
 		return err
 	}
-	d.provider = provider
-	return provider.Start(ctx)
+	return d.provider.Start(ctx)
 }
 
 // Stop gracefully shuts down the provider
@@ -81,12 +105,30 @@ func (d *DexIdP) Storage() storage.Storage {
 	return d.provider.Storage()
 }
 
+// CreateUser creates a new user with the given email, username, and password
+func (d *DexIdP) CreateUser(ctx context.Context, email, username, password string) error {
+	return d.provider.CreateUser(ctx, email, username, password)
+}
+
+// DeleteUser removes a user by email
+func (d *DexIdP) DeleteUser(ctx context.Context, email string) error {
+	return d.provider.DeleteUser(ctx, email)
+}
+
+// ListUsers returns all users
+func (d *DexIdP) ListUsers(ctx context.Context) ([]storage.Password, error) {
+	return d.provider.ListUsers(ctx)
+}
+
 // IssuerURL returns the OIDC issuer URL
 func (d *DexIdP) IssuerURL() string {
+	if d.yamlConfig != nil {
+		return d.yamlConfig.Issuer
+	}
 	return d.config.Issuer
 }
 
 // DiscoveryEndpoint returns the OIDC discovery endpoint URL
 func (d *DexIdP) DiscoveryEndpoint() string {
-	return d.config.Issuer + "/dex/.well-known/openid-configuration"
+	return d.IssuerURL() + "/.well-known/openid-configuration"
 }
