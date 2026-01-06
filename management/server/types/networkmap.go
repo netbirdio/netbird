37c1ab092be50a6440c743001d24557ec3a8c4be
crn4 <vladimir.klementyev@gmail.com>
2026-01-06 12:36:53 +0100

    removed comparison debug code

--- management/server/types/networkmap.go
+++ management/server/types/networkmap.go
@@ -3,8 +3,6 @@ package types
 import (
 	"context"
 
-	log "github.com/sirupsen/logrus"
-
 	nbdns "github.com/netbirdio/netbird/dns"
 	nbpeer "github.com/netbirdio/netbird/management/server/peer"
 	"github.com/netbirdio/netbird/management/server/telemetry"
@@ -29,17 +27,9 @@ func (a *Account) GetPeerNetworkMapExp(
 	peersCustomZone nbdns.CustomZone,
 	validatedPeers map[string]struct{},
 	metrics *telemetry.AccountManagerMetrics,
-	isRouter bool,
 ) *NetworkMap {
 	a.initNetworkMapBuilder(validatedPeers)
-	nmap := a.NetworkMapCache.GetPeerNetworkMap(ctx, peerID, peersCustomZone, validatedPeers, metrics)
-	if isRouter && len(nmap.Peers) > 0 && len(nmap.RoutesFirewallRules) == 0 {
-		log.WithContext(ctx).Debugf("NetworkMapBuilder: generated network map for peer %s with peers but no routes firewall rules, network serial %d", peerID, nmap.Network.Serial)
-	}
-	if !isRouter && len(nmap.Peers) > 0 && len(nmap.FirewallRules) == 0 {
-		log.WithContext(ctx).Debugf("NetworkMapBuilder: generated network map for peer %s with peers but no firewall rules, network serial %d", peerID, nmap.Network.Serial)
-	}
-	return nmap
+	return a.NetworkMapCache.GetPeerNetworkMap(ctx, peerID, peersCustomZone, validatedPeers, metrics)
 }
 
 func (a *Account) OnPeerAddedUpdNetworkMapCache(peerId string) error {
