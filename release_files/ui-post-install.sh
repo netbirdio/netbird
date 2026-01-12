8932d1010ee706d5182863287462ec1224848bda
Ian Knight <ian.knight@acaciasystems.com.au>
2026-01-12 05:58:30 +0000

    Fix ui-post-install.sh remove use of loginuid (#5082) Fixes #5082 by using non-truncated user field output from ps instead of getting uid and converting with id. Avoids the use of sed by using word-splitting on a known-format value.

--- release_files/ui-post-install.sh
+++ release_files/ui-post-install.sh
@@ -7,9 +7,9 @@ set -u
 pid="$(pgrep -x -f /usr/bin/netbird-ui || true)"
 if [ -n "${pid}" ]
 then
-  uid="$(cat /proc/"${pid}"/loginuid)"
-  username="$(id -nu "${uid}")"
+  username="$(ps -o 'user:256=' -p "${pid}")"
   # Only re-run if it was already running
   pkill -x -f /usr/bin/netbird-ui >/dev/null 2>&1
-  su - "${username}" -c 'nohup /usr/bin/netbird-ui > /dev/null 2>&1 &'
+  # shellcheck disable=SC2086 # Use word-splitting to trim whitespace
+  su - ${username} -c 'nohup /usr/bin/netbird-ui > /dev/null 2>&1 &'
 fi
