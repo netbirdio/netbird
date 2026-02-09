889ffc1f2da125bbe735c737fc7cf53aaaa478cd
braginini <bangvalo@gmail.com>
2026-02-09 18:45:06 +0100

    Fix getting-started

--- infrastructure_files/getting-started.sh
+++ infrastructure_files/getting-started.sh
@@ -480,7 +480,8 @@ render_combined_yaml() {
 server:
   listenAddress: ":80"
   exposedAddress: "$NETBIRD_HTTP_PROTOCOL://$NETBIRD_DOMAIN:$NETBIRD_PORT"
-  stunPort: $NETBIRD_STUN_PORT
+  stunPorts:
+    - $NETBIRD_STUN_PORT
   metricsPort: 9090
   healthcheckAddress: ":9000"
   logLevel: "info"
