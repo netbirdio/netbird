1754160686ee536b513140fe4fdb66ee02b1b7a1
pascal <pascal@netbird.io>
2026-02-09 19:21:43 +0100

    handle default ports

--- management/server/types/account.go
+++ management/server/types/account.go
@@ -1956,7 +1956,7 @@ func (a *Account) InjectProxyPolicies(ctx context.Context) {
 					}
 				}
 
-				policyID := fmt.Sprintf("proxy-access-%s-%s", service.ID, proxyPeer.ID)
+				policyID := fmt.Sprintf("proxy-access-%s-%s-s", service.ID, proxyPeer.ID, target.Path)
 				a.Policies = append(a.Policies, &Policy{
 					ID:      policyID,
 					Name:    fmt.Sprintf("Proxy Access to %s", service.Name),
