e368d2995b771b0ed288c0ac4cc46c91471ffe6a
Viktor Liu <viktor@netbird.io>
2026-02-12 22:57:28 +0800

    Fix test

--- management/server/http/testing/testing_tools/channel/channel.go
+++ management/server/http/testing/testing_tools/channel/channel.go
@@ -93,7 +93,7 @@ func BuildApiBlackBoxWithDBState(t testing_tools.TB, sqlFile string, expectedPee
 	accessLogsManager := accesslogsmanager.NewManager(store, permissionsManager, nil)
 	proxyTokenStore := nbgrpc.NewOneTimeTokenStore(1 * time.Minute)
 	proxyServiceServer := nbgrpc.NewProxyServiceServer(accessLogsManager, proxyTokenStore, nbgrpc.ProxyOIDCConfig{}, peersManager, userManager)
-	domainManager := manager.NewManager(store, proxyServiceServer)
+	domainManager := manager.NewManager(store, proxyServiceServer, permissionsManager)
 	reverseProxyManager := reverseproxymanager.NewManager(store, am, permissionsManager, proxyServiceServer, domainManager)
 	proxyServiceServer.SetProxyManager(reverseProxyManager)
 	am.SetServiceManager(reverseProxyManager)
