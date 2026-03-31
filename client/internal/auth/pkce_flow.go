0d4ebb9ecb3fe3dbfe9294506ceb463bbf3ee65c
bcmmbaga <bethuelmbaga12@gmail.com>
2026-03-31 17:18:41 +0300

    Remove ClientSecret from auth flow configs
    Signed-off-by: bcmmbaga <bethuelmbaga12@gmail.com>

--- client/internal/auth/pkce_flow.go
+++ client/internal/auth/pkce_flow.go
@@ -38,8 +38,6 @@ const (
 type PKCEAuthProviderConfig struct {
 	// ClientID An IDP application client id
 	ClientID string
-	// ClientSecret An IDP application client secret
-	ClientSecret string
 	// Audience An Audience for to authorization validation
 	Audience string
 	// TokenEndpoint is the endpoint of an IDP manager where clients can obtain access token
@@ -111,8 +109,7 @@ func NewPKCEAuthorizationFlow(config PKCEAuthProviderConfig) (*PKCEAuthorization
 	}
 
 	cfg := &oauth2.Config{
-		ClientID:     config.ClientID,
-		ClientSecret: config.ClientSecret,
+		ClientID: config.ClientID,
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  config.AuthorizationEndpoint,
 			TokenURL: config.TokenEndpoint,
