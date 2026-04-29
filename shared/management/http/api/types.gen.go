dedb7e51e9f5888a4ef45e77b4bf382452434d02
crn4 <vladimir@netbird.io>
2026-04-29 11:57:34 +0200

    remove owner email from msp fields

--- shared/management/http/api/types.gen.go
+++ shared/management/http/api/types.gen.go
@@ -1641,9 +1641,6 @@ type CreateResellerMSPRequest struct {
 	// Name The name for the MSP
 	Name string `json:"name"`
 
-	// OwnerEmail Expected owner email for the MSP account
-	OwnerEmail *string `json:"owner_email,omitempty"`
-
 	// PriceId Stripe price ID to set up managed subscription for the MSP
 	PriceId *string `json:"price_id,omitempty"`
 
@@ -3602,9 +3599,6 @@ type ResellerMSPResponse struct {
 	// Name The MSP name
 	Name string `json:"name"`
 
-	// OwnerEmail Expected owner email for the MSP account (set by reseller at creation)
-	OwnerEmail *string `json:"owner_email,omitempty"`
-
 	// ResellerCustomerId Reseller's internal customer reference for this MSP
 	ResellerCustomerId *string `json:"reseller_customer_id,omitempty"`
 }
@@ -4240,9 +4234,6 @@ type TenantResponseStatus string
 
 // UpdateResellerMSPRequest defines model for UpdateResellerMSPRequest.
 type UpdateResellerMSPRequest struct {
-	// OwnerEmail Expected owner email for the MSP account
-	OwnerEmail *string `json:"owner_email,omitempty"`
-
 	// ResellerCustomerId Reseller's internal customer reference for this MSP
 	ResellerCustomerId *string `json:"reseller_customer_id,omitempty"`
 }
