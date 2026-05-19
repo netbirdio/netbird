5a3301b3c715709b7b19f3033a4a08a89311703e
Theodor S. Midtlien <theodor@midtlien.com>
2026-05-19 21:53:26 +0200

    Fix comment

--- util/logrotate_nonlinux.go
+++ util/logrotate_nonlinux.go
@@ -2,10 +2,9 @@
 
 package util
 
-// FindLogrotateConflicts scans the standard logrotate locations and returns
-// true and the first config file that contains a non-comment line indicating
-// it's configured for netbird.
-// Will always return false for non-linux device.
+// FindLogrotateConflicts scans the standard logrotate locations for
+// indications of conflict with netbird. It will always return false for
+// non-linux devices.
 func FindFirstLogrotateConflict() (bool, string) {
 	return false, ""
 }
