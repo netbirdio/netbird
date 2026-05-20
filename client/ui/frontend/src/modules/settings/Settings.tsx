42534b24c504ac92d89fb1fa86c8eb2314d10acb
Eduard Gert <kontakt@eduardgert.de>
2026-05-20 13:43:18 +0200

    fix scrollarea inside settings

--- client/ui/frontend/src/modules/settings/Settings.tsx
+++ client/ui/frontend/src/modules/settings/Settings.tsx
@@ -39,6 +39,7 @@ export const Settings = () => {
             <SettingsNavigationTriggers />
             <MainRightSide>
                 <ScrollArea.Root
+                    key={active}
                     type={"auto"}
                     className={"flex-1 min-h-0 overflow-hidden"}
                 >
