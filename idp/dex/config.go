13e41e432c4fb36b9fd18c5323e7fb97c2013f2a
crn4 <vladimir@netbird.io>
2026-05-21 15:21:28 +0200

    idp dex fix

--- idp/dex/config.go
+++ idp/dex/config.go
@@ -308,7 +308,7 @@ func (s *Storage) OpenStorage(logger *slog.Logger) (storage.Storage, error) {
 		if file == "" {
 			return nil, fmt.Errorf("sqlite3 storage requires 'file' config")
 		}
-		return (&sql.SQLite3{File: file}).Open(logger)
+		return newSQLite3(file).Open(logger)
 	case "postgres":
 		dsn, _ := s.Config["dsn"].(string)
 		if dsn == "" {
