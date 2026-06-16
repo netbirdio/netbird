40f369eb989e7409c30ab59a575fbe5b23b63708
Brad Ison <bison@netbird.io>
2026-06-16 16:00:35 +0200

    [management, signal] Read pprof listen address from environment
    This adds support for configuring the pprof listen address for the
    standalone management and signal services via the NB_PPROF_ADDR
    environment variable.  The default address and behavior remain the
    same, i.e. pprof is enabled by default on: `localhost:6060`

--- management/main.go
+++ management/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+
 	// nolint:gosec
 	_ "net/http/pprof"
 	"os"
@@ -10,9 +11,18 @@ import (
 	"github.com/netbirdio/netbird/management/cmd"
 )
 
+func pprofAddr() string {
+	listenAddr := os.Getenv("NB_PPROF_ADDR")
+	if listenAddr == "" {
+		return "localhost:6060"
+	}
+
+	return listenAddr
+}
+
 func main() {
 	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
+		log.Println(http.ListenAndServe(pprofAddr(), nil))
 	}()
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
