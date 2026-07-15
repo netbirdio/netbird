69e96596384a6d3ff6d5ba5aa010c58f6c2c3152
pascal <pascal@netbird.io>
2026-07-15 11:40:20 +0200

    control pprof via env var

--- management/main.go
+++ management/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"net/http"
+	// nolint:gosec
+	_ "net/http/pprof"
 	"os"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/netbirdio/netbird/management/cmd"
 )
 
 func main() {
+	if pprofAddr := os.Getenv("NB_PPROF_ADDR"); pprofAddr != "" {
+		log.Infof("pprof enabled, listening on: %s", pprofAddr)
+		go func() {
+			log.Println(http.ListenAndServe(pprofAddr, nil))
+		}()
+	}
+
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
