6ccea902bf7943d5fbb91a9d8889f8a4ea608482
Optic00 <benjamin.busche@gmail.com>
2026-07-31 20:25:42 +0200

    test(client): cover successful interface index lookup

--- client/internal/dns/interface_index_test.go
+++ client/internal/dns/interface_index_test.go
@@ -1,6 +1,28 @@
 package dns
 
-import "testing"
+import (
+	"net"
+	"testing"
+)
+
+func TestGetInterfaceIndexExisting(t *testing.T) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		t.Fatalf("list network interfaces: %v", err)
+	}
+	if len(interfaces) == 0 {
+		t.Fatal("expected at least one network interface")
+	}
+
+	iface := interfaces[0]
+	index, err := getInterfaceIndex(iface.Name)
+	if err != nil {
+		t.Fatalf("look up existing interface %q: %v", iface.Name, err)
+	}
+	if index != iface.Index {
+		t.Fatalf("expected interface index %d, got %d", iface.Index, index)
+	}
+}
 
 func TestGetInterfaceIndexMissing(t *testing.T) {
 	index, err := getInterfaceIndex("netbird-interface-that-does-not-exist")
