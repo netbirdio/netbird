d4e1c8978e20f7eb99377284fd3b037e626826c7
pascal <pascal@netbird.io>
2026-08-03 23:08:43 +0200

    add all group to user group lookup

--- management/internals/network_map_db/pgsql/user.go
+++ management/internals/network_map_db/pgsql/user.go
@@ -8,9 +8,14 @@ import (
 
 const (
 	GetAllowedUserIdsQuery = `
-	select id, array (select json_array_elements_text(auto_groups::json)) as auto_groups 
+	select id, array (select json_array_elements_text(auto_groups::json)) as auto_groups
 	from users
 	where account_id=$1 and not blocked and not is_service_user
+	`
+
+	GetAllGroupIdQuery = `
+	select id from groups
+	where account_id=$1 and name='All'
 	`
 )
 
@@ -33,12 +38,28 @@ func GetAllowedUsersViaPgxConnection(ctx context.Context, con *pgx.Conn, account
 		return nil, nil, err
 	}
 
+	rows, err = con.Query(ctx, GetAllGroupIdQuery, accountId)
+	if err != nil {
+		return nil, nil, err
+	}
+	allGroupIds, err := pgx.CollectRows(rows, pgx.RowTo[string])
+	if err != nil {
+		return nil, nil, err
+	}
+	allGroupId := ""
+	if len(allGroupIds) > 0 {
+		allGroupId = allGroupIds[0]
+	}
+
 	userIdIdx := make(map[string]struct{})
 	groupIdToUserIds := make(map[string][]string)
 	for _, user := range users {
 		userIdIdx[user.ID] = struct{}{}
 		for _, groupId := range user.AutoGroups {
 			groupIdToUserIds[groupId] = append(groupIdToUserIds[groupId], user.ID)
+		}
+		if allGroupId != "" {
+			groupIdToUserIds[allGroupId] = append(groupIdToUserIds[allGroupId], user.ID)
 		}
 	}
 
