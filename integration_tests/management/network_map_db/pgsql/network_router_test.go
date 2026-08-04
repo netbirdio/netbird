dbfdd04c7bf94b4d98ca07b2c867d3ce596b61c1
Dmitri Dolguikh <dmitri.external@netbird.io>
2026-08-04 16:43:17 +0200

    adding network-routers integration query test
    Signed-off-by: Dmitri Dolguikh <dmitri.external@netbird.io>

--- integration_tests/management/network_map_db/pgsql/network_router_test.go
+++ integration_tests/management/network_map_db/pgsql/network_router_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetGroups(t *testing.T) {
+func TestGetNetworkRouters(t *testing.T) {
 	ctx := context.TODO()
 
 	s, err := networkmap_pgsql.NewPostgresqlStore(ctx, dsn)
@@ -23,11 +23,9 @@ func TestGetGroups(t *testing.T) {
 	assert.NoError(t, err)
 
 	_, err = s.Pool.Query(ctx,
-		"insert into groups (id, account_id, name, resources, public_id) VALUES('test-group-id-1',$1,'test-group-1', '[{\"ID\":\"host-id-1\",\"Type\":\"host\"}]','public-id-1')", acctId)
-	assert.NoError(t, err)
+		"insert into network_routers (id, account_id, public_id, peer, network_id, masquerade, metric, enabled) VALUES('test-nr-id-1',$1,'public-id-1','peer-id-1','network-id-1',TRUE,999,TRUE)", acctId)
 	_, err = s.Pool.Query(ctx,
-		"insert into groups (id, account_id, name, resources, public_id) VALUES('test-group-id-2',$1,'test-group-2', '[{\"ID\":\"subnet-id-1\",\"Type\":\"subnet\"}, {\"ID\":\"host-id-2\",\"Type\":\"host\"}]','public-id-2')", acctId)
-	assert.NoError(t, err)
+		"insert into network_routers (id, account_id, public_id, peer, network_id, masquerade, metric, enabled) VALUES('test-nr-id-2',$1,'public-id-2','peer-id-2','network-id-2',TRUE,333,TRUE)", acctId)
 	_, err = s.Pool.Query(ctx,
 		"insert into group_peers (peer_id, group_id) VALUES('peer-id-1','test-group-id-1')")
 	assert.NoError(t, err)
@@ -53,30 +51,4 @@ func TestGetGroups(t *testing.T) {
 	)
 	assert.NotNil(t, resourceToGroupIdx["host-id-2"]["test-group-id-2"])
 	assert.NotNil(t, resourceToGroupIdx["subnet-id-1"]["test-group-id-2"])
-}
-
-// Verify handling of empty fields in groups table
-// Verify that group's PublicID gets populated on retrieval
-// TODO (dmitri) PublicID should not be populated with delta updates,
-// which require stable PublicIDs
-func TestGetGroupsWithoutExpectedFields(t *testing.T) {
-	ctx := context.TODO()
-
-	s, err := networkmap_pgsql.NewPostgresqlStore(ctx, dsn)
-	assert.NoError(t, err)
-
-	acctId := xid.New().String()
-
-	_, err = s.Pool.Query(ctx,
-		"insert into accounts (id) VALUES($1)", acctId)
-	assert.NoError(t, err)
-
-	_, err = s.Pool.Query(ctx,
-		"insert into groups (id, account_id) VALUES('test-group-id-1',$1)", acctId)
-	assert.NoError(t, err)
-
-	groups, _, err := s.GetGroups(ctx, acctId)
-	assert.NoError(t, err)
-	assert.Len(t, groups, 1)
-	assert.NotEmpty(t, groups[0].PublicID)
 }
