373442271ab69114541248d58603b7abfa188b4d
Dmitri Dolguikh <dmitri.external@netbird.io>
2026-09-10 13:16:04 +0200

    updated comments
    Signed-off-by: Dmitri Dolguikh <dmitri.external@netbird.io>

--- util/wsproxy/server/proxy.go
+++ util/wsproxy/server/proxy.go
@@ -98,8 +98,10 @@ func (ph *proxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		Context: ctx,
 		Handler: ph.handler,
 		BaseConfig: &http.Server{
-			// this is a per-stream timeout which for an h2c is set on reception of a complete HEADERS frame
-			// and effectively sets a deadline for reading of complete request body
+			// b/c we are wrapping a ws connection, ReadTimeout is effectively ignored until
+			// an h2 stream is opened and its read timeout is set. Until that time we are relying
+			// on ws connection built-in timeouts, which we have no contol over.
+			// Stream timeout effectively sets a deadline for reading of a complete request body.
 			ReadTimeout: 5 * time.Second,
 		},
 	})
