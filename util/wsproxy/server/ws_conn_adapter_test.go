0eb7d3c947fabb4d32f62011bf3967f61ef8cdf3
Dmitri Dolguikh <dmitri.external@netbird.io>
2026-09-10 18:38:40 +0200

    use os.TempDir to get tmp dir
    Signed-off-by: Dmitri Dolguikh <dmitri.external@netbird.io>

--- util/wsproxy/server/ws_conn_adapter_test.go
+++ util/wsproxy/server/ws_conn_adapter_test.go
@@ -22,9 +22,6 @@ import (
 )
 
 func TestAdapterHandlingConnectionClosures(t *testing.T) {
-	tmpdir, err := os.MkdirTemp("", "tmp-socket")
-	assert.NoError(t, err)
-
 	var cases = []struct {
 		description string
 		casenum     int
@@ -37,7 +34,7 @@ func TestAdapterHandlingConnectionClosures(t *testing.T) {
 
 	for _, c := range cases {
 		t.Run(c.description, func(t *testing.T) {
-			serversock := filepath.Join(tmpdir, "http-server-"+strconv.FormatInt(rand.Int64(), 10)+".sock")
+			serversock := filepath.Join(os.TempDir(), "http-server-"+strconv.FormatInt(rand.Int64(), 10)+".sock")
 			t.Cleanup(func() { os.Remove(serversock) })
 
 			l, err := net.Listen("unix", serversock)
@@ -114,10 +111,7 @@ func TestAdapterHandlingConnectionClosures(t *testing.T) {
 func TestAdapterHandlingHttpConnection_NoHeadersSent(t *testing.T) {
 	t.Skip("currently disabled as it requires idle timeout to be set")
 
-	tmpdir, err := os.MkdirTemp("", "tmp-socket")
-	assert.NoError(t, err)
-
-	serversock := filepath.Join(tmpdir, "http-server-"+strconv.FormatInt(rand.Int64(), 10)+".sock")
+	serversock := filepath.Join(os.TempDir(), "http-server-"+strconv.FormatInt(rand.Int64(), 10)+".sock")
 	defer os.Remove(serversock)
 
 	l, err := net.Listen("unix", serversock)
